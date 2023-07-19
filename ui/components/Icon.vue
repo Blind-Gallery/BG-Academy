27fea08a7d745af08337ba9f5b0e52575b58a0c8
David Muñoz Guzmán <davemg71@hotmail.com>
2023-07-19 13:28:42 -0600

    icon rotate atribute added

--- ui/components/Icon.vue
+++ ui/components/Icon.vue
@@ -1,5 +1,5 @@
 <template>
-  <Icon :icon="icon" :width="width" :color="color" />
+  <Icon :icon="icon" :width="width" :color="color" :rotate="rotate" />
 </template>
 
 <script>
@@ -22,6 +22,10 @@ export default {
     color: {
       type: String,
       default: 'currentColor' // Color por defecto
+    },
+    rotate: {
+      type: String,
+      default: 'null'
     }
 
   }
