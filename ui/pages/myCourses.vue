5341cb41af9055643c5efadf4be579d3111df243
David Muñoz Guzmán <davemg71@hotmail.com>
2023-07-19 08:14:16 -0600

    course in progress card created

--- ui/pages/myCourses.vue
+++ ui/pages/myCourses.vue
@@ -16,26 +16,44 @@
             </div>
           </b-tab>
           <b-tab title="In progress">
-            <div class="d-flex flex-column align-items-center justify-content-center" style="height: 65vh;">
+            <div v-if="false" class="d-flex flex-column align-items-center justify-content-center" style="height: 65vh;">
               <h3>It looks like you don't have any courses yet</h3>
               <p>Explore our courses and push the limits of your creativity!</p>
               <button class="secondary-btn">
                 Explore courses
               </button>
             </div>
+            <b-row v-else style="height: 65vh">
+              <b-col lg="6">
+                <NuxtLink style="text-decoration: none;" to="/courseNavigator">
+                  <div class="d-flex align-items-center border rounded p-3">
+                    <div>
+                      <img class="mr-4 rounded" width="150px" src="https://cdn.discordapp.com/attachments/995431473828077618/1126451747213291600/taylankoca_imagine_a_modern_art_gallery_with_the_curator_in_it._799e1c51-2808-4a56-b4ab-31ca85854b9c.png">
+                    </div>
+                    <div class="d-flex flex-column">
+                      <h4>Gallery business</h4>
+                      <p class="small">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis.
+                      </p>
+
+                      <b-progress class="mb-2" height="5px" :value="value" />
+                      <div class="d-flex justify-content-between">
+                        <p style="font-weight: 600;" class="small m-0">
+                          Progress
+                        </p><p class="small m-0">
+                          {{ value }}%
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </NuxtLink>
+              </b-col>
+            </b-row>
           </b-tab>
-          <b-tab title="Completed">
-            <div class="d-flex flex-column align-items-center justify-content-center" style="height: 65vh;">
-              <h3>It looks like you don't have any courses yet</h3>
-              <p>Explore our courses and push the limits of your creativity!</p>
-              <button class="secondary-btn">
-                Explore courses
-              </button>
-            </div>
-          </b-tab>
+
           <b-tab title="Certificates">
-            <b-row>
-              <b-col lg="12">
+            <b-row style="height: 65vh">
+              <b-col lg="6">
                 <PxCertificate />
               </b-col>
             </b-row>
@@ -45,11 +63,28 @@
     </b-container>
   </div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      value: 50
+    }
+  },
+  mounted () {
+
+  }
+
+}
+</script>
 <style>
 .nav-tabs .nav-link.active{
     color: #495057;
     font-weight: 600;
     background-color: #fff;
     border-color: #ffffff #ffffff rgb(0, 185, 205);;
+}
+
+.progress-bar{
+  background-color: #00B9CD;
 }
 </style>
