b12814d779feb0d0f19985c5754f1b07cfcbca2c
David Muñoz Guzmán <davemg71@hotmail.com>
2023-07-25 07:09:26 -0600

    cards added to each tab section

--- ui/pages/myCourses.vue
+++ ui/pages/myCourses.vue
@@ -7,13 +7,26 @@
       <div class="mb-5">
         <b-tabs content-class="mt-3">
           <b-tab title="All" active>
-            <div class="d-flex flex-column align-items-center justify-content-center" style="height: 65vh;">
+            <div v-if="false" class="d-flex flex-column align-items-center justify-content-center" style="height: 65vh;">
               <h3>It looks like you don't have any courses yet</h3>
               <p>Explore our courses and push the limits of your creativity!</p>
               <button class="secondary-btn">
                 Explore courses
               </button>
             </div>
+
+            <b-row v-else style="height: 65vh">
+              <b-col lg="4">
+                <PxCard
+                  :is-certificate="false"
+                  :pfp="'https://pbs.twimg.com/profile_images/1562353277647339521/UAZlyXN2_400x400.jpg'"
+                  :instructor="'Hugo Santana'"
+                  :description="'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis.'"
+                  :title="'Introduction to digital objects'"
+                  :cover="'https://cdn.discordapp.com/attachments/995431544019755070/1124593093560254555/anomalyofapes_code_art_black_and_white_tile_texture_flat_graphi_f5d49018-e6de-4513-9679-fcaaef4f7b19.png'"
+                />
+              </b-col>
+            </b-row>
           </b-tab>
           <b-tab title="In progress">
             <div v-if="false" class="d-flex flex-column align-items-center justify-content-center" style="height: 65vh;">
@@ -23,38 +36,36 @@
                 Explore courses
               </button>
             </div>
-            <b-row v-else style="height: 65vh">
-              <b-col lg="6">
-                <NuxtLink style="text-decoration: none;" to="/courseNavigator">
-                  <div class="d-flex align-items-center border rounded p-3">
-                    <div>
-                      <img class="mr-4 rounded" width="150px" src="https://cdn.discordapp.com/attachments/995431473828077618/1126451747213291600/taylankoca_imagine_a_modern_art_gallery_with_the_curator_in_it._799e1c51-2808-4a56-b4ab-31ca85854b9c.png">
-                    </div>
-                    <div class="d-flex flex-column">
-                      <h4>Gallery business</h4>
-                      <p class="small">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis.
-                      </p>
 
-                      <b-progress class="mb-2" height="5px" :value="value" />
-                      <div class="d-flex justify-content-between">
-                        <p style="font-weight: 600;" class="small m-0">
-                          Progress
-                        </p><p class="small m-0">
-                          {{ value }}%
-                        </p>
-                      </div>
-                    </div>
-                  </div>
+            <b-row v-else style="height: 65vh">
+              <b-col lg="4">
+                <NuxtLink class="link" style="text-decoration: none;" to="/courseNavigator">
+                  <PxCard
+                    :is-progress="true"
+                    :is-certificate="false"
+                    :pfp="'https://pbs.twimg.com/profile_images/1562353277647339521/UAZlyXN2_400x400.jpg'"
+                    :instructor="'Hugo Santana'"
+                    :description="'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis.'"
+                    :title="'Introduction to digital objects'"
+                    :cover="'https://cdn.discordapp.com/attachments/995431544019755070/1124593093560254555/anomalyofapes_code_art_black_and_white_tile_texture_flat_graphi_f5d49018-e6de-4513-9679-fcaaef4f7b19.png'"
+                  />
                 </NuxtLink>
               </b-col>
             </b-row>
           </b-tab>
 
           <b-tab title="Certificates">
-            <b-row style="height: 65vh">
-              <b-col lg="6">
-                <PxCertificate />
+            <b-row>
+              <b-col lg="4">
+                <PxCard
+                  :is-progress="false"
+                  :is-certificate="true"
+                  :pfp="'https://pbs.twimg.com/profile_images/1562353277647339521/UAZlyXN2_400x400.jpg'"
+                  :instructor="'Hugo Santana'"
+                  :description="'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis.'"
+                  :title="'Introduction to digital objects'"
+                  :cover="'https://cdn.discordapp.com/attachments/995431544019755070/1124593093560254555/anomalyofapes_code_art_black_and_white_tile_texture_flat_graphi_f5d49018-e6de-4513-9679-fcaaef4f7b19.png'"
+                />
               </b-col>
             </b-row>
           </b-tab>
