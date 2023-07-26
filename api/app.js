c589098d54475f11e09fd7dab754c37c5064314e
tlacloc <desneruda@gmail.com>
2023-07-26 13:19:39 -0600

    🔧 chore(app.js): add fastify-plugin to register decorateFastifyInstance function to decorate fastify instance with login and jwt objects ✨ feat(app.js): add Login, GQL, and JWT imports to use in decorateFastifyInstance function ✨ feat(app.js): add GRAPHQL_ENDPOINT and GRAPHQL_SECRET environment variables to gqlConfig object ✨ feat(app.js): add decorateFastifyInstance function to decorate fastify instance with login and jwt objects ✨ feat(app.js): register decorateFastifyInstance function using fastify-plugin

--- api/app.js
+++ api/app.js
@@ -2,12 +2,51 @@
 
 const path = require('path')
 const AutoLoad = require('@fastify/autoload')
+const fp = require('fastify-plugin')
+
+const {
+  Login,
+} = require('./model')
+
+const {
+  GQL,
+  JWT
+} = require('./service')
+
+const {
+  GRAPHQL_ENDPOINT,
+  GRAPHQL_SECRET
+} = process.env
+
+const gqlConfig = {
+  endpoint: GRAPHQL_ENDPOINT,
+  secret: GRAPHQL_SECRET
+}
+
+async function decorateFastifyInstance (fastify) {
+  const gql = new GQL(gqlConfig)
+  const jwt = new JWT({})
+  const opts = {
+    tokenExpirationTimeMins: 60,
+    refreshTokenExpirationTimeMins: 1440
+  }
+  jwt.init()
+
+  const login = new Login({
+    gql,
+    jwt,
+    opts
+  })
+  fastify.decorate('login', login)
+  fastify.decorate('jwt', jwt)
+}
 
 // Pass --options via CLI arguments in command to enable these options.
 module.exports.options = {}
 
 module.exports = async function (fastify, opts) {
   // Place here your custom code!
+  fastify.register(fp(decorateFastifyInstance))
 
   // Do not touch the following lines
 
