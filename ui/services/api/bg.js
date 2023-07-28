465dd356b7ded9f5152b85a383134f2ec5521839
tlacloc <desneruda@gmail.com>
2023-07-27 18:37:44 -0600

    ✨ feat(api/bg.js): add BlindGalleryAcademy class with login methods for email, Google, and wallet authentication

---
+++ ui/services/api/bg.js
@@ -0,0 +1,12 @@
+export default class BlindGalleryAcademy {
+  constructor ({ }) {
+
+  }
+
+  async loginWithEmail ({ email, password }) {
+  }
+
+  async loginWithGoogle ({ email }) {}
+
+  async loginWithWallet ({ wallet, signedMessage }) {}
+}
