30b9947f195c5d266a06e22cf32c0b7cf11ae7d4
David Muñoz Guzmán <davemg71@hotmail.com>
2023-08-19 09:08:09 -0600

    formulate language configured on english

--- ui/plugins/formulate.config.js
+++ ui/plugins/formulate.config.js
@@ -1,8 +1,8 @@
-import { de } from '@braid/vue-formulate-i18n'
+import { en } from '@braid/vue-formulate-i18n'
 
 export default {
-  plugins: [de],
-  locale: 'de',
+  plugins: [en],
+  locale: 'en',
   rules: {
     foobar: ({ value }) => ['foo', 'bar'].includes(value)
   }
