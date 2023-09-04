8e693ecf0bce6ad8193e501e03f64f265e14b887
David Muñoz Guzmán <datzeldave@gmail.com>
2023-09-04 08:27:59 -0600

    poppins font added to the app

--- ui/nuxt.config.js
+++ ui/nuxt.config.js
@@ -16,7 +16,8 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css' }
+      { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700&display=swap' }
 
     ],
 
