52feb553e9a66542cb00d5eb56d87352a8b6a92d
David Muñoz Guzmán <datzeldave@gmail.com>
2023-09-12 11:07:15 -0600

    watcher added to the PxPlayer

--- ui/components/PxPlayer.vue
+++ ui/components/PxPlayer.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div id="player" :data-vimeo-id="id" data-vimeo-width="900" />
+    <div :id="playerId" data-vimeo-autoplay="true" :data-vimeo-id="id" data-vimeo-width="900" />
   </div>
 </template>
 <script>
@@ -15,18 +15,29 @@ export default {
   },
   data () {
     return {
-      player: null
+      player: null,
+      playerId: 'vimeo-player'
+    }
+  },
+  watch: {
+    id: function (newVal) {
+      this.updatePlayer(newVal)
     }
   },
   mounted () {
-    this.player = new Player('player', {
-      badge: 0
-    })
-
-    this.player.setColors(['#000', '#00b9cd', '#fff', '#00b9cd'])
+    this.initPlayer()
   },
   methods: {
+    initPlayer () {
+      this.player = new Player(this.playerId)
 
+      this.player.setColors(['#000', '#00b9cd', '#fff', '#00b9cd'])
+    },
+    updatePlayer (newVideoId) {
+      this.player.loadVideo(newVideoId).then(() => {
+        this.player.play()
+      })
+    }
   }
 }
 </script>
