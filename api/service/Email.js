15e53343b2ecf9bbd62fc7dbc25a96ff8cf099d4
tlacloc <desneruda@gmail.com>
2023-09-15 19:46:35 -0600

    🔧 chore(Email.js): refactor welcome email message to use constants for better maintainability and readability ✨ feat(Email.js): add HTML version of welcome email message for better user experience

--- api/service/Email.js
+++ api/service/Email.js
@@ -1,6 +1,30 @@
 require('dotenv').config()
 const sgMail = require('@sendgrid/mail')
 
+const WELCOME_MESSAGE = `
+Hello there!
+
+Welcome to the Academy by Blind Gallery -  SLOGAN.
+
+You can access our platform here : https://qa-bg-academy-client-c92169fac952.herokuapp.com/
+
+If you have any questions, reply to this email.
+`
+
+const WELCOME_MESSAGE_HTML = `
+<strong>Hello there!</strong>
+
+<p>Welcome to the Academy by Blind Gallery -  SLOGAN.</p>
+
+<p>You can access our platform <a href="https://qa-bg-academy-client-c92169fac952.herokuapp.com/">here</a></p>
+
+<p>If you have any questions, reply to this email.</p>
+
+<p>Best regards,</p>
+
+<p>Blind Gallery Team</p>
+`
+
 class Email {
   constructor ({ apiKey }) {
     this.sendgrid = sgMail
@@ -24,7 +48,7 @@ class Email {
   async sendWelcomeEmail ({ to }) {
     const subject = 'Welcome to the Academy by Blind Gallery'
     const text = 'Welcome to Blind Gallery'
-    const html = 'Hello there! Welcome to the Academy by Blind Gallery -  SLOGAN.'
+    const html = WELCOME_MESSAGE_HTML
 
     const res = await this.sendEmail({ to, subject, text, html })
 
