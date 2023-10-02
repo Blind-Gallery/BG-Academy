9660185418308080bc71b762f1109f2699930b29
Tlalocman <desneruda@gmail.com>
2023-10-02 12:58:09 -0600

    ✨ feat(index.js): add IPFS module to the list of exported modules in the service index file to enable IPFS functionality in the API service

--- api/service/index.js
+++ api/service/index.js
@@ -2,10 +2,12 @@ const GQL = require('./GQL')
 const JWT = require('./JWT')
 const Email = require('./Email')
 const Documents = require('./Documents')
+const IPFS = require('./IPFS')
 
 module.exports = {
   GQL,
   JWT,
   Email,
-  Documents
+  Documents,
+  IPFS
 }
