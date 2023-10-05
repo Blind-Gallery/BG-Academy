fabfa55e9870b03feb4f08209f7caff2373f3f08
Tlalocman <desneruda@gmail.com>
2023-10-05 17:17:48 -0600

    ✨ feat(service/index.js): add Payments module to the list of exported modules to enable payment functionality in the API

--- api/service/index.js
+++ api/service/index.js
@@ -3,11 +3,13 @@ const JWT = require('./JWT')
 const Email = require('./Email')
 const Documents = require('./Documents')
 const IPFS = require('./IPFS')
+const Payments = require('./Payments')
 
 module.exports = {
   GQL,
   JWT,
   Email,
   Documents,
-  IPFS
+  IPFS,
+  Payments
 }
