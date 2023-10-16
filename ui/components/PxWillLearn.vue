4c5bd1cf5dcaed020e92897b4b9e0864736ddc2f
David Muñoz Guzmán <datzeldave@gmail.com>
2023-10-16 11:26:20 -0600

    component created for the 'you will learn' element

---
+++ ui/components/PxWillLearn.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="d-flex flex-column shadow-sm rounded p-3">
+    <Icon icon="material-symbols:check-small" color="#00c851" width="32" />
+    <p class="m-0" style="font-weight: 600;">
+      {{ title }}
+    </p>
+    <p class="small">
+      {{ description }}
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: 'Title'
+    },
+    description: {
+      type: String,
+      default: 'Description'
+    }
+
+  }
+
+}
+</script>
