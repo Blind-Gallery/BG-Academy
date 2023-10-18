fef35acb0d5c9ed32097961a6676d3df3852ca22
Tlalocman <desneruda@gmail.com>
2023-10-17 18:21:21 -0600

    📦 chore(service): add Documents, IPFS, and Payments services to the service index file 🐛 fix(Documents.js): fix missing import statement for IPFS class 🐛 fix(IPFS.js): fix missing import statements and add missing constructor for BaseIpfs class ✨ feat(Payments.js): add createInvoke method to Payment class for creating payment intents

--- api/service/index.js
+++ api/service/index.js
@@ -1,9 +1,15 @@
 const GQL = require('./GQL')
 const JWT = require('./JWT')
 const Email = require('./Email')
+const Documents = require('./Documents')
+const IPFS = require('./IPFS')
+const Payments = require('./Payments')
 
 module.exports = {
   GQL,
   JWT,
-  Email
+  Email,
+  Documents,
+  IPFS,
+  Payments
 }
