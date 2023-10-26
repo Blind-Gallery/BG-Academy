8ba2d6bb727da2a5c4e1cf1c7e3e5c3063a14509
Tlalocman <desneruda@gmail.com>
2023-10-26 13:25:04 -0600

    📦 chore(Payments.js): add Payments class to handle payment-related operations
    This commit adds a new file `Payments.js` which contains the `Payments` class. The class is responsible for handling payment-related operations. It takes in dependencies such as `gql`, `email`, `opts`, `jwt`, `stripe`, and `tezos` as constructor parameters.
    
    The `Payments` class currently has two methods:
    1. `createStripePaymentIntent`: This method is used to create a payment intent with Stripe. It takes in parameters such as `amount`, `currency`, `paymentMethodTypes`, and `receiptEmail`. It makes use of the `stripe` dependency to create the payment intent.
    2. `verifyStripeWebhook`: This method is used to verify a Stripe webhook. It takes in parameters `signature` and `body`. It makes use of the `stripe` dependency to verify the webhook signature and returns the event.
    
    Both methods handle errors and log them to the console.

---
+++ api/model/Payments.js
@@ -0,0 +1,36 @@
+const { Unauthorized, BadRequest } = require('http-errors')
+const { request, gql } = require('graphql-request')
+
+class Payments {
+  constructor ({ gql, email, opts, jwt, stripe, tezos }) {
+    this.gql = gql
+    this.email = email
+    this.opts = opts
+    this.jwt = jwt
+    this.stripe = stripe
+    this.tezos = tezos
+  }
+
+  async createStripePaymentIntent ({ amount, currency, paymentMethodTypes, receiptEmail }) {
+    let paymentIntent = null
+    try {
+      paymentIntent = await this.stripe.paymentIntent(amount, currency, paymentMethodTypes, receiptEmail)
+    } catch (err) {
+      console.error(err)
+    }
+    return paymentIntent
+  }
+
+  async verifyStripeWebhook (signature, body) {
+    let event = null
+    try {
+      event = await this.stripe.verify(signature, body)
+    } catch (err) {
+      console.error(err)
+      throw new BadRequest(`Webhook Error: ${err.message}`)
+    }
+    return event
+  }
+}
+
+module.exports = Payments
