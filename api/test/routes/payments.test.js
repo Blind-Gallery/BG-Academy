34d0d8c53b009d250579ea391141fe41ec605dd0
Tlalocman <desneruda@gmail.com>
2023-10-27 09:29:56 -0600

    🚧 chore(payments.test.js): add initial test for default root route
    This commit adds an initial test for the default root route in the `payments.test.js` file. The test uses the `tap` testing framework and the `build` helper function to create an instance of the app. It then makes a request to the `/payments/tezos/payment-intent` endpoint and logs the response.

---
+++ api/test/routes/payments.test.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const { test } = require('tap')
+const { build } = require('../helper')
+
+test('default root route', async (t) => {
+  const app = await build(t)
+
+  const res = await app.inject({
+    url: '/payments/tezos/payment-intent'
+  })
+
+  console.log(res)
+})
