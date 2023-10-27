763c9c222a5f027dfdd87ce4573d63c59fd6d7bd
Tlalocman <desneruda@gmail.com>
2023-10-27 09:30:30 -0600

    🔧 fix(Payments.js): add coinGecko dependency to the Payments class constructor to fix missing reference error ✨ feat(Payments.js): add getCoursePrice method to fetch the price of a course from the GraphQL API ✨ feat(Payments.js): add getTezosPrice method to fetch the current price of Tezos in USD from CoinGecko API ✨ feat(Payments.js): modify createTezosPaymentIntent method to calculate the Tezos price based on the course price and the current Tezos price ✨ feat(Payments.js): add verifyTezosPayment method to verify a Tezos payment transaction

--- api/model/Payments.js
+++ api/model/Payments.js
@@ -1,22 +1,41 @@
 const { Unauthorized, BadRequest } = require('http-errors')
 const { request, gql } = require('graphql-request')
 
+const GET_COURSE_BY_ID = gql`
+query ($id: Int!) {
+  courses_by_pk(id: $id) {
+    id
+    price
+  }
+}
+`
 class Payments {
-  constructor ({ gql, email, opts, jwt, stripe, tezos }) {
+  constructor ({ gql, email, opts, jwt, stripe, tezos, coinGecko }) {
     this.gql = gql
     this.email = email
     this.opts = opts
     this.jwt = jwt
     this.stripe = stripe
     this.tezos = tezos
+    this.coinGecko = coinGecko
   }
 
   async getCoursePrice (courseId) {
+    const { courses_by_pk: course } = await this.gql.request(
+      GET_COURSE_BY_ID,
+      { id: courseId }
+    )
 
+    return course.price
   }
 
-  async getTezosPrice () {
-
+  async getTezosPrice (usdAmount) {
+    const { tezos } = await this.coinGecko.getCoinPrice({
+      ids: ['tezos'],
+      currency: 'usd'
+    })
+    const conversion = usdAmount / tezos.usd
+    return conversion
   }
 
   async createStripePaymentIntent ({ amount, currency, paymentMethodTypes, receiptEmail }) {
@@ -40,11 +59,13 @@ class Payments {
     return event
   }
 
-  async createTezosPaymentIntent () {
-
+  async createTezosPaymentIntent ({ courseId }) {
+    const coursePrice = await this.getCoursePrice(courseId)
+    const tezosPrice = await this.getTezosPrice(coursePrice)
+    console.info(coursePrice, tezosPrice)
   }
 
-  async verifyTezosPayment () {
+  async verifyTezosPayment ({ transaction }) {
 
   }
 }
