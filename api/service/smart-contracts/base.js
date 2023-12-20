8175af73c6c5a813fb9d6edc95942dd3cecdad1c
Tlalocman <desneruda@gmail.com>
2023-12-20 01:51:13 -0600

    📝 docs(base.js): update method names to follow camel case convention for better readability and consistency
    The method names `set_administrator`, `add_moderator`, and `remove_moderator` have been updated to `setAdministrator`, `addModerator`, and `removeModerator` respectively. This change improves the readability and follows the camel case naming convention for methods.

--- api/service/smart-contracts/base.js
+++ api/service/smart-contracts/base.js
@@ -21,13 +21,13 @@ class BlindGalleryPermissions extends TezosTransactions {
   }
 
   /**
-   * @name set_administrator
+   * @name setAdministrator
    * @description Set the administrator of the contract
    * @param {String} address - Address of the administrator
    * @param {Number} c - Confirmation blocks
    * @returns {Promise} - Confirmation
    */
-  async set_administrator (address, c = TezosConstants.DEFAULT_CONFIRMATION_BLOCKS) {
+  async setAdministrator (address, c = TezosConstants.DEFAULT_CONFIRMATION_BLOCKS) {
     if (!address) {
       return console.error('Address is not defined')
     }
@@ -48,14 +48,14 @@ class BlindGalleryPermissions extends TezosTransactions {
   }
 
   /**
-   * @name add_moderator
+   * @name addModerator
    * @description Add a moderator to the contract
    * @param {String} address - Address of the moderator
    * @param {String} name - Name of the moderator
    * @param {Number} c - Confirmation blocks
    * @returns {Promise} - Confirmation
    */
-  async add_moderator (address, name, c = TezosConstants.DEFAULT_CONFIRMATION_BLOCKS) {
+  async addModerator (address, name, c = TezosConstants.DEFAULT_CONFIRMATION_BLOCKS) {
     if (!address) {
       return console.error('Address is not defined')
     }
@@ -79,13 +79,13 @@ class BlindGalleryPermissions extends TezosTransactions {
   }
 
   /**
-   * @name remove_moderator
+   * @name removeModerator
    * @description Remove a moderator from the contract
    * @param {String} address - Address of the moderator
    * @param {Number} c - Confirmation blocks
    * @returns {Promise} - Confirmation
    */
-  async remove_moderator (address, c = TezosConstants.DEFAULT_CONFIRMATION_BLOCKS) {
+  async removeModerator (address, c = TezosConstants.DEFAULT_CONFIRMATION_BLOCKS) {
     if (!address) {
       return console.error('Address is not defined')
     }
