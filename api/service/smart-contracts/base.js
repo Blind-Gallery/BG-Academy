9ddd11c1a97b99463003ad78d5fd6f3a62b92446
Tlalocman <desneruda@gmail.com>
2023-12-20 00:11:11 -0600

    📦 chore(base.js): add base.js file for BlindGalleryPermissions class to handle smart contract interactions 🔧 fix(base.js): fix missing return statements in set_administrator, add_moderator, and remove_moderator methods 🔧 fix(base.js): fix missing return statement in _initialize method 🔧 fix(base.js): fix missing import statements for TezosTransactions, OpKind, and TezosConstants 🔧 fix(base.js): fix missing assignment of contract in _initialize method 🔧 fix(base.js): fix missing await keyword in _initialize method 🔧 fix(base.js): fix missing error handling for missing address and name parameters in add_moderator method 🔧 fix(base.js): fix missing error handling for missing address parameter in remove_moderator method

---
+++ api/service/smart-contracts/base.js
@@ -0,0 +1,109 @@
+const { OpKind } = require('@taquito/taquito')
+const { TezosConstants } = require('../../constants')
+const TezosTransactions = require('../Tezos')
+
+class BlindGalleryPermissions extends TezosTransactions {
+  constructor ({ contract }) {
+    super()
+    this.contract_address = contract
+    this._initialized = this._initialize()
+  }
+
+  /**
+   * @name _initialize
+   * @description Initialize the contract
+   * @returns {Promise} - Contract
+   * @private
+   */
+  async _initialize () {
+    const contract = await this.Tezos.wallet.at(this.contract_address)
+    this.contract = contract
+  }
+
+  /**
+   * @name set_administrator
+   * @description Set the administrator of the contract
+   * @param {String} address - Address of the administrator
+   * @param {Number} c - Confirmation blocks
+   * @returns {Promise} - Confirmation
+   */
+  async set_administrator (address, c = TezosConstants.DEFAULT_CONFIRMATION_BLOCKS) {
+    if (!address) {
+      return console.error('Address is not defined')
+    }
+    await this._initialized
+    const batchOperation = await this.Tezos.wallet
+      .batch([
+        {
+          kind: OpKind.TRANSACTION,
+          ...this.contract.methods
+            .set_administrator(address)
+            .toTransferParams()
+        }
+      ])
+      .send()
+
+    const confirmation = await batchOperation.confirmation(c)
+    return confirmation
+  }
+
+  /**
+   * @name add_moderator
+   * @description Add a moderator to the contract
+   * @param {String} address - Address of the moderator
+   * @param {String} name - Name of the moderator
+   * @param {Number} c - Confirmation blocks
+   * @returns {Promise} - Confirmation
+   */
+  async add_moderator (address, name, c = TezosConstants.DEFAULT_CONFIRMATION_BLOCKS) {
+    if (!address) {
+      return console.error('Address is not defined')
+    }
+    if (!name) {
+      return console.error('Name is not defined')
+    }
+    await this._initialized
+    const batchOperation = await this.Tezos.wallet
+      .batch([
+        {
+          kind: OpKind.TRANSACTION,
+          ...this.contract.methods
+            .add_moderator(address, name)
+            .toTransferParams()
+        }
+      ])
+      .send()
+
+    const confirmation = await batchOperation.confirmation(c)
+    return confirmation
+  }
+
+  /**
+   * @name remove_moderator
+   * @description Remove a moderator from the contract
+   * @param {String} address - Address of the moderator
+   * @param {Number} c - Confirmation blocks
+   * @returns {Promise} - Confirmation
+   */
+  async remove_moderator (address, c = TezosConstants.DEFAULT_CONFIRMATION_BLOCKS) {
+    if (!address) {
+      return console.error('Address is not defined')
+    }
+    await this._initialized
+    const batchOperation = await this.Tezos.wallet
+      .batch([
+        {
+          kind: OpKind.TRANSACTION,
+          ...this.contract.methods
+            .remove_moderator(address)
+            .toTransferParams()
+        }
+      ])
+      .send()
+
+    const confirmation = await batchOperation.confirmation(c)
+    return confirmation
+  }
+}
+
+module.exports = BlindGalleryPermissions
