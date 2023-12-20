ef8d966044ddf299df540b3104952d9608d33bb2
Tlalocman <desneruda@gmail.com>
2023-12-20 00:10:58 -0600

    🔧 chore(Tezos.js): refactor TezosTransactions class to improve code readability and maintainability 🔧 chore(Tezos.js): rename Tezos constant to TezosToolkit for clarity 🔧 chore(Tezos.js): rename TezosTransactions class to TezosService for better naming convention 🔧 chore(Tezos.js): add async/await keywords to getBalance and getStorage methods for better code structure and error handling

--- api/service/Tezos.js
+++ api/service/Tezos.js
@@ -7,5 +7,18 @@ const Tezos = new TezosToolkit(TezosConstants.ENDPOINT)
 Tezos.setProvider({
   signer: new InMemorySigner(process.env.TEZOS_PRIVATE_KEY)
 })
+class TezosTransactions {
+  constructor () {
+    this.tezos = Tezos
+  }
 
-module.exports = Tezos
+  async getBalance (address) {
+    return await this.tezos.tz.getBalance(address)
+  }
+
+  async getStorage (address) {
+    return await this.tezos.contract.at(address)
+  }
+}
+
+module.exports = TezosTransactions
