0a2d5df77c040b7e4c3f563f32ac330045dd0759
David Muñoz Guzmán <datzeldave@gmail.com>
2023-12-22 11:03:36 -0600

    slogan changed

--- ui/pages/index.vue
+++ ui/pages/index.vue
@@ -7,8 +7,8 @@
           <b-row class="align-items-center">
             <b-col class="d-flex flex-column intro-left-col" style="gap:1rem">
               <h1>
-                Your starting point in
-                <span style="color:#00B9CD"> Digital Art & Culture</span>
+                Your starting point to
+                <span style="color:#00B9CD"> learn blockchain art</span>
               </h1>
               <p>
                 Explore interactive courses led by industry experts, paving your way in the digital art revolution.
@@ -26,38 +26,6 @@
         </b-container>
       </b-container>
       <!--INFO CONTAINER-->
-      <b-container v-if="!$auth.loggedIn" fluid style="background-color: #F6F6F6;">
-        <b-container class="mb-5 py-4" style="max-width: 1240px;">
-          <!--PARTNERS CONTAINER-->
-          <h4 class="m-0" style="text-align: center; font-weight: 600; opacity: 0.7;">
-            Meet our partners
-          </h4>
-
-          <div class="partners-container ">
-            <img
-              width="120px"
-              alt="joyn"
-              src="https://app.joyn.xyz/_next/static/media/logo-dark.1d2f9f52.svg"
-            >
-
-            <img
-              width="150px"
-              alt="autonomy"
-              src="https://autonomy.io/Logo.svg"
-            >
-            <img
-              width="105px"
-              alt="tezos"
-              src="https://imgs.search.brave.com/Ni4B87Cq07txaSZWBtEEf8YjF5llOTY-L8WPXow5tZs/rs:fit:1200:778:1/g:ce/aHR0cHM6Ly9icmFu/ZHBhbGV0dGVzLmNv/bS93cC1jb250ZW50/L3VwbG9hZHMvMjAy/MS8wNS90ZXpvcy0w/Mi5wbmc"
-            >
-            <img
-              width="160px"
-              alt="fxhash"
-              src="https://fxhash.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F84947e48-fba3-4cbb-b203-aaad507b315b%2Ffxhash_tbg_bt_01.png?table=block&id=b34cff0e-7784-40ec-9020-cea678fe9d41&spaceId=f9d1e984-50ed-4650-a543-95d2b6c0ba90&width=1860&userId=&cache=v2"
-            >
-          </div>
-        </b-container>
-      </b-container>
 
       <!--LOGGED CONTAINER-->
       <b-container v-if="$auth.loggedIn" class="mb-5" style="max-width: 1240px;">
