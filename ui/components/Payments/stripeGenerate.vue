5da0b9b04103c870d8bcbb116540ba5ce59bd639
Tlalocman <desneruda@gmail.com>
2023-12-27 16:15:37 -0600

    🆕 feat(stripeGenerate.vue): add new component for generating Stripe payment
    This commit adds a new component called `stripeGenerate.vue` which is responsible for generating a Stripe payment. The component includes a template with a Stripe payment element and a button for checkout. It also includes a script section with props for price and email, data for Stripe publishable key and options for appearance and confirm parameters. The component is created on mount and includes a method for generating the payment intent and a method for submitting the payment.

---
+++ ui/components/Payments/stripeGenerate.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <stripe-element-payment
+      v-if="elementsOptions.clientSecret"
+      ref="paymentRef"
+      :pk="pk"
+      :elements-options="elementsOptions"
+      :confirm-params="confirmParams"
+    />
+    <button @click="pay">
+      Checkout
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    price: {
+      type: Number,
+      required: true,
+      default: 1
+    },
+    email: {
+      type: String,
+      required: true
+    }
+  },
+  data () {
+    this.pk = process.env.STRIPE_PUBLISHABLE_KEY
+    return {
+      elementsOptions: {
+        appearance: {} // appearance options
+      },
+      confirmParams: {
+        return_url: 'http://localhost:8082/buyCourse/success' // success url
+      }
+    }
+  },
+  created () {
+    this.generatePaymentIntent()
+  },
+  methods: {
+    async generatePaymentIntent () {
+      const { paymentIntent } = await this.$axios.$post('/payments/stripe/create-intent', {
+        amount: this.price * 100,
+        currency: 'usd',
+        paymentMethodTypes: ['card'],
+        receiptEmail: this.email
+      })
+      console.info('Payment intent: ', paymentIntent)
+      console.info('route:', this.$nuxt.$route)
+      this.elementsOptions.clientSecret = paymentIntent.client_secret
+      this.$forceUpdate() // this is a hack to force the component to re-render and update the client secret
+    },
+    pay () {
+      this.$refs.paymentRef.submit()
+    }
+  }
+}
+</script>
