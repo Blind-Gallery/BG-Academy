8208376e3c7b143238550c0056c07bb8e7bc042b
David Muñoz Guzmán <108280181+Datzel@users.noreply.github.com>
2024-01-04 11:40:17 -0600

    Refactoring and improvement of the alert component. (#180)
    * Refactoring and improvement of the alert component.
    
    * default icon added

--- ui/components/PxAlert.vue
+++ ui/components/PxAlert.vue
@@ -2,25 +2,44 @@
   <div>
     <b-alert
       :show="dismissCountDown"
-      dismissible
-      variant="success"
+      class="alert"
+      :class="statusColor"
       @dismissed="dismissCountDown=0"
       @dismiss-count-down="countDownChanged"
     >
-      <p class="m-0">
-        {{ successMsg }}
-      </p>
+      <div class="w-100 d-flex">
+        <div>
+          <Icon
+            :icon="statusIcon"
+            width="22"
+            class="mr-2"
+          />
+        </div>
+        <div class="d-flex flex-column">
+          <p style="font-weight: 600;" class="m-0">
+            {{ status }}
+          </p>
+          <span class="m-0 small">
+            {{ message }}
+          </span>
+        </div>
+      </div>
     </b-alert>
   </div>
 </template>
 <script>
+
 export default {
+
   data () {
     return {
       dismissSecs: 10,
       dismissCountDown: 0,
       showDismissibleAlert: false,
-      successMsg: ''
+      message: '',
+      status: '',
+      statusColor: '',
+      statusIcon: ''
     }
   },
 
@@ -28,9 +47,27 @@ export default {
     countDownChanged (dismissCountDown) {
       this.dismissCountDown = dismissCountDown
     },
-    showAlert (successMsg) {
+    showAlert (status, message) {
       this.dismissCountDown = this.dismissSecs
-      this.successMsg = successMsg
+      this.message = message
+      this.status = status
+
+      const statusColors = {
+        Error: 'error-alert',
+        Warning: 'warning-alert',
+        Info: 'info-alert',
+        Success: 'success-alert'
+      }
+
+      const statusIcon = {
+        Error: 'material-symbols:info-outline-rounded',
+        Warning: 'material-symbols:warning-outline-rounded',
+        Info: 'material-symbols:info-outline-rounded',
+        Success: 'material-symbols:check-circle-outline-rounded'
+      }
+
+      this.statusColor = statusColors[status] || 'info-alert'
+      this.statusIcon = statusIcon[status] || 'material-symbols:info-outline-rounded'
     }
   }
 }
@@ -41,5 +78,25 @@ export default {
     bottom: 0;
     right:0;
     z-index: 2;
+    color: #ffff;
+    border: none;
+    margin: 1rem;
+
+}
+
+.info-alert{
+  background-color:#0282cd;
+}
+
+.success-alert{
+  background-color:#3ed082;
+}
+
+.warning-alert{
+  background-color:#f5b501;
+}
+
+.error-alert{
+  background-color:#ef4114;
 }
 </style>
