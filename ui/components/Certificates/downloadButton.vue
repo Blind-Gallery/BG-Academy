a43ccab39b2e3cba4b8be5ea1b8ee68d49d45795
Tlalocman <desneruda@gmail.com>
2024-01-10 09:22:09 -0600

    🔧 chore(downloadButton.vue): add missing semicolon to improve code consistency 🚀 feat(downloadButton.vue): add functionality to download certificate for a specific course

--- ui/components/Certificates/downloadButton.vue
+++ ui/components/Certificates/downloadButton.vue
@@ -3,3 +3,23 @@
     Download certificate
   </button>
 </template>
+
+<script>
+export default {
+  props: {
+    courseId: {
+      type: Number,
+      required: true
+    }
+  },
+  methods: {
+    async getCertificate () {
+      const { cid } = await this.$axios.$post('/docs/certificate', {
+        userId: this.$auth.user.id,
+        courseId: this.courseId
+      })
+      console.info(cid)
+    }
+  }
+}
+</script>
