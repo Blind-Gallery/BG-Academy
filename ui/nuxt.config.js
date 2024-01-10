bf9931bdba42dcd20e81dd7ea00295984c440ed4
Tlalocman <desneruda@gmail.com>
2024-01-09 18:37:17 -0600

    🐛 fix(stripeGenerate.vue): add courseId prop to stripeGenerate component to pass the course id to the payment request 🔧 chore(nuxt.config.js): update the default baseURL for axios to 'http://localhost:5000/' to match the server port 🔧 chore(_courseId.vue): pass the course id as a prop to the payments-stripe-generate component to ensure correct payment processing for the selected course

--- ui/nuxt.config.js
+++ ui/nuxt.config.js
@@ -77,7 +77,7 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: process.env.BASE_URL || 'http://localhost:3000/'
+    baseURL: process.env.BASE_URL || 'http://localhost:5000/'
   },
 
   apollo: {
