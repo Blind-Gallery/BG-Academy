a29d007b4797b2782f7b21277d62b943fad162a0
David Muñoz Guzmán <datzeldave@gmail.com>
2024-01-22 08:48:24 -0600

    alert added when the user claim a certificate

--- ui/components/Certificates/mintButton.vue
+++ ui/components/Certificates/mintButton.vue
@@ -1,15 +1,28 @@
 <template>
-  <button
-    v-show="$auth?.user?.tezos_info"
-    class="secondary-btn"
-    @click="getCertificate"
-  >
-    Claim certificate
-  </button>
+  <div>
+    <button
+      v-show="$auth?.user?.tezos_info"
+      class="secondary-btn"
+      @click="getCertificate"
+    >
+      <div v-if="loading" class="d-flex align-items-center">
+        <span>Claiming</span> <Icon class="ml-2" icon="eos-icons:bubble-loading" width="1rem" />
+      </div>
+      <div v-else>
+        Claim certificate
+      </div>
+    </button>
+    <PxAlert ref="alert" />
+  </div>
 </template>
 
 <script>
+
+import PxAlert from '../PxAlert.vue'
 export default {
+  components: {
+    PxAlert
+  },
   props: {
     courseId: {
       type: Number,
@@ -30,6 +43,8 @@ export default {
         userId: this.$auth.user.id,
         courseId: this.courseId
       })
+      this.$refs.alert.showAlert('Success', 'You have successfully acquired your certificate')
+      this.loading = false
       console.info(status, opHash)
     }
   }
