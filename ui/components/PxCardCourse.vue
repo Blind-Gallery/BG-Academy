c4ecb4ae21248ba6503ca3e4f990c1839018f96a
David Muñoz Guzmán <datzeldave@gmail.com>
2024-02-01 16:26:38 -0600

    coming courses added in the front page and new prop added to the card course component

--- ui/components/PxCardCourse.vue
+++ ui/components/PxCardCourse.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <div class="card-container shadow-sm rounded border">
+    <div class="card-container shadow-sm rounded border position-relative">
+      <div v-if="comingSoon" class=" coming-soon">
+        <span class="small " style="color: #00b9cd;"><b>Coming soon</b></span>
+      </div>
       <div class="card-data p-4">
         <div class="d-flex align-items-center mb-2">
           <b-avatar style="border: 1px solid #fff;" :src="pfp" size="2rem" class="mr-2" /><span class="small">{{ instructor }}</span>
@@ -11,7 +14,7 @@
             {{ description }}
           </p>
         </div>
-        <div class="d-flex align-items-center">
+        <div v-if="!comingSoon" class="d-flex align-items-center">
           <NuxtLink :to="url">
             <button class="mr-3 secondary-btn-black">
               View course
@@ -54,7 +57,7 @@ export default {
       required: true
     },
 
-    isProgress: {
+    comingSoon: {
       type: Boolean,
       required: false,
       default: false
@@ -97,6 +100,15 @@ export default {
   -webkit-box-orient: vertical;
   overflow: hidden;
 
+}
+
+.coming-soon{
+  z-index: 5;
+  background-color: #fff;
+  right: 0; padding: 0.5rem;
+  border-radius: 0px 0px 0px 10px;
+  position: absolute;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 20px 25px -5px, rgba(0, 0, 0, 0.04) 0px 10px 10px -5px;
 }
 
   </style>
