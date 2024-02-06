ea7cdf539f5182d34e06d613ec6697a4fb0fb11f
David Muñoz Guzmán <davemg71@hotmail.com>
2024-02-06 13:06:22 -0600

    certificate card style changes

--- ui/components/PxCertificate.vue
+++ ui/components/PxCertificate.vue
@@ -1,31 +1,29 @@
 <template>
   <div>
-    <div class="shadow-sm mb-4">
-      <div class="card-body d-flex flex-column align-items-center">
-        <div class="mr-4 w-25">
-          <img width="100%" class="rounded" :src="cover">
-        </div>
-        <div class="d-flex flex-column w-75">
-          <div class="d-flex" style="gap:0.5rem">
-            <h6 style="font-weight: 600;" class="card-title m-0">
-              {{ title }}
-            </h6>
-            <Icon
-              v-b-tooltip.hover
-              title="Course certificate"
-              color="#00b9cd"
-              icon="material-symbols:verified-outline"
-              width="1.25rem"
-            />
-          </div>
-          <p class="text-secondary small">
-            Instructed by {{ instructor }}
-          </p>
-          <certificates-download-button
-            class="mb-2"
-            :course-id="courseId"
+    <div class="card-body shadow-sm rounded d-flex flex-column align-items-center">
+      <div class="w-100">
+        <img width="100%" class="rounded" :src="cover">
+      </div>
+      <div class="d-flex flex-column mt-3">
+        <div class="d-flex" style="gap:0.5rem">
+          <h6 style="font-weight: 600;" class="card-title m-0 text-truncate">
+            {{ title }}
+          </h6>
+          <Icon
+            v-b-tooltip.hover
+            title="Course certificate"
+            color="#00b9cd"
+            icon="material-symbols:verified-outline"
+            width="1.25rem"
           />
         </div>
+        <p class="text-secondary small">
+          Instructed by {{ instructor }}
+        </p>
+        <certificates-download-button
+          class="mb-2"
+          :course-id="courseId"
+        />
       </div>
     </div>
   </div>
