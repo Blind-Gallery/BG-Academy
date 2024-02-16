9d8e5b57003923b772e6985905592991af77d187
crissthiandi <albertocenaa@gmail.com>
2024-02-16 13:36:12 -0600

    Add [id].vue file with dynamic content fetching

---
+++ ui/pages/frames/[id].vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- eslint-disable-next-line vue/no-v-html -->
+  <div v-html="htmlContent" />
+</template>
+<script setup>
+definePageMeta({
+  layout: false,
+});
+const route = useRoute();
+
+const asyncData = async () => {
+  const data = await fetch(
+    `https://frame-academy-dot-curious-domain-369714.uc.r.appspot.com/academy-quiz/${route.params.id}`
+  ).then((res) => res.text());
+  return data;
+};
+const htmlContent = ref("");
+const data = await asyncData();
+htmlContent.value = data;
+</script>
