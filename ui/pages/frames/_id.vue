118af4d44d455da439d1b6bd41c71b90694c6c37
Cristhian Diaz <albertocenaa@gmail.com>
2024-02-19 15:03:40 -0600

    Feature/frames mask (#233)
    * Add [id].vue file with dynamic content fetching
    
    * Refactor frame component to vue 2.7

---
+++ ui/pages/frames/_id.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- eslint-disable-next-line vue/no-v-html -->
+  <div v-html="htmlContent" />
+</template>
+
+<script>
+export default {
+  layout: 'empty',
+  // eslint-disable-next-line space-before-function-paren
+  async asyncData({ params }) {
+    const res = await fetch(
+      `https://frame-academy-dot-curious-domain-369714.uc.r.appspot.com/academy-quiz/${params.id}`
+    ).then(res => res.text())
+    return { htmlContent: res }
+  },
+  // eslint-disable-next-line space-before-function-paren
+  data() {
+    return {
+      htmlContent: ''
+    }
+  }
+
+}
+</script>
