daf1fc3af43ee0542a37b0c3138dd8be4141c116
Datzel <davemg71@hotmail.com>
2024-08-03 10:56:56 -0600

    Hero section made as a component

--- ui/pages/index.vue
+++ ui/pages/index.vue
@@ -2,35 +2,7 @@
   <div>
     <div v-if="!$apollo.loading">
       <!--INTRODUCTION CONTAINER-->
-      <b-container v-if="!$auth.loggedIn" class="main-container pt-5 pb-5" fluid>
-        <b-container style="max-width: 1240px;">
-          <b-row class="align-items-center py-4">
-            <b-col class="d-flex flex-column intro-left-col" style="gap:1rem">
-              <h1>
-                Your starting point to
-                <span style="color:#00B9CD"> learn blockchain art</span>
-              </h1>
-              <p>
-                Explore interactive courses led by industry experts, paving your way in the digital art revolution.
-              </p>
-              <div class="d-flex gap-2">
-                <a style="text-decoration: none;" href="#explore-courses" class="secondary-btn-black">
-                  Explore courses
-                </a>
-              </div>
-            </b-col>
-            <b-col class="d-lg-flex justify-content-end d-none">
-              <div class="w-100" style="border-radius: 5px;overflow: hidden;">
-                <PxPlayer
-                  video-id="898151728"
-                  chapter-id=""
-                  width="100%"
-                />
-              </div>
-            </b-col>
-          </b-row>
-        </b-container>
-      </b-container>
+      <HeroSection v-if="!$auth.loggedIn" />
       <!--INFO CONTAINER-->
 
       <!--LOGGED CONTAINER-->
@@ -489,7 +461,6 @@ import { SwiperCore, Swiper, SwiperSlide } from 'swiper-vue2'
 
 import 'swiper/swiper-bundle.css'
 import { gql } from 'graphql-tag'
-import PxPlayer from '~/components/PxPlayer.vue'
 
 SwiperCore.use([Pagination, Navigation])
 
@@ -550,8 +521,8 @@ export default {
   },
   components: {
     Swiper,
-    SwiperSlide,
-    PxPlayer
+    SwiperSlide
+
   },
 
   data () {
