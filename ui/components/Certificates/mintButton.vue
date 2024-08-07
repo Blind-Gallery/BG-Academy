a72bea7494cc42f9abe301737c54794fc2af9c76
Datzel <davemg71@hotmail.com>
2024-08-06 20:05:51 -0600

    hash operation value  imported inside of the mint btn component

--- ui/components/Certificates/mintButton.vue
+++ ui/components/Certificates/mintButton.vue
@@ -1,27 +1,49 @@
 <template>
-  <button
-    class="secondary-btn"
-    :disabled="loading"
-    @click="getCertificate"
-  >
-    <span v-if="loading">Claiming Certificate...</span>
-    <span v-else>Claim certificate</span>
-  </button>
+  <div class="w-100">
+    <button
+      v-if="!hash"
+      class="secondary-btn w-100"
+      :disabled="loading"
+      @click="getCertificate"
+    >
+      <span v-if="loading">Claiming Certificate...</span>
+      <span v-else>Claim certificate</span>
+    </button>
+
+    <a v-else class="text-decoration-none w-100" :href="`${TZKT_ENDPOINT}/${hash}`" target="_blank">
+      <button class="secondary-btn w-100">
+
+        Check transaction
+
+      </button>
+    </a>
+  </div>
 </template>
 
 <script>
+import { TZKT_ENDPOINT } from '~/constants'
 export default {
   props: {
     courseId: {
       type: Number,
       required: true
+    },
+    hash: {
+      type: Number,
+      required: false,
+      default: null
     }
   },
   data () {
     return {
       loading: false,
-      url: null
+      url: null,
+      TZKT_ENDPOINT: null
     }
+  },
+
+  mounted () {
+    this.TZKT_ENDPOINT = TZKT_ENDPOINT
   },
   methods: {
     async getCertificate () {
