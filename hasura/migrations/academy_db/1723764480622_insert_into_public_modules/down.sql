c905c68e0fa5737042d0e8651482b6dceaa857ec
tlacloc <desneruda@gmail.com>
2024-08-15 17:29:53 -0600

    add missing module

---
+++ hasura/migrations/academy_db/1723764480622_insert_into_public_modules/down.sql
@@ -0,0 +1,1 @@
+DELETE FROM "public"."modules" WHERE "id" = 'dafb0e22-8aac-4098-aab4-100beafef5fa';
---
+++ hasura/migrations/academy_db/1723764572107_insert_into_public_chapters/down.sql
@@ -0,0 +1,1 @@
+DELETE FROM "public"."chapters" WHERE "id" = 'b2b58372-1a97-42b5-ac2e-c04a52fe841a';
