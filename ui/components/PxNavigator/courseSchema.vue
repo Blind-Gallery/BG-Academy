8cfd1c49653dd97fdf03bb5b1af6de897e135cab
Datzel <davemg71@hotmail.com>
2024-08-17 17:49:06 -0600

    test section added

--- ui/components/PxNavigator/courseSchema.vue
+++ ui/components/PxNavigator/courseSchema.vue
@@ -17,6 +17,14 @@
         <span class="tw-text-xs">{{ chapter.title }}</span>
         <span class="tw-text-xs tw-text-gray-500">{{ formattedDuration(chapter.duration) }}</span>
       </NuxtLink>
+      <NuxtLink :to="'/courseNavigator/test/' + chapterModule.id" class="tw-flex tw-flex-col tw-p-4 tw-text-inherit hover:tw-bg-gray-100 tw-ease-in-out tw-duration-200 tw-cursor-pointer hover:tw-no-underline hover:tw-text-inherit">
+        <div class="tw-w-full tw-flex tw-items-center tw-justify-between">
+          <span class="tw-text-xs">Test</span>
+          <Icon icon="material-symbols-light:smart-display-outline-rounded" width="1rem" />
+        </div>
+
+        <span class="tw-text-xs tw-text-gray-500">Total questions: {{ chapterModule.questions.length }}</span>
+      </NuxtLink>
     </div>
   </div>
 </template>
