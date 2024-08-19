f94187c302a3270e0bbf61077f528206670868d2
tlacloc <desneruda@gmail.com>
2024-08-19 09:21:42 -0600

    Fix incorrect answers in Ilithya quiz questions

--- hasura/migrations/academy_db/1724080695618_fix_ilithya_questions/down.sql
+++ hasura/migrations/academy_db/1724080695618_fix_ilithya_questions/down.sql
@@ -1,1 +1,4 @@
+
 DELETE FROM "public"."question_options" WHERE "id" = '4f0867cc-2c57-4a6a-97a3-a94f71048e74';
+
+--- no down migrations
--- hasura/migrations/academy_db/1724080695618_fix_ilithya_questions/up.sql
+++ hasura/migrations/academy_db/1724080695618_fix_ilithya_questions/up.sql
@@ -1,3 +1,4 @@
+
 --- Fixes first question of Ilithya quiz (module 1, question 1)
 UPDATE question_options SET is_correct = false WHERE id = '528a7b25-0046-46b0-a174-2d22e042a3c7';
 
@@ -7,3 +8,4 @@ UPDATE question_options SET is_correct = true WHERE id = '76c83b70-e59f-446a-bf3
 UPDATE question_options SET is_correct = false WHERE id = '82667748-3839-4d86-ad38-d53f1761f03f';
 
 UPDATE question_options SET is_correct = true WHERE id = 'd1dbd325-8197-4a87-a3a5-82508d828a30';
+INSERT INTO "public"."question_options"("id", "question_id", "text", "is_correct") VALUES (E'4f0867cc-2c57-4a6a-97a3-a94f71048e74', E'c882d398-8edc-438b-9c4c-9c28c11fea24', E'Vertex shader', true);
