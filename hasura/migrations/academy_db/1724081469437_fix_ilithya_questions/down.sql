c321ca42c3401c3065045f5ce57f8fb28cb8d104
Tlalocman <desneruda@gmail.com>
2024-08-19 10:03:30 -0600

    Fix incorrect answers in Ilithya quiz questions (#296)
    * Fix incorrect answers in Ilithya quiz questions
    
    * Fix incorrect answers in Ilithya quiz questions
    
    * Fix incorrect answers in Ilithya quiz questions
    
    * Fix incorrect answers in Ilithya quiz questions
    
    * add comments

---
+++ hasura/migrations/academy_db/1724081469437_fix_ilithya_questions/down.sql
@@ -0,0 +1,105 @@
+
+DELETE FROM "public"."question_options" WHERE "id" = 'b97ee09e-804a-4a6e-b373-8b7924f763ed';
+
+DELETE FROM "public"."question_options" WHERE "id" = '6a890636-662e-47d9-a603-80623f4f5a82';
+
+DELETE FROM "public"."question_options" WHERE "id" = '14be3e1c-8d67-407d-a498-e055deec3dcd';
+
+DELETE FROM "public"."question_options" WHERE "id" = '2fb9539e-4c98-49f2-8386-c34175ebc99e';
+
+DELETE FROM "public"."questions" WHERE "id" = '5fa55917-16b7-4d81-a01a-7d93ddd75f5a';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'f1dfa1af-a21a-417d-9a3a-e1ba13966ccf';
+
+DELETE FROM "public"."question_options" WHERE "id" = '3ec9748f-14ba-46ee-b2aa-f56a511dc104';
+
+DELETE FROM "public"."question_options" WHERE "id" = '5b0b26ad-8695-46d5-bd80-932faab6a9ac';
+
+DELETE FROM "public"."question_options" WHERE "id" = '803a3fa5-4b43-4a53-818d-1b8452eda9ac';
+
+DELETE FROM "public"."questions" WHERE "id" = '4243764f-d99e-4d2b-a938-431d9899acbd';
+
+DELETE FROM "public"."question_options" WHERE "id" = '2782007f-0091-4fcc-b0bc-b7600069fbd4';
+
+DELETE FROM "public"."question_options" WHERE "id" = '41a7bd6c-291b-4bce-8251-0fb06563c142';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'dfb16401-fd41-46c0-b180-687e650f520d';
+
+DELETE FROM "public"."question_options" WHERE "id" = '1ae3b768-00f6-405f-99d2-750de209e8fa';
+
+DELETE FROM "public"."questions" WHERE "id" = '584d1ac0-a79f-43b0-a819-591258760a8d';
+
+DELETE FROM "public"."question_options" WHERE "id" = '086ba3bb-64fb-4fb7-9e2e-fd5a999dabe0';
+
+DELETE FROM "public"."question_options" WHERE "id" = '78d67590-4334-4739-860a-9e8958ee4737';
+
+DELETE FROM "public"."question_options" WHERE "id" = '5cfbb300-2b65-4b02-9ea0-e251780689a9';
+
+DELETE FROM "public"."question_options" WHERE "id" = '1ee282d7-30e9-4330-b5df-c05a14be336c';
+
+DELETE FROM "public"."questions" WHERE "id" = '46047732-05e2-47dc-aed9-08f835b62941';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'd279fd96-516a-40e8-9e16-8778319d66df';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'd85159a7-48f6-4db7-a2a3-92b1f752059d';
+
+DELETE FROM "public"."question_options" WHERE "id" = '8c368aaa-1cd6-41ec-a60a-f705682403df';
+
+DELETE FROM "public"."question_options" WHERE "id" = '1f1a1d4e-7f3c-4407-958e-97d601c134b2';
+
+DELETE FROM "public"."questions" WHERE "id" = 'd865cb8f-5be3-44c2-9956-08eefe17ff07';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'd84bb1e2-a3ab-4e48-a300-4576b7d8bc16';
+
+DELETE FROM "public"."question_options" WHERE "id" = '37d4728f-d0f0-4345-aa22-a7878706f717';
+
+DELETE FROM "public"."question_options" WHERE "id" = '5d69d73b-5ce9-452c-a610-0b71804e84ff';
+
+DELETE FROM "public"."question_options" WHERE "id" = '193bb298-da9f-4a8d-b539-cee99cc25b58';
+
+DELETE FROM "public"."questions" WHERE "id" = 'e76ae3c7-fdc9-4fb2-bbcb-6d56ea0d3ea3';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'fc4cd24a-16c5-4e69-9663-4dfaf4ba65b8';
+
+DELETE FROM "public"."question_options" WHERE "id" = '67f595d9-09e7-46fa-b898-8ad634b0b99b';
+
+DELETE FROM "public"."question_options" WHERE "id" = '7abbab78-c35d-48f9-b36f-d6ef9a335766';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'f5727b6f-1820-4df2-92d7-1180d3eefb07';
+
+DELETE FROM "public"."questions" WHERE "id" = '9d43be05-d75e-4775-9f27-769f93837618';
+
+DELETE FROM "public"."question_options" WHERE "id" = '595bdfed-4022-440e-9ee2-b8dc97f09d94';
+
+DELETE FROM "public"."question_options" WHERE "id" = '7ba1e873-e5e7-4587-9c88-f841d3d39918';
+
+DELETE FROM "public"."question_options" WHERE "id" = '61e5ef07-a40a-49c2-add1-52f9f8639b5d';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'aa599411-6275-4be5-bb8d-eefeb0d7f83c';
+
+DELETE FROM "public"."questions" WHERE "id" = '55fc9cdd-85d9-4d78-828f-61b9eace2683';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'e99589df-c91f-45cf-bf84-52a2d40cd0cf';
+
+DELETE FROM "public"."question_options" WHERE "id" = '8d2bdd77-8add-4a50-89db-4c4273dbf004';
+
+DELETE FROM "public"."question_options" WHERE "id" = '6d2bc871-fab5-4076-8d19-a6f4e7b05e26';
+
+DELETE FROM "public"."question_options" WHERE "id" = '3b7bf4fe-8f0b-49b3-8cc4-2de7417e25e4';
+
+DELETE FROM "public"."questions" WHERE "id" = 'edd72c7f-6125-4927-9478-f1de3e35f4ca';
+
+DELETE FROM "public"."question_options" WHERE "id" = '4b8c2f65-6afd-48d0-a9c7-3a3cefb55285';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'ec9da624-f3d3-4db3-b1ac-70ab0db1e444';
+
+DELETE FROM "public"."question_options" WHERE "id" = '08b6f368-3e6d-48f5-b985-10e1d0c80d08';
+
+DELETE FROM "public"."question_options" WHERE "id" = '1022cc63-1dbc-45e1-a8b6-22a38cba1882';
+
+DELETE FROM "public"."questions" WHERE "id" = '0707bba0-d35f-4be6-9de2-e8d5de6e0927';
+
+
+DELETE FROM "public"."question_options" WHERE "id" = '4f0867cc-2c57-4a6a-97a3-a94f71048e74';
+
+--- no down migrations
