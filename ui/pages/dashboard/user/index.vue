81d193ecf143645ff3fa005d73d7b081156de693
crissthiandi <albertocenaa@gmail.com>
2024-08-19 09:58:54 -0600

    Refactor dashboard page to show aggregated statistics by teacher

--- ui/pages/dashboard/user/index.vue
+++ ui/pages/dashboard/user/index.vue
@@ -2,10 +2,10 @@
   <div style="display: flex; flex-direction: column; align-items: center; text-align: center;">
     <h1>Dashboard by teacher</h1>
     <div style="text-align: left;">
-      <p>Number of courses: {{ user_course_aggregate }}</p>
-      <p>Number of users: {{ users_aggregate }}</p>
-      <p>Number of tezos - users: {{ tezos_aggregate }}</p>
-      <p>Number of email - users: {{ emails_aggregate }}</p>
+      <p>Number of courses: {{ courses_aggregate.aggregate.count }}</p>
+      <p>Number of users: {{ users_aggregate.aggregate.count }}</p>
+      <p>Number of tezos - users: {{ tezos_aggregate.aggregate.count }}</p>
+      <p>Number of email - users: {{ emails_aggregate.aggregate.count }}</p>
       <p>Last course bought at: {{ user_course[0].created_at }}</p>
       <p>Number of credit card sales: {{ stripe_transaction_info.length }}</p>
       <p>Number of tezos sales: {{ tezos_transaction_info.length }}</p>
@@ -17,89 +17,92 @@
 
 <script>
 import { gql } from 'graphql-tag'
-
-const apolloQueries = {
-  courses_aggregate: {
-    query: gql`query ($id: String = "") {
-      courses_aggregate(where: {teacher: {id: {_eq: $id}}}) {
-        aggregate {
-          count
-        }
-      }
-    }`,
-    variables: {
-      id: 1 // this.$auth.loggedIn ? this.$auth.user.id : ''
-    }
-  },
-  users_aggregate: {
-    query: gql`query ($id: String = "") {
-      users_aggregate(where: {courses: {course: {teacher_id: {_eq: $id}}}}) {
-        aggregate {
-          count
-        }
-      }
-    }`,
-    variables: {
-      id: 1 // this.$auth.loggedIn ? this.$auth.user.id : ''
-    }
-  },
-  user_course: {
-    query: gql`query {
-      user_course(order_by: {created_at: desc}, limit: 1) {
-        created_at
-      }
-    }`
-  },
-  tezos_aggregate: {
-    query: gql`query {
-      tezos_aggregate {
-        aggregate {
-          count
-        }
-      }
-    }`
-  },
-  emails_aggregate: {
-    query: gql`query {
-      emails_aggregate {
-        aggregate {
-          count
-        }
-      }
-    }`
-  },
-  stripe_transaction_info: {
-    query: gql`query {
-      stripe_transaction_info(where: {amount: {_is_null: false}}) {
-        amount
-        created_at
-        id
-      }
-    }`
-  },
-  tezos_transaction_info: {
-    query: gql`query {
-      tezos_transaction_info(where: {amount: {_is_null: false}}) {
-        amount
-        created_at
-        id
-      }
-    }`
-  }
-}
-
+const userId = "b1c88742-d6a4-4556-8e92-83a7e9374dc5" // this.$auth.loggedIn ? this.$auth.user.id : ''
 export default {
+  apollo: {
+    courses_aggregate: {
+      query: gql`query ($id: String) {
+        courses_aggregate(where: {teacher: {id: {_eq: $id}}}) {
+          aggregate {
+            count
+          }
+        }
+      }`,
+      variables: {
+        id: userId
+      }
+    },
+    users_aggregate: {
+      query: gql`query ($id: String) {
+        users_aggregate(where: {courses: {course: {teacher_id: {_eq: $id}}}}) {
+          aggregate {
+            count
+          }
+        }
+      }`,
+      variables: {
+        id: userId
+      }
+    },
+    user_course: {
+      query: gql`query ($id: String) {
+        user_course(order_by: {created_at: desc}, limit: 1,
+          where: {course: {teacher_id: {_eq: $id}}}) {
+          created_at
+        }
+      }`,
+      variables: {
+        id: userId
+      }
+    },
+    tezos_aggregate: {
+      query: gql`query ($id: String) {
+        tezos_aggregate (where: {user_info: {courses: {course_id: {_eq: $id}}}}) {
+          aggregate {
+            count
+          }
+        }
+      }`,
+      variables: {
+        id: userId
+      }
+    },
+    emails_aggregate: {
+      query: gql`query ($id: String) {
+        emails_aggregate (where: {user_info: {courses: {course_id: {_eq: $id}}}}) {
+          aggregate {
+            count
+          }
+        }
+      }`,
+      variables: {
+        id: userId
+      }
+    },
+    stripe_transaction_info: {
+      query: gql`query {
+        stripe_transaction_info(where: {amount: {_is_null: false}}) {
+          amount
+          created_at
+          id
+        }
+      }`
+    },
+    tezos_transaction_info: {
+      query: gql`query {
+        tezos_transaction_info(where: {amount: {_is_null: false}}) {
+          amount
+          created_at
+          id
+        }
+      }`
+    }
+  },
   data () {
     return {
-      courses_aggregate: 0,
-      user_course_aggregate: 0,
-      users_aggregate: 0,
       total_volume_credit_card: 0,
       total_volume_tezos: 0
     }
-  },
-  created () {
-    this.getStats()
   },
   mounted () {
     this.stripe_transaction_info.forEach((transaction) => {
@@ -108,25 +111,6 @@ export default {
     this.tezos_transaction_info.forEach((transaction) => {
       this.total_volume_tezos += transaction.amount
     })
-  },
-  methods: {
-    getStats () {
-      this.$apollo.query(apolloQueries.courses_aggregate)
-        .then((response) => {
-          this.courses_aggregate = response.data.courses_aggregate.aggregate.count
-        })
-        .catch((error) => {
-          console.error(error)
-        })
-
-      this.$apollo.query(apolloQueries.user_course_aggregate)
-        .then((response) => {
-          this.user_course_aggregate = response.data.user_course_aggregate.aggregate.count
-        })
-        .catch((error) => {
-          console.error(error)
-        })
-    }
   }
 }
 </script>
