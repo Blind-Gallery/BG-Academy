4ac663ff665d85b9d1de153f5f5c7bd740aeaed5
Datzel <davemg71@hotmail.com>
2024-08-22 11:51:16 -0600

    challenge view created

---
+++ ui/pages/courseNavigator/challenge/index.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  data () {
+    return {
+      isUserAllowed: false
+    }
+  },
+
+  methods: {
+    checkUserAccess () {
+      this.isUserAllowed = true
+    }
+  }
+}
+</script>
+<template>
+  <div class="tw-container tw-px-4 tw-mx-auto  lg:tw-container">
+    <div v-if="!isUserAllowed">
+      <challenge-PxInstructions />
+      <div class="tw-flex tw-w-full tw-justify-between tw-items-center tw-flex-wrap">
+        <button-PxSecondary text="Go back" prefix-icon="navigate-before" />
+        <button-PxPrimary text="Continue" suffix-icon="navigate-next" @click="checkUserAccess" />
+      </div>
+    </div>
+    <challenge-PxMint v-else />
+  </div>
+</template>
