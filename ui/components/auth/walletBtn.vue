a4975efebc2fd401b85ef5a204056fc28cea9ee8
Datzel <davemg71@hotmail.com>
2024-08-22 16:01:05 -0600

    wallet btn made with tailwind

--- ui/components/auth/walletBtn.vue
+++ ui/components/auth/walletBtn.vue
@@ -57,15 +57,13 @@ export default {
 <template>
   <div>
     <button
-      class="secondary-btn"
-      style="width: 100%; position: relative; margin-bottom: 1rem"
+      class="tw-rounded tw-border tw-text-cyan-500 tw-p-2 tw-border-cyan-500 hover:tw-border-cyan-600 tw-ease-in-out tw-duration-200 hover:tw-text-cyan-600 tw-w-full"
       @click="walletConnect"
     >
       <Icon
         icon="material-symbols:account-balance-wallet-outline"
-        color="#00b9cd"
+        class="text-cyan-500"
         width="21"
-        style="position: absolute; left: 24px; top: 9px"
       />
       Connect Wallet
     </button>
