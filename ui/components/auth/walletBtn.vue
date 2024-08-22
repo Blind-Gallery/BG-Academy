4aa2b20a98d25a78c29c3fc7530c7290477794e9
Datzel <davemg71@hotmail.com>
2024-08-22 15:14:09 -0600

    script from the wallet btn migrated inside of the walletBtn component

--- ui/components/auth/walletBtn.vue
+++ ui/components/auth/walletBtn.vue
@@ -1,3 +1,59 @@
+<script>
+import { dappClient } from '~/services/tezos'
+export default {
+  data () {
+    return {
+      walletForm: {}
+    }
+  },
+
+  methods: {
+    async walletConnect () {
+      try {
+        const data = await this.getWalletAccessData()
+        await this.$auth.loginWith('local', {
+          data
+        })
+        const { disconnectWallet, checkIfWalletIsConnected } = dappClient()
+        const { connected: isWalletConnected } =
+          await checkIfWalletIsConnected()
+        if (isWalletConnected && !this.$auth.loggedIn) {
+          await disconnectWallet()
+        }
+
+        this.$bvModal.hide('signin')
+      } catch (error) {
+        if (error.response && error.response.status === 401) {
+          console.info(error)
+        }
+      }
+    },
+
+    async getWalletAccessData () {
+      const { connectAccount, requestLoginSignPayload } = dappClient()
+      const wallet = await connectAccount()
+      if (!wallet.success) {
+        console.error('Wallet not connected')
+      }
+      const {
+        publicKey,
+        wallet: tezosAddress,
+        signedPayload: signedMessage,
+        payload
+      } = await requestLoginSignPayload()
+      const data = {
+        ...this.walletForm,
+        publicKey,
+        wallet: tezosAddress,
+        signedMessage,
+        payload
+      }
+      return data
+    }
+  }
+}
+</script>
+
 <template>
   <div>
     <button
