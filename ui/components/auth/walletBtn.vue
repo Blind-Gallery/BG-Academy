7fe7d86936787324fbeadacf2422d811ada9b27d
Datzel <davemg71@hotmail.com>
2024-08-22 15:13:04 -0600

    wallet element migrated inside of the walletBtn component

--- ui/components/auth/walletBtn.vue
+++ ui/components/auth/walletBtn.vue
@@ -1,3 +1,17 @@
 <template>
-  <div>Wallet</div>
+  <div>
+    <button
+      class="secondary-btn"
+      style="width: 100%; position: relative; margin-bottom: 1rem"
+      @click="walletConnect"
+    >
+      <Icon
+        icon="material-symbols:account-balance-wallet-outline"
+        color="#00b9cd"
+        width="21"
+        style="position: absolute; left: 24px; top: 9px"
+      />
+      Connect Wallet
+    </button>
+  </div>
 </template>
