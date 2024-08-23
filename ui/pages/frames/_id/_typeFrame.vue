a03aa73f016f2700b9e0d56ad32cff9e7f3c27df
crissthiandi <albertocenaa@gmail.com>
2024-08-23 09:17:57 -0600

    feat: Update asyncData function to include typeFrame parameter

--- ui/pages/frames/_id/_typeFrame.vue
+++ ui/pages/frames/_id/_typeFrame.vue
@@ -8,8 +8,9 @@ export default {
   layout: 'empty',
   // eslint-disable-next-line space-before-function-paren
   async asyncData({ params }) {
+    const typeFrame = params?.typeFrame ?? 'academy-quiz'
     const res = await fetch(
-      `https://frame-academy-dot-curious-domain-369714.uc.r.appspot.com/academy-quiz/${params.id}`
+      `https://frame-academy-dot-curious-domain-369714.uc.r.appspot.com/${typeFrame}/${params.id}`
     ).then(res => res.text())
     return { htmlContent: res }
   },
