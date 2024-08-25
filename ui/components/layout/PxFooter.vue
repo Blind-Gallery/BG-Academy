6b36a6542cf780c710d6b7fb67fae8eea73b8ae7
Datzel <davemg71@hotmail.com>
2024-08-25 12:02:12 -0600

    socials updated

--- ui/components/layout/PxFooter.vue
+++ ui/components/layout/PxFooter.vue
@@ -16,28 +16,28 @@
           We are here to answer your questions, join our discord and ask anything!
         </p>
         <div class="tw-flex tw-items-center tw-gap-2">
-          <NuxtLink
-            to="https://warpcast.com/blind-gallery"
+          <a
+            href="https://warpcast.com/blind-gallery"
             target="_blank"
             class="tw-flex tw-items-center tw-justify-center tw-bg-cyan-500 tw-rounded-full tw-w-[20px] tw-h-[20px] tw-p-1 tw-text-white hover:tw-text-white hover:tw-bg-cyan-600 tw-ease-in-out tw-duration-200"
           >
             <Icon icon="simple-icons:farcaster" size="1rem" />
-          </NuxtLink>
-          <NuxtLink
-            to="https://discord.com/invite/zxxZv6HUfr"
+          </a>
+          <a
+            href="https://discord.com/invite/zxxZv6HUfr"
             target="_blank"
             class="tw-flex tw-items-center tw-justify-center tw-bg-cyan-500 tw-rounded-full tw-w-[20px] tw-h-[20px] tw-p-1 tw-text-white hover:tw-text-white hover:tw-bg-cyan-600 tw-ease-in-out tw-duration-200"
           >
             <Icon icon="ic:baseline-discord" size="1.25rem" />
-          </NuxtLink>
+          </a>
 
-          <NuxtLink
-            to="https://x.com/BlindGallery_"
+          <a
+            href="https://x.com/BlindGallery_"
             target="_blank"
             class="tw-flex tw-items-center tw-justify-center tw-bg-cyan-500 tw-rounded-full tw-w-[20px] tw-h-[20px] tw-p-1 tw-text-white hover:tw-text-white hover:tw-bg-cyan-600 tw-ease-in-out tw-duration-200"
           >
             <Icon icon="ri:twitter-x-fill" size="1rem" />
-          </NuxtLink>
+          </a>
         </div>
       </div>
     </div>
