966cb14d30191830b6fa858ab979a49db5bd81fb
Datzel <davemg71@hotmail.com>
2024-08-25 11:39:55 -0600

    footer made as a component

---
+++ ui/components/layout/PxFooter.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div class="tw-container lg:tw-flex tw-items-center tw-justify-between tw-py-6">
+      <div class="tw-flex tw-flex-col tw-border-b-2 tw-pb-2 lg:tw-pb-0 lg:tw-border-b-0">
+        <NuxtLink to="/" class="tw-mb-4">
+          <img src="https://moccasin-perfect-trout-941.mypinata.cloud/ipfs/QmdttBPgdS8ERmJQgacHire1y2F8uHJLEm6oNmSx9yisbV" width="110px">
+        </NuxtLink>
+        <span class="tw-text-xs tw-text-gray-500">© 2024 Blind Gallery Academy. All rights reserved.</span>
+        <span class="tw-text-xs tw-text-gray-500">Powered by Tezos.</span>
+      </div>
+      <div>
+        <h6 class="tw-font-bold tw-text-xs">
+          Do you need help?
+        </h6>
+        <p class="tw-text-xs tw-text-gray-500">
+          We are here to answer your questions, join our discord and ask anything!
+        </p>
+        <div class="tw-flex tw-items-center tw-gap-2">
+          <NuxtLink
+            to="https://warpcast.com/blind-gallery"
+            target="_blank"
+            class="tw-flex tw-items-center tw-justify-center tw-bg-cyan-500 tw-rounded-full tw-w-[20px] tw-h-[20px] tw-p-1 tw-text-white hover:tw-text-white hover:tw-bg-cyan-600 tw-ease-in-out tw-duration-200"
+          >
+            <Icon icon="simple-icons:farcaster" size="1rem" />
+          </NuxtLink>
+          <NuxtLink
+            to="https://discord.com/invite/zxxZv6HUfr"
+            target="_blank"
+            class="tw-flex tw-items-center tw-justify-center tw-bg-cyan-500 tw-rounded-full tw-w-[20px] tw-h-[20px] tw-p-1 tw-text-white hover:tw-text-white hover:tw-bg-cyan-600 tw-ease-in-out tw-duration-200"
+          >
+            <Icon icon="ic:baseline-discord" size="1.25rem" />
+          </NuxtLink>
+
+          <NuxtLink
+            to="https://x.com/BlindGallery_"
+            target="_blank"
+            class="tw-flex tw-items-center tw-justify-center tw-bg-cyan-500 tw-rounded-full tw-w-[20px] tw-h-[20px] tw-p-1 tw-text-white hover:tw-text-white hover:tw-bg-cyan-600 tw-ease-in-out tw-duration-200"
+          >
+            <Icon icon="ri:twitter-x-fill" size="1rem" />
+          </NuxtLink>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
