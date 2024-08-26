8535d649399712aafd4a5ee4ee340a3bab8f6d43
tlacloc <desneruda@gmail.com>
2024-08-26 10:17:49 -0600

    feat: Update resources for chapters in academy_db
    This commit updates the resources for several chapters in the academy_db. The resources URLs have been modified to include relevant links for each chapter.

---
+++ hasura/migrations/academy_db/1724686582628_update_resources_text/up.sql
@@ -0,0 +1,92 @@
+UPDATE chapters
+SET resources = '[Wikipedia: Polygon Mesh](https://en.wikipedia.org/wiki/Polygon_mesh)\n[Mythbusters Demo GPU versus CPU](https://www.youtube.com/watch?v=-P28LKWTzrI)\n[Khronos OpenGL Wiki: Core Language (GLSL)](https://www.khronos.org/opengl/wiki/Core_Language_(GLSL))'
+WHERE id = 'b127c9e1-698c-4cd7-a534-c41c87b1893b';
+
+UPDATE chapters
+SET resources = '[Khronos OpenGL Wiki: Fragment Shader](https://www.khronos.org/opengl/wiki/Fragment_Shader)\n[Khronos OpenGL Wiki: Core Language (GLSL)](https://www.khronos.org/opengl/wiki/Core_Language_(GLSL))\n[Khronos OpenGL](https://www.khronos.org/)\n[Graphics Programming Compendium](https://graphicscompendium.com/)\n[Graphics Programming Compendium: The Graphics Pipeline](https://graphicscompendium.com/intro/01-graphics-pipeline#the-graphics-pipeline)'
+WHERE id = '1631e746-0efc-4e49-becb-6a0c6c44a749';
+
+UPDATE chapters
+SET resources = '[WebGL Fundamentals: WebGL Precision Issues](https://webglfundamentals.org/webgl/lessons/webgl-precision-issues.html)\n[Khronos OpenGL Wiki: Type Qualifier (GLSL)](https://www.khronos.org/opengl/wiki/Type_Qualifier_(GLSL))\n[Khronos OpenGL Wiki: Built-in Variable (GLSL)](https://www.khronos.org/opengl/wiki/Built-in_Variable_(GLSL))\n[Khronos OpenGL Wiki: Data Type (GLSL)](https://www.khronos.org/opengl/wiki/Data_Type_(GLSL))'
+WHERE id = 'e7387a97-af99-4976-87b9-6510235c89d8';
+
+
+UPDATE chapters
+SET resources = '[Wikipedia: UV Mapping](https://en.wikipedia.org/wiki/UV_mapping)'
+WHERE id = '9c294d3f-4df5-4532-bb81-ce6621b8dc13';
+
+UPDATE chapters
+SET resources = '[Wikipedia: Additive Color](https://en.wikipedia.org/wiki/Additive_color)\n[Shader Template (using Three.js v.166.0)](https://codepen.io/ilithya/pen/wvOLPZr)\n[Shader Template Gist](https://gist.github.com/ilithya/4c4415c013b2d18e6c83bbc34c2e47e8)\n[ColorKit Color Picker](https://colorkit.co/color/91eab9/)'
+WHERE id = 'ea905e11-a99c-4148-8705-338779763f35';
+
+UPDATE chapters
+SET resources = '[Shader Template (using Three.js v.166.0)](https://codepen.io/ilithya/pen/wvOLPZr)'
+WHERE id = 'e22d8840-239e-42c9-b6f7-dbc76a656b1a';
+
+UPDATE chapters
+SET resources = '[Shader Template (using Three.js v.166.0)](https://codepen.io/ilithya/pen/wvOLPZr)\n[The Book of Shaders Glossary](https://thebookofshaders.com/glossary/)\n[The Book of Shaders Glossary: mix()](https://thebookofshaders.com/glossary/?search=mix)'
+WHERE id = '40cbea88-12fd-4e7f-a374-6cf8440d9603';
+
+UPDATE chapters
+SET resources = '[Shader Template (using Three.js v.166.0)](https://codepen.io/ilithya/pen/wvOLPZr)'
+WHERE id = 'e982fbb2-7c2b-48a8-969b-7d7fbcc313f4';
+
+UPDATE chapters
+SET resources = '[Shader Template (using Three.js v.166.0)](https://codepen.io/ilithya/pen/wvOLPZr)\n[The Book of Shaders Glossary: length()](https://thebookofshaders.com/glossary/?search=length)\n[The Book of Shaders Glossary: step()](https://thebookofshaders.com/glossary/?search=step)'
+WHERE id = '0e6008a4-5402-41ad-bec4-7d4237e46400';
+
+UPDATE chapters
+SET resources = '[The Book of Shaders Glossary: distance()](https://thebookofshaders.com/glossary/?search=distance)\n[Khronos GLSL Functions: distance](https://registry.khronos.org/OpenGL-Refpages/gl4/html/distance.xhtml)'
+WHERE id = '974053b1-077c-4fca-bc2c-131c9bc5fcb5';
+
+UPDATE chapters
+SET resources = '[The Book of Shaders Glossary: smoothstep()](https://thebookofshaders.com/glossary/?search=smoothstep)'
+WHERE id = '3c0d4bf4-bd5d-4bb6-bf58-730e0d242ce6';
+
+UPDATE chapters
+SET resources = '[Shader Template (using Three.js v.166.0)](https://codepen.io/ilithya/pen/wvOLPZr)\n[The Book of Shaders Glossary: abs()](https://thebookofshaders.com/glossary/?search=abs)\n[The Book of Shaders Glossary: max()](https://thebookofshaders.com/glossary/?search=max)\n[Khronos GLSL Functions: max](https://registry.khronos.org/OpenGL-Refpages/gl4/html/max.xhtml)'
+WHERE id = 'e7a4c97a-619e-4cda-9d9f-fb080a6bd23b';
+
+UPDATE chapters
+SET resources = '[The Book of Shaders Glossary: min()](https://thebookofshaders.com/glossary/?search=min)\n[The Book of Shaders Glossary: max()](https://thebookofshaders.com/glossary/?search=max)\n[The Book of Shaders Glossary: smoothstep()](https://thebookofshaders.com/glossary/?search=smoothstep)\n[The Book of Shaders Glossary: step()](https://thebookofshaders.com/glossary/?search=step)'
+WHERE id = 'b57e8b32-780f-47e5-9a9d-3a12ee6a7fd6';
+
+UPDATE chapters
+SET resources = '[Shader Template (using Three.js v.166.0)](https://codepen.io/ilithya/pen/wvOLPZr)'
+WHERE id = '899ecea6-9c2d-4779-9677-81057dafbb72';
+
+UPDATE chapters
+SET resources = '[Shader Template (using Three.js v.166.0)](https://codepen.io/ilithya/pen/wvOLPZr)\n[The Book of Shaders Glossary: abs()](https://thebookofshaders.com/glossary/?search=abs)'
+WHERE id = '04847452-264b-4bed-be37-ac2ce8b7c316';
+
+UPDATE chapters
+SET resources = '[Shader Template (using Three.js v.166.0)](https://codepen.io/ilithya/pen/wvOLPZr)\n[The Book of Shaders Glossary: fract()](https://thebookofshaders.com/glossary/?search=fract)\n[The Book of Shaders Glossary: mod()](https://thebookofshaders.com/glossary/?search=mod)'
+WHERE id = '5fc650d0-8c74-4454-90fd-bf169dd740f5';
+
+UPDATE chapters
+SET resources = '[Shader Template (using Three.js v.166.0)](https://codepen.io/ilithya/pen/wvOLPZr)'
+WHERE id = '4651437f-2d0a-4b0c-9fb3-0cbcf517775d';
+
+UPDATE chapters
+SET resources = '[Shader Template (using Three.js v.166.0)](https://codepen.io/ilithya/pen/wvOLPZr)'
+WHERE id = '6156514a-9bb8-4b88-bbe7-117641d5ae28';
+
+UPDATE chapters
+SET resources = '[Shader Template (using Three.js v.166.0)](https://codepen.io/ilithya/pen/wvOLPZr)'
+WHERE id = '1c3463e5-8774-46b2-9b41-8b6c85fb2650';
+
+UPDATE chapters
+SET resources = '[Shader Template (using Three.js v.166.0)](https://codepen.io/ilithya/pen/wvOLPZr)\n[Three.js Documentation: Clock](https://threejs.org/docs/index.html?q=clock#api/en/core/Clock)'
+WHERE id = '22560dcf-970c-4e46-8a8c-c8cdc4dcb5ff';
+
+UPDATE chapters
+SET resources = '[Shader Template (using Three.js v.166.0)](https://codepen.io/ilithya/pen/wvOLPZr)\n[The Book of Shaders Glossary: sin()](https://thebookofshaders.com/glossary/?search=sin)  \n[The Book of Shaders Glossary: cos()](https://thebookofshaders.com/glossary/?search=cos)  \n[The Book of Shaders Glossary: tan()](https://thebookofshaders.com/glossary/?search=tan)'
+WHERE id = 'c33a7b2f-e979-4247-888a-da13f03a3b06';
+
+UPDATE chapters
+SET resources = '[The Book of Shaders Glossary: clamp()](https://thebookofshaders.com/glossary/?search=clamp)  \n[The Book of Shaders Glossary: mix()](https://thebookofshaders.com/glossary/?search=mix)'
+WHERE id = 'cd1f2792-0139-4e4b-b72d-0c1f0b6bcb43';
+
+UPDATE chapters
+SET resources = '[#anydayshaders on Instagram](https://www.instagram.com/explore/tags/anydayshaders/)  \n[#anydayshaders on Twitter (X)](https://x.com/search?q=%23anydayshaders&src=hashtag_click&f=live)  \n[#anydayshaders](https://www.ilithya.rocks/blog/anydayshaders/)'
+WHERE id = 'b2b58372-1a97-42b5-ac2e-c04a52fe841a';
