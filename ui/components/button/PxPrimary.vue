bd95052f9e6d319128d83c1630943023ac5b0c91
David Muñoz Guzmán <108280181+Datzel@users.noreply.github.com>
2024-08-26 14:17:22 -0600

    Refactor/layout (#326)
    * margin added in the modal
    
    * width prop added in the btn components
    
    * tertiary btn componente, created
    
    * unnecesary elements removed from the DOM
    
    * unnecesary functions and variables removed from the script
    
    * header made as a component
    
    * footer made as a component
    
    * initial footer and alert removed from the layout
    
    * socials updated
    
    * unnecesary function removed from layout
    
    * profile header component created
    
    * validations added to display the profile componente in header

--- ui/components/button/PxPrimary.vue
+++ ui/components/button/PxPrimary.vue
@@ -5,6 +5,11 @@ export default {
       type: String,
       required: true
     },
+    width: {
+      type: String,
+      required: false,
+      default: null
+    },
     suffixIcon: {
       type: String,
       required: false,
@@ -21,7 +26,7 @@ export default {
 
 <template>
   <div>
-    <button class="tw-flex tw-min-w-40 tw-gap-1 tw-items-center tw-justify-center tw-text-sm tw-text-white tw-bg-cyan-500 tw-rounded tw-p-2 hover:tw-bg-cyan-600 tw-ease-in-out tw-duration-200" @click="$emit('click')">
+    <button type="button" :class="width" class="tw-flex tw-border   tw-gap-1 tw-border-cyan-500 tw-items-center tw-justify-center tw-text-sm tw-text-white tw-bg-cyan-500 tw-rounded tw-py-2 tw-px-6 hover:tw-bg-cyan-600 hover:tw-border-cyan-600 tw-ease-in-out tw-duration-200" @click="$emit('click')">
       <Icon v-if="prefixIcon" :icon="`material-symbols-light:${prefixIcon}`" width="1.5rem" />
       <span>{{ text }}</span>
       <div v-if="suffixIcon">
--- ui/components/button/PxTertiary.vue
+++ ui/components/button/PxTertiary.vue
@@ -26,7 +26,7 @@ export default {
 
 <template>
   <div>
-    <button type="button" :class="width" class="tw-flex  tw-gap-1 tw-items-center tw-justify-center tw-text-sm tw-text-cyan-500 tw-border  tw-border-cyan-500 tw-rounded tw-py-2 tw-px-6 hover:tw-border-cyan-600  hover:tw-text-cyan-600 tw-ease-in-out tw-duration-200" @click="$emit('click')">
+    <button type="button" :class="width" class="tw-flex  tw-gap-1 tw-items-center tw-justify-center tw-text-sm tw-text-gray-500  tw-py-2 tw-px-6 hover:tw-border-cyan-600  hover:tw-text-gray-900 tw-ease-in-out tw-duration-200 tw-underline tw-underline-offset-1" @click="$emit('click')">
       <Icon v-if="prefixIcon" :icon="`material-symbols-light:${prefixIcon}`" width="1.5rem" />
       <span>{{ text }}</span>
       <div v-if="suffixIcon">
