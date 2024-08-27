ff86286d42f02d4040706ae5363e8d516bb3a14e
crissthiandi <albertocenaa@gmail.com>
2024-08-27 11:34:05 -0600

    feat: Add second module and update module dependencies

--- hasura/migrations/academy_db/1724778742864_add_heey_modules/down.sql
+++ hasura/migrations/academy_db/1724778742864_add_heey_modules/down.sql
@@ -2,8 +2,3 @@
 DELETE FROM "public"."modules" WHERE "id" = 'c5a014db-7995-4cb6-8514-2377f5d47988';
 
 DELETE FROM "public"."modules" WHERE "id" = '4674105d-8dca-49d6-a705-512e66db9dfd';
-
-
-DELETE FROM "public"."modules" WHERE "id" = '4c02988e-e748-4fab-b0e4-fbcb50d063b9';
-
-DELETE FROM "public"."modules" WHERE "id" = '3c863d7e-cf4c-4dfd-8c98-fa117cee3908';
