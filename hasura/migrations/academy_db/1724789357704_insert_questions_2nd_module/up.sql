eb8511a096fae99ca282a7035679c79069f4086d
tlacloc <desneruda@gmail.com>
2024-08-27 14:10:41 -0600

    feat: Add questions for the 2nd module in the generative art course

---
+++ hasura/migrations/academy_db/1724789357704_insert_questions_2nd_module/up.sql
@@ -0,0 +1,50 @@
+---- insert questions for the 2nd module
+INSERT INTO "public"."questions"("text", "answer_id", "id", "module_id") VALUES (E'Who is credited with creating the earliest form of electronic art, known as "Oscillons"?', null, E'54c6666b-eabc-4dbb-b3db-215cf1bfb02a', E'4c02988e-e748-4fab-b0e4-fbcb50d063b9');
+
+INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (false, E'Frieder Nake', E'a30806fb-e1c1-4fd2-b30a-bc1a7db6f676', E'54c6666b-eabc-4dbb-b3db-215cf1bfb02a');
+
+INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (true, E'Ben Laposky', E'bef6e14b-f873-4968-ac02-0d7b0ec21bd1', E'54c6666b-eabc-4dbb-b3db-215cf1bfb02a');
+
+INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (false, E'Vera Molnar', E'f0cb242d-7e76-49c2-a0ee-da2cd82599fc', E'54c6666b-eabc-4dbb-b3db-215cf1bfb02a');
+
+INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (false, E'John Whitney', E'b7607fad-1a77-4267-8faa-5728f76a8561', E'54c6666b-eabc-4dbb-b3db-215cf1bfb02a');
+
+INSERT INTO "public"."questions"("text", "answer_id", "id", "module_id") VALUES (E'What did the Surrealists emphasize that laid the groundwork for generative art?', null, E'2ca666af-fccf-480b-8916-a6272270ca86', E'4c02988e-e748-4fab-b0e4-fbcb50d063b9');
+
+INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (false, E'Realism in art and detailed landscapes.', E'95850176-d8d1-4097-89be-5953129e5953', E'2ca666af-fccf-480b-8916-a6272270ca86');
+
+INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (false, E'The use of vivid colors and impressionistic techniques.', E'e651941a-3d0b-4169-ad53-51648d3fa493', E'2ca666af-fccf-480b-8916-a6272270ca86');
+
+INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (true, E'Spontaneous, unfiltered expression and the potential of chance and randomness. ', E'c7673b05-5438-49df-beed-f0eda3a024a8', E'2ca666af-fccf-480b-8916-a6272270ca86');
+
+INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (false, E'Strict adherence to traditional artistic techniques.', E'cef4ab48-1ff0-47ae-affe-048ea500151e', E'2ca666af-fccf-480b-8916-a6272270ca86');
+
+INSERT INTO "public"."questions"("text", "answer_id", "id", "module_id") VALUES (E'Who coined the term "Algorists" and what does it refer to?', null, E'5b0720f3-0eb0-4be9-9e9e-fae8eedf70a7', E'4c02988e-e748-4fab-b0e4-fbcb50d063b9');
+
+INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (true, E'Jean-Pierre Hébert; artists who use algorithms as a primary means of creating art. ', E'65b993e0-747d-49d9-ab9b-d98d9d2c7bc9', E'5b0720f3-0eb0-4be9-9e9e-fae8eedf70a7');
+
+INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (false, E'William Latham; artists who use evolutionary principles in art.', E'215b59a7-2eb4-4743-b4f6-a82da20c2765', E'5b0720f3-0eb0-4be9-9e9e-fae8eedf70a7');
+
+INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (false, E'Harold Cohen; artists who use AI to generate creative content.', E'1c6c2dc5-94c0-4f80-8889-0189f56ccd7b', E'5b0720f3-0eb0-4be9-9e9e-fae8eedf70a7');
+
+INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (false, E'Muriel Cooper; designers focusing on digital and interactive design.', E'8ad9e437-f2f1-4440-bc1c-a9286b980efd', E'5b0720f3-0eb0-4be9-9e9e-fae8eedf70a7');
+
+INSERT INTO "public"."questions"("text", "answer_id", "id", "module_id") VALUES (E'Which artist is known for the creation of the AARON program, a system designed to autonomously produce art?', null, E'f711d2d3-1505-45e0-a569-105fa9424bf7', E'4c02988e-e748-4fab-b0e4-fbcb50d063b9');
+
+INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (false, E'Joan Truckenbrod.', E'd26b461b-2c27-4921-9008-617018f4b350', E'f711d2d3-1505-45e0-a569-105fa9424bf7');
+
+INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (false, E'Manfred Mohr.', E'f16c1061-2e70-48b0-b0b3-515c171000aa', E'f711d2d3-1505-45e0-a569-105fa9424bf7');
+
+INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (true, E'Harold Cohen.', E'a0e52779-0aa2-4e46-afa4-f5a50d1961fb', E'f711d2d3-1505-45e0-a569-105fa9424bf7');
+
+INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (false, E'Georg Nees.', E'd637794c-d95c-4925-acc8-5c66fb556464', E'f711d2d3-1505-45e0-a569-105fa9424bf7');
+
+INSERT INTO "public"."questions"("text", "answer_id", "id", "module_id") VALUES (E'Which artist is known for creating the series "Des Ordres", exploring the tension between order and disorder in geometric compositions?', null, E'e464aa98-3890-4b0d-951c-3b6258fab660', E'4c02988e-e748-4fab-b0e4-fbcb50d063b9');
+
+INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (false, E'Lillian Schwartz.', E'42338df8-5623-4374-9c8b-f5ade3878ceb', E'e464aa98-3890-4b0d-951c-3b6258fab660');
+
+INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (false, E'John Whitney.', E'5b4e8d5e-4451-4d78-a51e-f5be201a8891', E'e464aa98-3890-4b0d-951c-3b6258fab660');
+
+INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (false, E'Roman Verostko.', E'6d8fa05e-8eab-449f-846c-8f30223eca05', E'e464aa98-3890-4b0d-951c-3b6258fab660');
+
+INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (true, E'Vera Molnar.', E'e8e7f0da-fe43-43a9-8dea-2bc8cf072e6d', E'e464aa98-3890-4b0d-951c-3b6258fab660');
