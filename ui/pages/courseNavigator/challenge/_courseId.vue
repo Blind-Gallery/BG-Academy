b4ade089a8d79c66c9182182a3460a13c340ce2d
Datzel <davemg71@hotmail.com>
2024-08-26 18:50:40 -0600

    challenge view renamed with the courseId

--- ui/pages/courseNavigator/challenge/_courseId.vue
+++ ui/pages/courseNavigator/challenge/_courseId.vue
@@ -12,7 +12,7 @@
               Go to <a href="https://objkt.com/" target="_blank">objkt.com</a>
             </h6>
             <p class="tw-text-xs tw-text-gray-500">
-              Connect your wallet to the site, if you don't have one download the extension <a href="https://www.templewallet.com/" target="_blank">here</a> and create it.
+              Connect your wallet to the site, if you don't have one, you can create one <a href="https://www.templewallet.com/" target="_blank">here.</a>
             </p>
           </div>
         </div>
@@ -34,7 +34,7 @@
               Fill the form
             </h6>
             <p class="tw-text-xs tw-text-gray-500">
-              Fill in the required fields and upload your project files.
+              Fill in the required fields and upload your project file.
             </p>
           </div>
         </div>
@@ -42,18 +42,14 @@
           <div class=" tw-w-full tw-flex  tw-border tw-flex-col tw-gap-2 tw-rounded tw-shadow-sm tw-border-l-4 tw-border-l-cyan-500 tw-p-4 tw-h-[200px]">
             <div><Icon style="color:#00B9CD" icon="material-symbols-light:checkbook-outline-rounded" width="2.5rem" /></div>
             <h6 class="tw-font-bold ">
-              Mint your project
+              Mint your shader
             </h6>
             <p class="tw-text-xs tw-text-gray-500">
-              If all the data have been filled in correctly, you will be able to sign the transaction in order to mint your project.
+              Sign in the transactions using your wallet to mint (upload) your shader to the blockchain.
             </p>
           </div>
         </div>
       </div>
-    </div>
-
-    <div class="tw-flex tw-w-full tw-justify-between tw-items-center tw-flex-wrap">
-      <button-PxSecondary text="Go back" prefix-icon="navigate-before" />
     </div>
   </div>
 </template>
