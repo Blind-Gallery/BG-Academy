2161a656c7bc42424d2be7e4f99f9a8ece9563a8
tlacloc <desneruda@gmail.com>
2024-08-27 13:08:19 -0600

    feat: add chapters from the 3rd module

---
+++ hasura/migrations/academy_db/1724785633159_insert_chapters_3rd_module/down.sql
@@ -0,0 +1,6 @@
+
+DELETE FROM "public"."chapters" WHERE "id" = '002542e0-6865-4556-b6d0-5723bbd788de';
+
+DELETE FROM "public"."chapters" WHERE "id" = '0a842cc9-30b1-443a-a5b1-ec8e623889da';
+
+DELETE FROM "public"."chapters" WHERE "id" = 'c5f8d254-8e7c-44b8-908d-5193d7125eb5';
