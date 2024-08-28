d5cc16e2eaad2a7f6d5db42510607fbe9c1a80c8
Cristhian Diaz <albertocenaa@gmail.com>
2024-08-28 12:07:07 -0500

    feat: Update questions and options for first module (#346)
    * Delete chapters from first module
    
    * feat: Add questions and options for first module
    
    * feat: Remove unnecessary question options from first module
    
    * feat: Update question options for first module
    
    * feat: Insert questions and options for first module
    
    * feat: Insert questions and options for first module
    
    * feat: Remove unnecessary question options from first module
    
    * feat: Update question options for first module

---
+++ hasura/migrations/academy_db/1724805566779_add_questions_first_module/down.sql
@@ -0,0 +1,51 @@
+
+DELETE FROM "public"."question_options" WHERE "id" = '75bac02d-c6fb-436b-adf3-6c125d0716fd';
+
+DELETE FROM "public"."question_options" WHERE "id" = '2841a1ec-d8d7-4739-811e-46773e610ce5';
+
+DELETE FROM "public"."question_options" WHERE "id" = '659df12c-5c2f-4003-8e87-cc7c432fb390';
+
+DELETE FROM "public"."question_options" WHERE "id" = '744049cc-a7fe-4709-84f7-bffe73cd3382';
+
+DELETE FROM "public"."questions" WHERE "id" = '3817ebd1-a481-4e59-b510-a84cd2d2e333';
+
+DELETE FROM "public"."question_options" WHERE "id" = '64242362-6f11-42ab-9295-97089fbf597c';
+
+DELETE FROM "public"."question_options" WHERE "id" = '55c6f583-41e8-49c5-8bbb-60bd8b58351e';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'd20d1fd5-5098-4524-880a-6c7e081fc965';
+
+DELETE FROM "public"."question_options" WHERE "id" = '8570d3a8-f18d-409a-931a-b1908a08e08c';
+
+DELETE FROM "public"."questions" WHERE "id" = 'c1b7a959-3fa9-493b-a804-e0e5a59ea68f';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'f828a2d6-e986-4804-9438-4ff1a5586dcd';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'd5665a2b-ec2b-4cfb-8591-90d0991e3e9e';
+
+DELETE FROM "public"."question_options" WHERE "id" = '559ed158-4aee-4108-846e-60b59321e92a';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'b9ae092e-aba6-402c-8b67-c53f8c41f25c';
+
+DELETE FROM "public"."questions" WHERE "id" = '9a572d3b-2f65-4bc0-84b5-9b2f0729ee1c';
+
+
+DELETE FROM "public"."question_options" WHERE "id" = '16c2a2c8-6cfb-4ab9-b5d6-0f8051fd1177';
+
+DELETE FROM "public"."question_options" WHERE "id" = '66f52200-91a0-44b7-b649-e0fa375b8445';
+
+DELETE FROM "public"."question_options" WHERE "id" = '4250a8d7-0394-479d-a6ea-31ea11df39bd';
+
+DELETE FROM "public"."question_options" WHERE "id" = '62220c6a-7ab6-45d7-a48e-87d6375d1f64';
+
+DELETE FROM "public"."questions" WHERE "id" = '95390883-1a17-406b-87c8-679af4dba456';
+
+DELETE FROM "public"."question_options" WHERE "id" = '5e7d9508-ebcb-474d-9abb-baf15da482e4';
+
+DELETE FROM "public"."question_options" WHERE "id" = '18a03edf-1cfb-4056-9457-5c8a46b3082b';
+
+DELETE FROM "public"."question_options" WHERE "id" = '0593ab8d-ada4-4ccf-84ab-2332c3e6bb78';
+
+DELETE FROM "public"."question_options" WHERE "id" = '0a9037ac-9802-46a3-8cad-b9ed3c926eb6';
+
+DELETE FROM "public"."questions" WHERE "id" = 'd190d41a-b6ae-41a2-bd6f-00481e9002a0';
---
+++ hasura/migrations/academy_db/1724805566779_add_questions_first_module/up.sql
@@ -0,0 +1,52 @@
+
+
+
+INSERT INTO "public"."questions"("text", "answer_id", "id", "module_id") VALUES (E'What is generative art?', null, E'd190d41a-b6ae-41a2-bd6f-00481e9002a0', E'3c863d7e-cf4c-4dfd-8c98-fa117cee3908');
+
+INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (false, E'Art created solely by human hands without any digital intervention.', E'0a9037ac-9802-46a3-8cad-b9ed3c926eb6', E'd190d41a-b6ae-41a2-bd6f-00481e9002a0');
+
+INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (true, E'A form of art created through the use of algorithms, processes, or systems defined by an artist.', E'0593ab8d-ada4-4ccf-84ab-2332c3e6bb78', E'd190d41a-b6ae-41a2-bd6f-00481e9002a0');
+
+INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (false, E'Art that is always reproducible and never varies in its outcomes.', E'18a03edf-1cfb-4056-9457-5c8a46b3082b', E'd190d41a-b6ae-41a2-bd6f-00481e9002a0');
+
+INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (false, E'A type of art that only focuses on natural phenomena and organic forms.', E'5e7d9508-ebcb-474d-9abb-baf15da482e4', E'd190d41a-b6ae-41a2-bd6f-00481e9002a0');
+
+INSERT INTO "public"."questions"("text", "answer_id", "id", "module_id") VALUES (E'Which style of generative art draws inspiration from natural phenomena and biological processes?', null, E'95390883-1a17-406b-87c8-679af4dba456', E'3c863d7e-cf4c-4dfd-8c98-fa117cee3908');
+
+INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (false, E'Algorithmic abstraction.', E'62220c6a-7ab6-45d7-a48e-87d6375d1f64', E'95390883-1a17-406b-87c8-679af4dba456');
+
+INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (false, E'Narrative generative art.', E'4250a8d7-0394-479d-a6ea-31ea11df39bd', E'95390883-1a17-406b-87c8-679af4dba456');
+
+INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (false, E'Generative AI art.', E'66f52200-91a0-44b7-b649-e0fa375b8445', E'95390883-1a17-406b-87c8-679af4dba456');
+
+INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (true, E'Organic generative art.', E'16c2a2c8-6cfb-4ab9-b5d6-0f8051fd1177', E'95390883-1a17-406b-87c8-679af4dba456');
+
+INSERT INTO "public"."questions"("text", "answer_id", "id", "module_id") VALUES (E'What is a defining feature of generative art?', null, E'9a572d3b-2f65-4bc0-84b5-9b2f0729ee1c', E'3c863d7e-cf4c-4dfd-8c98-fa117cee3908');
+
+INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (false, E'Its consistent and predictable outcomes.', E'b9ae092e-aba6-402c-8b67-c53f8c41f25c', E'9a572d3b-2f65-4bc0-84b5-9b2f0729ee1c');
+
+INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (false, E'Its rejection of all forms of traditional artistic movements.', E'559ed158-4aee-4108-846e-60b59321e92a', E'9a572d3b-2f65-4bc0-84b5-9b2f0729ee1c');
+
+INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (true, E'Its ability to produce unique and unpredictable outcomes.', E'd5665a2b-ec2b-4cfb-8591-90d0991e3e9e', E'9a572d3b-2f65-4bc0-84b5-9b2f0729ee1c');
+
+INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (false, E'Its strict adherence to realism and photorealistic styles.', E'f828a2d6-e986-4804-9438-4ff1a5586dcd', E'9a572d3b-2f65-4bc0-84b5-9b2f0729ee1c');
+
+INSERT INTO "public"."questions"("text", "answer_id", "id", "module_id") VALUES (E'How do NFTs relate to generative art?', null, E'c1b7a959-3fa9-493b-a804-e0e5a59ea68f', E'3c863d7e-cf4c-4dfd-8c98-fa117cee3908');
+
+INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (false, E'They replace traditional art forms entirely.', E'8570d3a8-f18d-409a-931a-b1908a08e08c', E'c1b7a959-3fa9-493b-a804-e0e5a59ea68f');
+
+INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (false, E'They are the only way generative art can be displayed and sold.', E'd20d1fd5-5098-4524-880a-6c7e081fc965', E'c1b7a959-3fa9-493b-a804-e0e5a59ea68f');
+
+INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (true, E'They represent ownership or proof of authenticity for unique digital items, including generative artworks.', E'55c6f583-41e8-49c5-8bbb-60bd8b58351e', E'c1b7a959-3fa9-493b-a804-e0e5a59ea68f');
+
+INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (false, E'They standardize all generative art to a single format.', E'64242362-6f11-42ab-9295-97089fbf597c', E'c1b7a959-3fa9-493b-a804-e0e5a59ea68f');
+
+INSERT INTO "public"."questions"("text", "answer_id", "id", "module_id") VALUES (E'What distinguishes generative AI art from generative art?', null, E'3817ebd1-a481-4e59-b510-a84cd2d2e333', E'3c863d7e-cf4c-4dfd-8c98-fa117cee3908');
+
+INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (true, E'Generative AI art relies on algorithms trained on data, while generative art uses algorithms created by artists.', E'744049cc-a7fe-4709-84f7-bffe73cd3382', E'3817ebd1-a481-4e59-b510-a84cd2d2e333');
+
+INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (false, E'Generative AI art always uses traditional artistic techniques.', E'659df12c-5c2f-4003-8e87-cc7c432fb390', E'3817ebd1-a481-4e59-b510-a84cd2d2e333');
+
+INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (false, E'Generative art always includes photorealistic images, while generative AI art focuses on abstract forms.', E'2841a1ec-d8d7-4739-811e-46773e610ce5', E'3817ebd1-a481-4e59-b510-a84cd2d2e333');
+
+INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (false, E'Generative AI art cannot incorporate human-made rules or algorithms.', E'75bac02d-c6fb-436b-adf3-6c125d0716fd', E'3817ebd1-a481-4e59-b510-a84cd2d2e333');
