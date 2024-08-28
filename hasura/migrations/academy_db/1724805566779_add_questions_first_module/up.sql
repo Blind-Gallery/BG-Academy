9a6bf7e87f536cdaff7e9795e8ef1d157dee6156
crissthiandi <albertocenaa@gmail.com>
2024-08-28 10:20:22 -0600

    feat: Update question options for first module

--- hasura/migrations/academy_db/1724805566779_add_questions_first_module/up.sql
+++ hasura/migrations/academy_db/1724805566779_add_questions_first_module/up.sql
@@ -7,9 +7,9 @@ INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id
 
 INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (true, E'A form of art created through the use of algorithms, processes, or systems defined by an artist.', E'0593ab8d-ada4-4ccf-84ab-2332c3e6bb78', E'd190d41a-b6ae-41a2-bd6f-00481e9002a0');
 
-INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (true, E'Art that is always reproducible and never varies in its outcomes.', E'18a03edf-1cfb-4056-9457-5c8a46b3082b', E'd190d41a-b6ae-41a2-bd6f-00481e9002a0');
+INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (false, E'Art that is always reproducible and never varies in its outcomes.', E'18a03edf-1cfb-4056-9457-5c8a46b3082b', E'd190d41a-b6ae-41a2-bd6f-00481e9002a0');
 
-INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (true, E'A type of art that only focuses on natural phenomena and organic forms.', E'5e7d9508-ebcb-474d-9abb-baf15da482e4', E'd190d41a-b6ae-41a2-bd6f-00481e9002a0');
+INSERT INTO "public"."question_options"("is_correct", "text", "id", "question_id") VALUES (false, E'A type of art that only focuses on natural phenomena and organic forms.', E'5e7d9508-ebcb-474d-9abb-baf15da482e4', E'd190d41a-b6ae-41a2-bd6f-00481e9002a0');
 
 INSERT INTO "public"."questions"("text", "answer_id", "id", "module_id") VALUES (E'Which style of generative art draws inspiration from natural phenomena and biological processes?', null, E'95390883-1a17-406b-87c8-679af4dba456', E'3c863d7e-cf4c-4dfd-8c98-fa117cee3908');
 
