652de6672add171f01a69eb45365362a3c3e8ea3
David Muñoz Guzmán <108280181+Datzel@users.noreply.github.com>
2024-08-28 09:32:29 -0600

    Enhancement/claim certificate modal (#345)
    * Buttons of certificates saved in the button folder
    
    * Component names of the buttons changed in the parent component
    
    * PxCertifate component migrated to the card folder and improved with tailwind
    
    * certificate components renamed
    
    * Renamed component to maintain consistency
    
    * Slot added for more flexibility
    
    * button to open the certificate modal created
    
    * Certificate card added inside the modal slot
    
    * function to open the modal added
    
    * query created to get the course certificate info
    
    * bootstrap modal removed
    
    * Certificate card added inside the modal slot
    
    * unnecesary query removed
    
    * query created to get the course certificate info

---
+++ ui/components/certificate/openModalButton.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  props: {
+    approvedCourse: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  }
+}
+</script>
+<template>
+  <div>
+    <button v-if="approvedCourse" class="tw-mb-2 tw-flex tw-border tw-w-full  tw-gap-1 tw-border-cyan-500 tw-items-center tw-justify-center tw-text-sm tw-text-white tw-bg-cyan-500 tw-rounded tw-py-2 tw-px-6 hover:tw-bg-cyan-600 hover:tw-border-cyan-600 tw-ease-in-out tw-duration-200" @click="$emit('click')">
+      <Icon icon="material-symbols-light:verified-rounded" width="1.25rem" />
+      <span>Get certificate</span>
+    </button>
+
+    <button
+      v-else
+      v-b-tooltip.hover
+      title="Complete the exams to obtain the certificate"
+      class="tw-opacity-50 tw-cursor-not-allowed tw-mb-2 tw-flex tw-border tw-w-full  tw-gap-1 tw-border-cyan-500 tw-items-center tw-justify-center tw-text-sm tw-text-white tw-bg-cyan-500 tw-rounded tw-py-2 tw-px-6 hover:tw-bg-cyan-600 hover:tw-border-cyan-600 tw-ease-in-out tw-duration-200"
+    >
+      <Icon icon="material-symbols-light:verified-rounded" width="1.25rem" />
+      <span>Get certificate</span>
+    </button>
+  </div>
+</template>
