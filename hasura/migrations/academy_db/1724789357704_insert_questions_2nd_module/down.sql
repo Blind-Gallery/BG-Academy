ecf8e61eb9a09b9c521ed7cea613b5bc4dbb93dd
Tlalocman <desneruda@gmail.com>
2024-08-27 18:46:47 -0600

    feat: Add questions for the 2nd module in the generative art course (#341)

---
+++ hasura/migrations/academy_db/1724789357704_insert_questions_2nd_module/down.sql
@@ -0,0 +1,50 @@
+
+DELETE FROM "public"."question_options" WHERE "id" = 'e8e7f0da-fe43-43a9-8dea-2bc8cf072e6d';
+
+DELETE FROM "public"."question_options" WHERE "id" = '6d8fa05e-8eab-449f-846c-8f30223eca05';
+
+DELETE FROM "public"."question_options" WHERE "id" = '5b4e8d5e-4451-4d78-a51e-f5be201a8891';
+
+DELETE FROM "public"."question_options" WHERE "id" = '42338df8-5623-4374-9c8b-f5ade3878ceb';
+
+DELETE FROM "public"."questions" WHERE "id" = 'e464aa98-3890-4b0d-951c-3b6258fab660';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'd637794c-d95c-4925-acc8-5c66fb556464';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'a0e52779-0aa2-4e46-afa4-f5a50d1961fb';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'f16c1061-2e70-48b0-b0b3-515c171000aa';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'd26b461b-2c27-4921-9008-617018f4b350';
+
+DELETE FROM "public"."questions" WHERE "id" = 'f711d2d3-1505-45e0-a569-105fa9424bf7';
+
+DELETE FROM "public"."question_options" WHERE "id" = '8ad9e437-f2f1-4440-bc1c-a9286b980efd';
+
+DELETE FROM "public"."question_options" WHERE "id" = '1c6c2dc5-94c0-4f80-8889-0189f56ccd7b';
+
+DELETE FROM "public"."question_options" WHERE "id" = '215b59a7-2eb4-4743-b4f6-a82da20c2765';
+
+DELETE FROM "public"."question_options" WHERE "id" = '65b993e0-747d-49d9-ab9b-d98d9d2c7bc9';
+
+DELETE FROM "public"."questions" WHERE "id" = '5b0720f3-0eb0-4be9-9e9e-fae8eedf70a7';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'cef4ab48-1ff0-47ae-affe-048ea500151e';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'c7673b05-5438-49df-beed-f0eda3a024a8';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'e651941a-3d0b-4169-ad53-51648d3fa493';
+
+DELETE FROM "public"."question_options" WHERE "id" = '95850176-d8d1-4097-89be-5953129e5953';
+
+DELETE FROM "public"."questions" WHERE "id" = '2ca666af-fccf-480b-8916-a6272270ca86';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'b7607fad-1a77-4267-8faa-5728f76a8561';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'f0cb242d-7e76-49c2-a0ee-da2cd82599fc';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'bef6e14b-f873-4968-ac02-0d7b0ec21bd1';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'a30806fb-e1c1-4fd2-b30a-bc1a7db6f676';
+
+DELETE FROM "public"."questions" WHERE "id" = '54c6666b-eabc-4dbb-b3db-215cf1bfb02a';
