cec914f901bcb71dac65b28eef9caada799f3a6e
Datzel <davemg71@hotmail.com>
2024-08-28 13:53:53 -0600

    margin bottom added in the challengeCard

--- ui/components/PxNavigator/challengeCard.vue
+++ ui/components/PxNavigator/challengeCard.vue
@@ -12,7 +12,7 @@ export default {
 
 <template>
   <div>
-    <NuxtLink :to="route" class="tw-flex tw-flex-col tw-border tw-rounded tw-p-4 tw-text-inherit hover:tw-bg-gray-100 tw-ease-in-out tw-duration-200 tw-cursor-pointer hover:tw-no-underline hover:tw-text-inherit">
+    <NuxtLink :to="route" class="tw-flex tw-mb-2 tw-flex-col tw-border tw-rounded tw-p-4 tw-text-inherit hover:tw-bg-gray-100 tw-ease-in-out tw-duration-200 tw-cursor-pointer hover:tw-no-underline hover:tw-text-inherit">
       <div class="tw-w-full tw-flex tw-items-center  tw-justify-between">
         <span class="tw-text-sm tw-font-bold">Challenge</span>
         <div>
