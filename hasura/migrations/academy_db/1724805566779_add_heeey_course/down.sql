0790667e1e9c986d3794cc67bbf776c49be9c304
Cristhian Diaz <albertocenaa@gmail.com>
2024-08-29 11:16:41 -0500

    add migration for heey curses (#332)
    * add migration for heey curses
    
    * add first module  (#334)
    
    * add first module (is necessary add the second module to get the next module id)
    
    * feat: Add second module and update module dependencies
    
    The code changes include adding the second module for the generative art course and updating the dependencies between modules. This change ensures that the next module ID is correctly set for each module.
    
    * feat: Add second module and update module dependencies
    
    * Migration/add heey module (#336)
    
    * add first module (is necessary add the second module to get the next module id)
    
    * feat: Add second module and update module dependencies
    
    The code changes include adding the second module for the generative art course and updating the dependencies between modules. This change ensures that the next module ID is correctly set for each module.
    
    * feat: Add second module and update module dependencies
    
    * feat: Add second module and update module dependencies
    
    * feat: Add second module and update module dependencies
    
    * Erick/feat/add-chapters-2nd-module (#338)
    
    * feat: add chapters from second module
    
    The code changes add chapters from the second module in the academy_db
    
    * feat: Add comment and heeey to spellcheck
    
    * feat: add chapters from the 3rd module (#339)
    
    * feat: insert chapters from 4th module (#340)
    
    * feat: Add questions for the 2nd module in the generative art course (#341)
    
    * feat: Add questions for the 3rd module in the generative art course (#342)
    
    * Delete chapters from first module (#344)
    
    * feat: Update questions and options for first module (#346)
    
    * Delete chapters from first module
    
    * feat: Add questions and options for first module
    
    * feat: Remove unnecessary question options from first module
    
    * feat: Update question options for first module
    
    * feat: Insert questions and options for first module
    
    * feat: Insert questions and options for first module
    
    * feat: Remove unnecessary question options from first module
    
    * feat: Update question options for first module
    
    * feat: Update course description and add questions for first module
    
    * update thumbnials (#360)
    
    * feat: Update course description and add Heeey's profile picture for the generative art course
    
    * feat: Update course description and add Heeey's profile picture for the generative art course
    
    * squash migration
    
    ---------
    
    Co-authored-by: Tlalocman <desneruda@gmail.com>

---
+++ hasura/migrations/academy_db/1724805566779_add_heeey_course/down.sql
@@ -0,0 +1,199 @@
+
+
+DELETE FROM "public"."question_options" WHERE "id" = '75bac02d-c6fb-436b-adf3-6c125d0716fd';
+
+DELETE FROM "public"."question_options" WHERE "id" = '2841a1ec-d8d7-4739-811e-46773e610ce5';
+
+DELETE FROM "public"."question_options" WHERE "id" = '659df12c-5c2f-4003-8e87-cc7c432fb390';
+
+DELETE FROM "public"."question_options" WHERE "id" = '744049cc-a7fe-4709-84f7-bffe73cd3382';
+
+DELETE FROM "public"."questions" WHERE "id" = '3817ebd1-a481-4e59-b510-a84cd2d2e333';
+
+DELETE FROM "public"."question_options" WHERE "id" = '64242362-6f11-42ab-9295-97089fbf597c';
+
+DELETE FROM "public"."question_options" WHERE "id" = '55c6f583-41e8-49c5-8bbb-60bd8b58351e';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'd20d1fd5-5098-4524-880a-6c7e081fc965';
+
+DELETE FROM "public"."question_options" WHERE "id" = '8570d3a8-f18d-409a-931a-b1908a08e08c';
+
+DELETE FROM "public"."questions" WHERE "id" = 'c1b7a959-3fa9-493b-a804-e0e5a59ea68f';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'f828a2d6-e986-4804-9438-4ff1a5586dcd';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'd5665a2b-ec2b-4cfb-8591-90d0991e3e9e';
+
+DELETE FROM "public"."question_options" WHERE "id" = '559ed158-4aee-4108-846e-60b59321e92a';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'b9ae092e-aba6-402c-8b67-c53f8c41f25c';
+
+DELETE FROM "public"."questions" WHERE "id" = '9a572d3b-2f65-4bc0-84b5-9b2f0729ee1c';
+
+
+DELETE FROM "public"."question_options" WHERE "id" = '16c2a2c8-6cfb-4ab9-b5d6-0f8051fd1177';
+
+DELETE FROM "public"."question_options" WHERE "id" = '66f52200-91a0-44b7-b649-e0fa375b8445';
+
+DELETE FROM "public"."question_options" WHERE "id" = '4250a8d7-0394-479d-a6ea-31ea11df39bd';
+
+DELETE FROM "public"."question_options" WHERE "id" = '62220c6a-7ab6-45d7-a48e-87d6375d1f64';
+
+DELETE FROM "public"."questions" WHERE "id" = '95390883-1a17-406b-87c8-679af4dba456';
+
+DELETE FROM "public"."question_options" WHERE "id" = '5e7d9508-ebcb-474d-9abb-baf15da482e4';
+
+DELETE FROM "public"."question_options" WHERE "id" = '18a03edf-1cfb-4056-9457-5c8a46b3082b';
+
+DELETE FROM "public"."question_options" WHERE "id" = '0593ab8d-ada4-4ccf-84ab-2332c3e6bb78';
+
+DELETE FROM "public"."question_options" WHERE "id" = '0a9037ac-9802-46a3-8cad-b9ed3c926eb6';
+
+DELETE FROM "public"."questions" WHERE "id" = 'd190d41a-b6ae-41a2-bd6f-00481e9002a0';
+
+
+DELETE FROM "public"."chapters" WHERE "id" = 'c95f74a1-b210-4b6d-86db-96caf8bf8ff8';
+
+DELETE FROM "public"."chapters" WHERE "id" = '7cf010fd-c614-4900-8e59-5d494b465aee';
+
+DELETE FROM "public"."chapters" WHERE "id" = 'df7ba392-86a7-442f-84ab-f146cddd0cbc';
+
+DELETE FROM "public"."chapters" WHERE "id" = 'c88ce423-43c8-4540-9558-a74b499d134f';
+
+
+DELETE FROM "public"."question_options" WHERE "id" = 'e1178108-589c-4d5c-8eb0-7c26614e6a79';
+
+DELETE FROM "public"."question_options" WHERE "id" = '9bfa76e0-17af-4119-9f1f-5989256b7d8f';
+
+DELETE FROM "public"."question_options" WHERE "id" = '21a3edab-ab98-4d7b-b8ff-71df81167217';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'f45f21fc-dadc-4136-87c5-f72c653c654e';
+
+DELETE FROM "public"."questions" WHERE "id" = 'de54ee2d-3893-402c-b9aa-8ad555dcdb4b';
+
+DELETE FROM "public"."question_options" WHERE "id" = '00fc077f-4058-4032-9559-66e48856654b';
+
+DELETE FROM "public"."question_options" WHERE "id" = '69f25f7a-7404-491b-ac6d-53b4b3a2709c';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'e663ddf2-7274-4f23-b658-88cee6aec17a';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'b299c770-011a-4362-b2bb-11869ef8d6a4';
+
+DELETE FROM "public"."questions" WHERE "id" = '49fdd3cc-4cb4-4020-becd-7c58382c6434';
+
+DELETE FROM "public"."question_options" WHERE "id" = '31651f82-bfc7-4b8a-a654-70ee66ea78e6';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'a50de4f5-9325-41b4-b003-b779fc4c5c3e';
+
+DELETE FROM "public"."question_options" WHERE "id" = '6c792f32-01bb-4b87-a7e9-b5a8e98ddf1f';
+
+DELETE FROM "public"."question_options" WHERE "id" = '34712719-e197-47b2-80f7-3d21ef1a9073';
+
+DELETE FROM "public"."questions" WHERE "id" = '2743868c-bdc3-4a5f-91d1-f1efd5146cd2';
+
+DELETE FROM "public"."question_options" WHERE "id" = '6a5072e4-0a9b-4be7-8f2f-2464b2d662e0';
+
+DELETE FROM "public"."question_options" WHERE "id" = '4a20cdf0-fb2e-4126-a1ed-5c30e19689dc';
+
+DELETE FROM "public"."question_options" WHERE "id" = '0dbecb05-2dda-4227-b629-c2727e5940d0';
+
+DELETE FROM "public"."question_options" WHERE "id" = '1e3ad354-42a0-4982-94e2-ab3b0351f982';
+
+DELETE FROM "public"."questions" WHERE "id" = 'f9fad854-0356-4aba-acd9-276cd06c705b';
+
+DELETE FROM "public"."question_options" WHERE "id" = '2def4879-f5ff-4750-bd3e-3acf7803fd00';
+
+DELETE FROM "public"."question_options" WHERE "id" = '3d3e24ac-7865-416b-987c-18af527bb043';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'b63705bc-9c71-4c3b-b952-7733a1dce4dc';
+
+DELETE FROM "public"."question_options" WHERE "id" = '7a8ddff3-39a6-463e-b935-cd47190f74b3';
+
+DELETE FROM "public"."questions" WHERE "id" = 'af8c2737-c81f-42be-b78a-d06bf6038dab';
+
+
+DELETE FROM "public"."question_options" WHERE "id" = 'e8e7f0da-fe43-43a9-8dea-2bc8cf072e6d';
+
+DELETE FROM "public"."question_options" WHERE "id" = '6d8fa05e-8eab-449f-846c-8f30223eca05';
+
+DELETE FROM "public"."question_options" WHERE "id" = '5b4e8d5e-4451-4d78-a51e-f5be201a8891';
+
+DELETE FROM "public"."question_options" WHERE "id" = '42338df8-5623-4374-9c8b-f5ade3878ceb';
+
+DELETE FROM "public"."questions" WHERE "id" = 'e464aa98-3890-4b0d-951c-3b6258fab660';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'd637794c-d95c-4925-acc8-5c66fb556464';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'a0e52779-0aa2-4e46-afa4-f5a50d1961fb';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'f16c1061-2e70-48b0-b0b3-515c171000aa';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'd26b461b-2c27-4921-9008-617018f4b350';
+
+DELETE FROM "public"."questions" WHERE "id" = 'f711d2d3-1505-45e0-a569-105fa9424bf7';
+
+DELETE FROM "public"."question_options" WHERE "id" = '8ad9e437-f2f1-4440-bc1c-a9286b980efd';
+
+DELETE FROM "public"."question_options" WHERE "id" = '1c6c2dc5-94c0-4f80-8889-0189f56ccd7b';
+
+DELETE FROM "public"."question_options" WHERE "id" = '215b59a7-2eb4-4743-b4f6-a82da20c2765';
+
+DELETE FROM "public"."question_options" WHERE "id" = '65b993e0-747d-49d9-ab9b-d98d9d2c7bc9';
+
+DELETE FROM "public"."questions" WHERE "id" = '5b0720f3-0eb0-4be9-9e9e-fae8eedf70a7';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'cef4ab48-1ff0-47ae-affe-048ea500151e';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'c7673b05-5438-49df-beed-f0eda3a024a8';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'e651941a-3d0b-4169-ad53-51648d3fa493';
+
+DELETE FROM "public"."question_options" WHERE "id" = '95850176-d8d1-4097-89be-5953129e5953';
+
+DELETE FROM "public"."questions" WHERE "id" = '2ca666af-fccf-480b-8916-a6272270ca86';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'b7607fad-1a77-4267-8faa-5728f76a8561';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'f0cb242d-7e76-49c2-a0ee-da2cd82599fc';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'bef6e14b-f873-4968-ac02-0d7b0ec21bd1';
+
+DELETE FROM "public"."question_options" WHERE "id" = 'a30806fb-e1c1-4fd2-b30a-bc1a7db6f676';
+
+DELETE FROM "public"."questions" WHERE "id" = '54c6666b-eabc-4dbb-b3db-215cf1bfb02a';
+
+
+DELETE FROM "public"."chapters" WHERE "id" = '3079e158-4009-4992-b5ff-656d9cee1639';
+
+DELETE FROM "public"."chapters" WHERE "id" = 'de689e53-86f0-4da9-961b-d588fa2e86c7';
+
+DELETE FROM "public"."chapters" WHERE "id" = '5c1213f8-6946-451c-92be-7ed9e14e9d85';
+
+
+DELETE FROM "public"."chapters" WHERE "id" = '002542e0-6865-4556-b6d0-5723bbd788de';
+
+DELETE FROM "public"."chapters" WHERE "id" = '0a842cc9-30b1-443a-a5b1-ec8e623889da';
+
+DELETE FROM "public"."chapters" WHERE "id" = 'c5f8d254-8e7c-44b8-908d-5193d7125eb5';
+
+
+DELETE FROM "public"."chapters" WHERE "id" = '8633204d-e5b3-4cf8-aaa0-e23fe4f0d7af';
+
+DELETE FROM "public"."chapters" WHERE "id" = '2b53d3d9-668c-49f8-803c-9504fb0ecfdf';
+
+DELETE FROM "public"."chapters" WHERE "id" = 'e56c1ca9-bb2b-4205-8313-e5165a886bed';
+
+
+DELETE FROM "public"."modules" WHERE "id" = 'c5a014db-7995-4cb6-8514-2377f5d47988';
+
+DELETE FROM "public"."modules" WHERE "id" = '4674105d-8dca-49d6-a705-512e66db9dfd';
+
+
+DELETE FROM "public"."modules" WHERE "id" = '4c02988e-e748-4fab-b0e4-fbcb50d063b9';
+
+DELETE FROM "public"."modules" WHERE "id" = '3c863d7e-cf4c-4dfd-8c98-fa117cee3908';
+
+
+DELETE FROM "public"."courses" WHERE "id" = '9df7cd5f-d9b3-4f37-9693-15ed1be65cf5';
+
+DELETE FROM "public"."teachers" WHERE "id" = 'f99406ba-af32-4bb3-b300-9eb034149ea5';
