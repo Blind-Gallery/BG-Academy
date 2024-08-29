61b1062e5e54736c21ed2ffaec2899367284d899
tlacloc <desneruda@gmail.com>
2024-08-29 11:42:09 -0600

    feat: Update you_will_learn descriptions for generative art modules
    The code changes update the you_will_learn descriptions for the generative art modules in the academy_db. This ensures that the descriptions accurately reflect the content covered in each module.

---
+++ hasura/migrations/academy_db/1724951710934_add_you_will_learn_heeey/up.sql
@@ -0,0 +1,15 @@
+UPDATE modules
+SET you_will_learn = 'The fundamentals of generative art, the role of algorithms and NFTs, and the distinctions between generative code art and generative AI art.'
+WHERE id = '3c863d7e-cf4c-4dfd-8c98-fa117cee3908';
+
+UPDATE modules
+SET you_will_learn = 'The origins of generative art, from early algorithmic influences and pioneers to its growth as a distinct movement.'
+WHERE id = '4c02988e-e748-4fab-b0e4-fbcb50d063b9';
+
+UPDATE modules
+SET you_will_learn = 'Explore the role of online communities, cross-disciplinary impact, and key artists shaping modern generative art.'
+WHERE id = '4674105d-8dca-49d6-a705-512e66db9dfd';
+
+UPDATE modules
+SET you_will_learn = 'How blockchain technology has influenced long-form generative art, including NFTs, Art Blocks, and notable artists.'
+WHERE id = 'c5a014db-7995-4cb6-8514-2377f5d47988';
