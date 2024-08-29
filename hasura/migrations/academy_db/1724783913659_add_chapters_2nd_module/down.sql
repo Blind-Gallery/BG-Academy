6dc977e0657eaebe7726b2a44b5a048d35921067
tlacloc <desneruda@gmail.com>
2024-08-29 10:15:05 -0600

    squash migration

--- hasura/migrations/academy_db/1724783913659_add_chapters_2nd_module/down.sql
+++
@@ -1,6 +0,0 @@
-
-DELETE FROM "public"."chapters" WHERE "id" = '8633204d-e5b3-4cf8-aaa0-e23fe4f0d7af';
-
-DELETE FROM "public"."chapters" WHERE "id" = '2b53d3d9-668c-49f8-803c-9504fb0ecfdf';
-
-DELETE FROM "public"."chapters" WHERE "id" = 'e56c1ca9-bb2b-4205-8313-e5165a886bed';
