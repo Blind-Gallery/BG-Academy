c2065e6b2fadb9eb098c5d6d9d4c48bccf605061
tlacloc <desneruda@gmail.com>
2024-08-30 16:21:13 -0600

    feat: Add columns for feedback rating and route to user_course table

---
+++ hasura/migrations/academy_db/1725056254349_feedback_enhancements/down.sql
@@ -0,0 +1,10 @@
+
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- alter table "public"."user_course" add column "feedback_route" text
+--  null;
+
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- alter table "public"."user_course" add column "feedback_rating" integer
+--  null;
---
+++ hasura/migrations/academy_db/1725056254349_feedback_enhancements/up.sql
@@ -0,0 +1,6 @@
+
+alter table "public"."user_course" add column "feedback_rating" integer
+ null;
+
+alter table "public"."user_course" add column "feedback_route" text
+ null;
