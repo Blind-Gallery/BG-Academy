88775a7a00e46b054572cd0c0b43f470992ccb36
tlacloc <desneruda@gmail.com>
2024-08-30 13:28:57 -0600

    Update EARLY_ACCESS_USER_IDS constant in index.js

--- ui/constants/index.js
+++ ui/constants/index.js
@@ -1,5 +1,5 @@
 // allow list
-export const EARLY_ACCESS_USER_IDS = process.env.EARLY_ACCESS_USER_IDS || []
+export const EARLY_ACCESS_USER_IDS = process.env.EARLY_ACCESS_USER_IDS ?? []
 // Tezos
 export const TEZTOK_API = 'https://api.teztok.com/v1/graphql'
 export const FXHASH_API = 'https://api.fxhash.xyz/graphql'
