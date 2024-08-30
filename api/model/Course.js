58d97658539b0f23fc63c05a7020febedf2a8dd8
tlacloc <desneruda@gmail.com>
2024-08-30 16:08:33 -0600

    feat: Update Course model to include methods for updating feedback and getting course ID from route

--- api/model/Course.js
+++ api/model/Course.js
@@ -1,15 +1,31 @@
 const { BadRequest } = require('http-errors')
-
 const { logger } = require('../service')
 
+const {
+  GET_COURSE_ID_FROM_CHAPTER_ID,
+  GET_COURSE_ID_FROM_MODULE_ID,
+  UPDATE_FEEDBACK
+} = require('../graphQL')
+
 class Course {
-  updateFeedback (feedback, rating, route, userId) {
-    logger.info(`Updating feedback for course ${route}`)
-    // update feedback
-    const courseId = this.getCourseIdFromRoute(route)
+  constructor ({ gql }) {
+    this.gql = gql
   }
 
-  getCourseIdFromRoute (route) {
+  async updateFeedback (feedback, rating, route, userId) {
+    logger.info(`Updating feedback for course ${route}`)
+    // update feedback
+    const courseId = await this.getCourseIdFromRoute(route)
+    const feedbackText = `${feedback} - ${rating}/5 (feedback from ${route})`
+    logger.debug(`Feedback: ${feedbackText}`)
+    await this.gql.request(UPDATE_FEEDBACK, {
+      feedback: feedbackText,
+      courseId,
+      userId
+    })
+  }
+
+  async getCourseIdFromRoute (route) {
     const routeParts = route.split('/')
     if (routeParts.length !== 4) {
       throw new BadRequest('Invalid route')
@@ -22,9 +38,15 @@ class Course {
       case 'challenge':
         return routeParts[3]
       case 'test':
-        return routeParts[3]
+        // eslint-disable-next-line no-case-declarations
+        const { modules_by_pk: modules } = await this.gql.request(
+          GET_COURSE_ID_FROM_MODULE_ID, { id: routeParts[3] })
+        return modules.course_id
       case 'chapter':
-        return routeParts[3]
+        // eslint-disable-next-line no-case-declarations
+        const { chapters_by_pk: chapters } = await this.gql.request(
+          GET_COURSE_ID_FROM_CHAPTER_ID, { id: routeParts[3] })
+        return chapters.module.course_id
       default:
         throw new BadRequest('Invalid route')
     }
