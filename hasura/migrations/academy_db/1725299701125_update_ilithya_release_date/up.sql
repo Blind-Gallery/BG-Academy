d2ae94050f4efc6d25a642220f56c8e812691e24
crissthiandi <albertocenaa@gmail.com>
2024-09-02 13:11:18 -0600

    feat: Update ilithya course release date to '2024-09-03T07:00:00.075383+00:00'

--- hasura/migrations/academy_db/1725299701125_update_ilithya_release_date/up.sql
+++ hasura/migrations/academy_db/1725299701125_update_ilithya_release_date/up.sql
@@ -1,4 +1,4 @@
 -- update release date to 2024-09-03 9:00:00 AM CET
 UPDATE courses
 SET release_date = '2024-09-03T07:00:00.075383+00:00'
-vWHERE id = '5f1f6044-21ba-4409-880e-02cd36568697';
+WHERE id = '5f1f6044-21ba-4409-880e-02cd36568697';
