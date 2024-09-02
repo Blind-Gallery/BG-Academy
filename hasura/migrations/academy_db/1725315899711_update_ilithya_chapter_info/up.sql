7afb2eedab6b991de7a0fe5b903d2113f507e548
crissthiandi <albertocenaa@gmail.com>
2024-09-02 17:43:03 -0600

    feat: Update ilithya course descriptions

---
+++ hasura/migrations/academy_db/1725315899711_update_ilithya_chapter_info/up.sql
@@ -0,0 +1,111 @@
+-- Module 0
+-- chapter 1
+UPDATE chapters
+SET description = 'ilithya introduces the basics of pixel shaders and expresses her excitement about helping viewers get started.'
+WHERE id = '3dbd7594-ecd1-487d-a98d-6a915049b455';
+
+-- Module 1
+-- chapter 1
+UPDATE chapters
+SET description = 'In this chapter, ilithya explains what shaders are. Shaders are a set of code implementing algorithms to render pixels on the screen.'
+WHERE id = 'b127c9e1-698c-4cd7-a534-c41c87b1893b'
+
+-- chapter 2
+UPDATE chapters
+SET description = ''
+WHERE id = '1631e746-0efc-4e49-becb-6a0c6c44a749'
+
+-- chapter 3
+UPDATE chapters
+SET description = ''
+WHERE id = 'e7387a97-af99-4976-87b9-6510235c89d8'
+
+-- chapter 4
+UPDATE chapters
+SET description = ''
+WHERE id = '9c294d3f-4df5-4532-bb81-ce6621b8dc13'
+
+-- chapter 5
+UPDATE chapters
+SET description = ''
+WHERE id = '5c0eb529-985d-44de-b83d-78360313efae'
+
+-- Module 2
+-- chapter 1
+UPDATE chapters
+SET description = ''
+WHERE id = 'ea905e11-a99c-4148-8705-338779763f35'
+
+-- chapter 2
+UPDATE chapters
+SET description = ''
+WHERE id = 'e22d8840-239e-42c9-b6f7-dbc76a656b1a'
+
+-- chapter 3
+UPDATE chapters
+SET description = ''
+WHERE id = '40cbea88-12fd-4e7f-a374-6cf8440d9603'
+
+-- Module 3
+-- chapter 1
+UPDATE chapters
+SET description = ''
+WHERE id = 'e982fbb2-7c2b-48a8-969b-7d7fbcc313f4'
+
+-- chapter 2,3,4
+UPDATE chapters
+SET description = ''
+WHERE id IN ('0e6008a4-5402-41ad-bec4-7d4237e46400', '974053b1-077c-4fca-bc2c-131c9bc5fcb5', '3c0d4bf4-bd5d-4bb6-bf58-730e0d242ce6')
+
+-- chapter 5,6
+UPDATE chapters
+SET description = ''
+WHERE id IN ('e7a4c97a-619e-4cda-9d9f-fb080a6bd23b','b57e8b32-780f-47e5-9a9d-3a12ee6a7fd6')
+
+-- chapter 7
+UPDATE chapters
+SET description = ''
+WHERE id = '899ecea6-9c2d-4779-9677-81057dafbb72'
+
+-- chapter 8
+UPDATE chapters
+SET description = ''
+WHERE id = '04847452-264b-4bed-be37-ac2ce8b7c316'
+
+-- Module 4
+-- chapter 1
+UPDATE chapters
+SET description = ''
+WHERE id = '5fc650d0-8c74-4454-90fd-bf169dd740f5'
+
+-- chapter 2, 3
+UPDATE chapters
+SET description = ''
+WHERE id IN ('4651437f-2d0a-4b0c-9fb3-0cbcf517775d', '83f27b85-5e10-4429-a9fc-6e04c8b9a6d2')
+
+-- chapter 4
+UPDATE chapters
+SET description = ''
+WHERE id = '6156514a-9bb8-4b88-bbe7-117641d5ae28'
+
+-- chapter 5
+UPDATE chapters
+SET description = ''
+WHERE id = '1c3463e5-8774-46b2-9b41-8b6c85fb2650'
+
+-- Module 5
+-- chapter 1
+UPDATE chapters
+SET description = ''
+WHERE id = '22560dcf-970c-4e46-8a8c-c8cdc4dcb5ff'
+
+-- chapter 2,3
+UPDATE chapters
+SET description = ''
+WHERE id IN ('c33a7b2f-e979-4247-888a-da13f03a3b06','cd1f2792-0139-4e4b-b72d-0c1f0b6bcb43')
+
+-- Module 6
+-- chapter 1
+UPDATE chapters
+SET description = ''
+WHERE id = 'b2b58372-1a97-42b5-ac2e-c04a52fe841a'
