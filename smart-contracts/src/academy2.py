68872dd2af2fdc98daea34f065655f259cb93e2f
tlacloc <desneruda@gmail.com>
2024-09-02 12:20:15 -0600

    refactor types, migrate crud, and add user

--- smart-contracts/src/academy2.py
+++ smart-contracts/src/academy2.py
@@ -24,6 +24,26 @@ burn = sp.address('tz1burnburnburnburnburnburnburjAYjjX')
 blind_gallery_address = sp.address('tz1UdddbVe3icmr5LRP1monxfR13ChsidcoX')
 fxhash_treasury = sp.address('tz1dtzgLYUHMhP6sWeFtFsHkHqyPezBBPLsZ')
 
+
+@sp.module
+# types definition
+def types():
+    course_key: type = sp.nat
+    course: type = sp.record(
+        name=sp.string,
+        price=sp.nat,
+        is_active=sp.bool,
+        teacher=sp.address,
+        teacher_cut=sp.nat,
+    )
+    course_map: type = sp.map[course_key, course]
+    user_course_key: type = sp.pair[sp.address, sp.nat]
+    user_course: type = sp.record(
+        is_active=sp.bool,
+        is_paid=sp.bool,
+        soul_bound_token_id=sp.nat,
+    )
+    user_courses_map: type = sp.map[user_course_key, user_course]
 # A SmartPy module
 @sp.module
 def main():
@@ -46,14 +66,15 @@ def main():
         def is_administrator_(self, sender):
             return sender == self.data.administrator
 
-        # @sp.private(with_storage="read-only")
-        # def is_admin_or_mod(self, address):
-        #     auth = False
-        #     if self.is_administrator_(address):
-        #         auth = True
-        #     if self.data.moderators.contains(address):
-        #         auth = True
-        #     return auth
+        @sp.private(with_storage="read-only")
+        def is_admin_or_mod(self, address):
+            sp.cast(address, sp.address)
+            auth = False
+            if address == self.data.administrator:
+                auth = True
+            if self.data.moderators.contains(address):
+                auth = True
+            return auth
         
         @sp.entrypoint
         def set_administrator(self, params):
@@ -82,12 +103,68 @@ def main():
         def __init__(self, administrator, metadata):
             AdminInterface.__init__(self, administrator)
             self.data.metadata = metadata
-# Tests
+            self.data.courses = sp.cast({},types.course_map)
+            self.data.course_count = sp.nat(0)
+            self.data.user_courses = sp.cast({},types.user_courses_map)
+
+        @sp.entrypoint
+        def crate_course(self, params):
+            sp.cast(params, sp.record(
+                name=sp.string,
+                price=sp.nat,
+                teacher=sp.address,
+                teacher_cut=sp.nat
+                ))
+            assert self.is_admin_or_mod(sp.sender), "USER_NOT_AUTHORIZED"
+            
+            self.data.course_count += 1
+            self.data.courses[self.data.course_count] = sp.record(
+                name=params.name,
+                price=params.price,
+                is_active=True,
+                teacher=params.teacher,
+                teacher_cut=params.teacher_cut
+            )
+            
+        
+        @sp.entrypoint
+        def update_course(self, params):
+            sp.cast(params, sp.record(
+                course_id=sp.nat,
+                name=sp.string,
+                price=sp.nat,
+                teacher=sp.address,
+                teacher_cut=sp.nat,
+                is_active=sp.bool))
+            
+            assert self.is_admin_or_mod(sp.sender), "USER_NOT_AUTHORIZED"
+            self.data.courses[params.course_id] = sp.record(
+                name=params.name,
+                price=params.price,
+                is_active=True,
+                teacher=params.teacher,
+                teacher_cut=params.teacher_cut
+            )
+
+        @sp.entrypoint
+        def add_course_to_user(self, params):
+            sp.cast(params, sp.record(
+                course_id=sp.nat,
+                user=sp.address,
+                token_id=sp.nat
+            ))
+            assert self.is_admin_or_mod(sp.sender), "USER_NOT_AUTHORIZED"
+            self.data.user_courses[(params.user, params.course_id)] = sp.record(
+                is_active=False,
+                is_paid=False,
+                soul_bound_token_id=params.token_id
+            )
+
+            # Tests
 @sp.add_test()
 def test():
     # We define a test scenario, together with some outputs and checks
-    # The scenario takes the module as a parameter
-    scenario = sp.test_scenario("Academy", main)
+    scenario = sp.test_scenario("Academy", [types, main])
     scenario.h1("Academy")
 
     # We first define a contract and add it to the scenario
