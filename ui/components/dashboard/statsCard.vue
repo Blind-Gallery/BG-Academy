6b85c179810db7d9f4d9d3ad5f2b1deee75393a4
David Muñoz Guzmán <108280181+Datzel@users.noreply.github.com>
2024-09-02 15:11:58 -0600

    Enhanement/sales dashboard UI (#408)
    * stats component created
    
    * Data migrated to components and unneeded data removed
    
    * Date and volume formatted with computed functions
    
    * validator added to avoid the null value
    
    * computed functions improvements
    
    * type of value, changed to avoid errors

---
+++ ui/components/dashboard/statsCard.vue
@@ -0,0 +1,53 @@
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    sales: {
+      type: Number,
+      default: 0,
+      validator: value => value >= 0
+    },
+    volume: {
+      type: String,
+      default: null
+    },
+    date: {
+      type: String,
+      default: null
+    },
+    icon: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    formattedVolume () {
+      if (this.volume === null) { return null }
+      return this.title.includes('credit') ? `$${this.volume}` : `$XTZ ${this.volume}`
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="tw-flex tw-flex-col hover:tw-shadow tw-duration-200 tw-ease-in-out tw-justify-between tw-border tw-rounded tw-w-full tw-h-[150px] tw-p-4 tw-mb-2 lg:tw-mb-0 tw-border-l-4 tw-border-l-cyan-500 tw-shadow-sm">
+    <div class="tw-border tw-rounded-full tw-p-1 tw-w-fit tw-text-gray-500">
+      <Icon :icon="icon" width="1.4rem" />
+    </div>
+    <div class="tw-flex tw-flex-col">
+      <h4 v-if="formattedVolume" class="tw-truncate tw-text-cyan-500 tw-mb-0">
+        {{ formattedVolume }}
+      </h4>
+      <h4 v-if="sales" class="tw-truncate tw-text-cyan-500 tw-mb-0">
+        {{ sales }}
+      </h4>
+      <h4 v-if="date" class="tw-truncate tw-text-cyan-500 tw-mb-0">
+        {{ date }}
+      </h4>
+      <span class="tw-text-gray-500 tw-text-sm">{{ title }}</span>
+    </div>
+  </div>
+</template>
