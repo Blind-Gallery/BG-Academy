d845b9c072b2a71e4c926591a54198f1014bca29
tlacloc <desneruda@gmail.com>
2024-09-02 11:30:10 -0600

    migrate admin interface

--- smart-contracts/src/academy2.py
+++ smart-contracts/src/academy2.py
@@ -15,6 +15,8 @@ TZIP16_Metadata_Base = {
     "pictures": [ { "link": "ipfs://QmawzWk2y9Q6KYTYBQvRNuRH5KtojgeGPUxUqycaNbvb9z", "type": "logo" } ]
 }
 
+METADATA_CID = 'ipfs://QmT9RrZtLX9qJH6kCQLkD8TwsQTbfXyd9wvubCCji6xSHT'
+
 token_id_type = sp.nat
 moderator_type = sp.address
 moderator_name_type = sp.string
@@ -25,8 +27,8 @@ fxhash_treasury = sp.address('tz1dtzgLYUHMhP6sWeFtFsHkHqyPezBBPLsZ')
 # A SmartPy module
 @sp.module
 def main():
-    # A class of contracts
-    class MyContract(sp.Contract):
+
+    class AdminInterface(sp.Contract):
         """ Provide the basics for having an administrator in the contract.
 
         Adds an `administrator` attribute in the storage record. Provides a
@@ -34,67 +36,65 @@ def main():
         programming function.
         """
 
-        def __init__(self, administrator, a):
+        def __init__(self, administrator):
             self.data.administrator=administrator
-            self.data.moderators={}
+            self.data.moderators={
+                sp.address('tz1XvjDEbTFgowT11uEyPM6rbbZ48zD3PsWm'): 'Backend Service'
+            }
 
         @sp.private(with_storage="read-only")
-        def is_administrator(self, sender):
-            sp.cast(sp.sender, sp.address)
-            """Not standard, may be re-defined through inheritance."""
-            return True
+        def is_administrator_(self, sender):
+            return sender == self.data.administrator
 
-        @sp.entry_point
+        # @sp.private(with_storage="read-only")
+        # def is_admin_or_mod(self, address):
+        #     auth = False
+        #     if self.is_administrator_(address):
+        #         auth = True
+        #     if self.data.moderators.contains(address):
+        #         auth = True
+        #     return auth
+        
+        @sp.entrypoint
         def set_administrator(self, params):
             """(Admin only) Set the contract administrator."""
-            assert self.is_administrator(sp.sender), "FA2_NOT_ADMIN"
+            assert self.is_administrator_(sp.sender), "FA2_NOT_ADMIN"
             self.data.administrator = params
 
-        @sp.private(with_storage="read-only")
-        def is_admin_or_mod(self, address):
-            auth = False
-            if self.is_administrator(address):
-                auth = True
-            if self.data.moderators.contains(address):
-                auth = True
-            return auth
-
-        @sp.entry_point
+        @sp.onchain_view()
+        def get_administrator(self):
+            return self.data.administrator
+        
+        @sp.entrypoint
         def add_moderator(self, params):
             sp.cast(params, sp.record(moderator=sp.address, name=sp.string),)
-            assert self.is_administrator(sp.sender), "FA2_NOT_ADMIN"
-            self.data.moderators[params.moderator] = params.name0
+            assert self.is_administrator_(sp.sender), "FA2_NOT_ADMIN"
+            self.data.moderators = sp.update_map(params.moderator, sp.Some(params.name), self.data.moderators)
 
-        @sp.entry_point
+        @sp.entrypoint
         def remove_moderator(self, params):
             sp.cast(params.moderator, sp.address)
-            assert self.is_administrator(sp.sender), "FA2_NOT_ADMIN"
+            assert self.is_administrator_(sp.sender), "FA2_NOT_ADMIN"
 
             del self.data.moderators[params.moderator]
 
+    class Academy(AdminInterface):
+        def __init__(self, administrator, metadata):
+            AdminInterface.__init__(self, administrator)
+            self.data.metadata = metadata
 # Tests
 @sp.add_test()
 def test():
     # We define a test scenario, together with some outputs and checks
     # The scenario takes the module as a parameter
-    scenario = sp.test_scenario("Welcome", main)
-    scenario.h1("Welcome")
+    scenario = sp.test_scenario("Academy", main)
+    scenario.h1("Academy")
 
     # We first define a contract and add it to the scenario
-    c1 = main.MyContract(12, 123)
+    admin = sp.test_account("Administrator")
+    scenario.show(admin.address)
+    contract_metadata = sp.scenario_utils.metadata_of_url(
+            "ipfs://QmaiAUj1FFNGYTu8rLBjc3eeN9cSKwaF8EGMBNDmhzPNFd"
+        )
+    c1 = main.Academy(admin.address, contract_metadata)
     scenario += c1
-
-    # And call some of its entrypoints
-    c1.myEntryPoint(12)
-    c1.myEntryPoint(13)
-    c1.myEntryPoint(14)
-    c1.myEntryPoint(50)
-    c1.myEntryPoint(50)
-    c1.myEntryPoint(50, _valid=False)  # this is expected to fail
-    # Finally, we check its final storage
-    scenario.verify(c1.data.myParameter1 == 151)
-
-    # We can define another contract using the current state of c1
-    c2 = main.MyContract(1, c1.data.myParameter1)
-    scenario += c2
-    scenario.verify(c2.data.myParameter2 == 151)
