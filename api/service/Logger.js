d98ce7201a7b01e4cf47ab4820194f34135c7991
tlacloc <desneruda@gmail.com>
2024-09-02 19:15:31 -0600

    refactor: Update pino logger configuration

--- api/service/Logger.js
+++ api/service/Logger.js
@@ -1,9 +1,13 @@
 const pino = require('pino')
-const pretty = require('pino-pretty')
 const level = process.env.LOG_LEVEL || 'info'
-const stream = pretty({
-  colorize: true
+const logger = pino({
+  level,
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      colorize: true
+    }
+  }
 })
-const logger = pino({ level }, stream)
 
 module.exports = logger
