0db3a104c6f6e3c79fc1d3b1fd6b637f8739ca57
tlacloc <desneruda@gmail.com>
2024-09-05 11:37:18 -0600

    feat: Add error handling to calculateTax method in Payment class
    This commit adds error handling to the calculateTax method in the Payment class of the Stripe.js file. It ensures that if there is an error while calculating tax, an InternalServerError is thrown. This improves the reliability and robustness of the tax calculation functionality in the application.

--- api/test/service/stripe.test.js
+++ api/test/service/stripe.test.js
@@ -1,47 +1,96 @@
 'use strict'
+const { faker } = require('@faker-js/faker')
 
-const { test } = require('tap')
+const { describe, test, before } = require('node:test')
+const assert = require('node:assert')
 const Stripe = require('../../service/Stripe.js')
-const stripe = new Stripe()
-let customerId = null
-test('stripe works standalone', async (t) => {
-  t.ok(stripe)
-  t.end()
-})
 
-test('Creates a customer', async (t) => {
-  const customer = await stripe.createCustomer({
-    email: 'erick@echolabs.co',
-    name: 'Erick'
+describe('Stripe', () => {
+  test('Stripe service works standalone', async (t) => {
+    // arrange
+    // act
+    const stripe = new Stripe()
+    // assert
+    assert.ok(stripe)
   })
-  customerId = customer.id
-  t.ok(customer)
-  t.end()
 })
 
-test('paymentIntent returns a valid paymentIntent', async (t) => {
-  const paymentIntent = await stripe.paymentIntent(5000, 'usd', ['card'], 'email@example.com')
-  t.ok(paymentIntent)
-  t.type(paymentIntent, 'object')
-  t.end()
+// describe.skip('Test Stripe service - Customers', () => {
+//   let stripe = null
+//   // const customerId = null
+//   before(() => {
+//     stripe = new Stripe()
+//   })
+
+//   test('Creates a customer', async (t) => {
+//     // arrange
+
+//     // act
+//     // const customer = await stripe.createCustomer({
+//     //   email: 'erick@echolabs.co',
+//     //   name: 'Erick'
+//     // })
+
+//     // assert
+//   })
+// })
+
+describe('Test Stripe service - Payment intents', () => {
+  let stripe = null
+  before(() => {
+    stripe = new Stripe()
+  })
+
+  test('paymentIntent returns a valid paymentIntent', async (t) => {
+    // arrange
+    const amount = 5000
+    const currency = 'usd'
+    const paymentMethodTypes = ['card']
+    const email = faker.internet.email()
+
+    // act
+    const paymentIntent = await stripe.paymentIntent(
+      amount,
+      currency,
+      paymentMethodTypes,
+      email
+    )
+
+    // assert
+    assert.ok(paymentIntent)
+  })
 })
 
-test('paymentIntent returns a valid paymentIntent', async (t) => {
-  const paymentIntent = await stripe.paymentIntent(5000, 'usd', ['card'], 'email@example.com')
-  t.ok(paymentIntent)
-  t.type(paymentIntent, 'object')
-  t.end()
-})
+describe('Test Stripe service - Taxes', () => {
+  let stripe = null
+  before(() => {
+    stripe = new Stripe()
+  })
 
-test('Retrieves tax settings', async (t) => {
-  const taxSettings = await stripe.retrieveTaxSettings()
-  t.ok(taxSettings)
-  t.type(taxSettings, 'object')
-  t.ok(taxSettings.taxCode)
-  t.end()
-})
+  test('Retrieves tax settings', async (t) => {
+    // arrange
 
-test('Calculates tax', async (t) => {
-  await stripe.calculateTax(5000, 'usd', 'TEST')
-  t.end()
+    // act
+    const taxSettings = await stripe.retrieveTaxSettings()
+
+    // assert
+    assert.ok(taxSettings)
+    assert.ok(taxSettings.taxCode)
+  })
+
+  test('Calculates tax without a customer', async (t) => {
+    // arrange
+    let failed = false
+    let errorMessages = ''
+    // act
+    try {
+      await stripe.calculateTax(5000, 'usd', 'TEST')
+    } catch (err) {
+      failed = true
+      errorMessages = err.message
+    }
+    // assert
+    assert.ok(failed)
+    assert.equal(errorMessages, 'Failed to calculate tax')
+  })
 })
