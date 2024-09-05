049eb0b84f105aa6ecaaed1a33b5165c0d34202c
Datzel <davemg71@hotmail.com>
2024-09-04 20:32:17 -0600

    Settings password view, created

---
+++ ui/pages/settings/password/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="tw-bg-gray-50 tw-py-10">
+    <div class="tw-container">
+      <div class="tw-grid tw-grid-cols-12 tw-gap-4">
+        <div class="tw-col-span-12 md:tw-col-span-4 lg:tw-col-start-3 lg:tw-col-span-2">
+          <settings-nav-bar />
+        </div>
+
+        <div class="tw-col-span-12 md:tw-col-span-8 lg:tw-col-start-5 lg:tw-col-span-6">
+          <div class="tw-bg-white tw-rounded tw-shadow tw-p-4">
+            <settings-change-password-form :user-id="$auth.user.id" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
