b069b35597d5977ed7e6c8d16801a18de4781d6c
Cristhian Diaz <albertocenaa@gmail.com>
2024-09-06 12:31:51 -0500

    Update teachers' profile pictures URLs (#443)
    * Update teachers' profile pictures URLs
    
    * Update teachers' profile pictures URLs
    
    * Update teachers' profile pictures URLs

---
+++ hasura/migrations/academy_db/1725581237165_remove_pinata_url/down.sql
@@ -0,0 +1,15 @@
+
+-- Ilithya
+UPDATE teachers
+SET pfp = E'https://moccasin-perfect-trout-941.mypinata.cloud/ipfs/QmV2PBdksQNvsyMfwxcXVvq4Y22BA8rXWSVmTFnMkhB9FG'
+WHERE id = 'b1c88742-d6a4-4556-8e92-83a7e9374dc5';
+
+-- Heeey
+UPDATE teachers
+SET pfp = E'https://moccasin-perfect-trout-941.mypinata.cloud/ipfs/QmVcpQcNSNhmYdLuTZzgTxmkMtr7qyNQKRnDpwLCiVTTDL'
+WHERE id = 'f99406ba-af32-4bb3-b300-9eb034149ea5';
+
+-- Blind Gallery
+UPDATE teachers
+SET pfp = E'https://moccasin-perfect-trout-941.mypinata.cloud/ipfs/QmQaWUwSojC4rvS3pyq4AU4ocwjwb1q87nNLjHmwUCqoks'
+WHERE id = '6332eb59-d69a-4700-815f-c85f27ac73ce';
