510aec6871b0cf3fb6cbf778b57bc1d9149b8996
crissthiandi <albertocenaa@gmail.com>
2024-09-05 19:04:10 -0600

    Update teachers' profile pictures URLs

---
+++ hasura/migrations/academy_db/1725581237165_remove_pinata_url/up.sql
@@ -0,0 +1,14 @@
+-- ilithya
+UPDATE teachers_user
+SET pfp = E'https://blind-gallery.infura-ipfs.io/ipfs/QmV2PBdksQNvsyMfwxcXVvq4Y22BA8rXWSVmTFnMkhB9FG'
+WHERE id = 'b1c88742-d6a4-4556-8e92-83a7e9374dc5';
+
+-- Heeey
+UPDATE teachers_user
+SET pfp = E'https://blind-gallery.infura-ipfs.io/ipfs/QmVcpQcNSNhmYdLuTZzgTxmkMtr7qyNQKRnDpwLCiVTTDL'
+WHERE id = 'f99406ba-af32-4bb3-b300-9eb034149ea5';
+
+-- Blind Gallery
+UPDATE teachers_user
+SET pfp = E'https://blind-gallery.infura-ipfs.io/ipfs/QmQaWUwSojC4rvS3pyq4AU4ocwjwb1q87nNLjHmwUCqoks'
+WHERE id = '6332eb59-d69a-4700-815f-c85f27ac73ce';
