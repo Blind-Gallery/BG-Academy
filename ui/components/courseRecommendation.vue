12b406b845b7c569fb494097aeea7b60ab7a2cab
Datzel <davemg71@hotmail.com>
2024-09-09 11:35:01 -0600

    styles improvements

--- ui/components/courseRecommendation.vue
+++ ui/components/courseRecommendation.vue
@@ -26,29 +26,31 @@ export default {
 </script>
 
 <template>
-  <div class="tw-flex tw-rounded tw-shadow tw-flex-col tw-gap-2 tw-p-8 tw-relative tw-h-[250px] tw-justify-between">
+  <div class="tw-flex tw-rounded tw-shadow tw-flex-col tw-gap-2 tw-pb-4 tw-px-4 tw-pt-6 tw-relative tw-h-[250px] tw-justify-between">
     <div class="tw-w-[40px] tw-h-[40px] tw-rounded-full tw-top-[-9%] tw-left-[10%] tw-shadow-sm tw-bg-cyan-500 tw-text-white tw-flex tw-items-center tw-justify-center tw-absolute">
       <Icon icon="material-symbols-light:format-quote" width="1.5rem" height="1.5rem" style="color: white" />
     </div>
     <div>
-      <p class="tw-text-gray-500 tw-m-0 tw-text-sm">
+      <p class="tw-text-gray-500 tw-m-0 tw-text-xs tw-whitespace-pre-line">
         {{ quote }}
       </p>
     </div>
 
     <div class="tw-flex tw-items-center tw-justify-between tw-border-t-2 tw-pt-2">
-      <div class="tw-flex tw-items-center tw-gap-2 tw-text-ellipsis tw-truncate">
+      <div class="tw-flex tw-items-center tw-items-center tw-gap-2 tw-text-ellipsis tw-truncate">
         <div>
           <img class="tw-w-[30px] tw-max-w-[30px]  tw-rounded-full tw-overflow-hidden tw-object-cover" :src="pfp" alt="Profile picture">
         </div>
-
-        <h6 class="tw-text-sm tw-text-gray-500 tw-truncate">
-          {{ name }}
-        </h6>
+        <div class="tw-flex tw-flex-col">
+          <h6 class="tw-text-sm  tw-truncate tw-m-0">
+            {{ name }}
+          </h6>
+          <span class="tw-text-xs tw-text-gray-500">{{ rol }}</span>
+        </div>
       </div>
-      <NuxtLink :to="social" target="_blank">
+      <a :href="social" target="_blank">
         <Icon icon="ri:twitter-x-line" width="1rem" height="1rem" />
-      </NuxtLink>
+      </a>
     </div>
   </div>
 </template>
