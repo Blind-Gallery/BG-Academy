d2b92172c93b018b2d5787ad2f324deab805c808
crissthiandi <albertocenaa@gmail.com>
2024-09-10 16:59:38 -0600

    feat: Add shareToTwitter method for sharing to Twitter

--- ui/services/share/index.js
+++ ui/services/share/index.js
@@ -21,4 +21,8 @@ export default class ShareSocialMediaService {
   shareToWarpcast (msj, urlEmbed) {
     window.open(`https://warpcast.com/~/compose?text=${msj}&embeds[]=${urlEmbed}`)
   }
+
+  shareToTwitter (msj, urlEmbed) {
+    window.open(`http://twitter.com/share?url=${urlEmbed}&via=trucsweb&image-src=${msj}&text=${title}`)
+  }
 }
