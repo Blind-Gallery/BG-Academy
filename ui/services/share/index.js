8fb461a13283bdc04aab96591dee3f362ba4d373
Datzel <davemg71@hotmail.com>
2024-09-11 16:40:07 -0600

    via trucsweb, removed

--- ui/services/share/index.js
+++ ui/services/share/index.js
@@ -3,7 +3,7 @@ export default class ShareSocialMediaService {
     window.open(`https://warpcast.com/~/compose?text=${msj}&embeds[]=${urlEmbed}`)
   }
 
-  shareToTwitter (msj, urlEmbed) {
-    window.open(`http://twitter.com/share?url=${urlEmbed}&via=trucsweb&text=${msj}`)
+  shareToTwitter(msj, urlEmbed) {
+    window.open(`http://twitter.com/share?url=${urlEmbed}&text=${msj}`)
   }
 }
