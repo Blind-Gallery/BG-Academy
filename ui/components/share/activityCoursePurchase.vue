1cecb7525fbd24d403ab7c483491716797ab264e
crissthiandi <albertocenaa@gmail.com>
2024-09-10 23:57:06 -0600

    feat: Update social media sharing functionality in activityCoursePurchase.vue

--- ui/components/share/activityCoursePurchase.vue
+++ ui/components/share/activityCoursePurchase.vue
@@ -1,25 +1,74 @@
+<script>
+import ShareSocialMediaService from '@/services/share'
+const shareSocialMediaService = new ShareSocialMediaService('coursePurchased')
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    instructor: {
+      type: String,
+      required: true
+    },
+    courseId: {
+      type: String,
+      required: true
+    }
+  },
+  data () {
+    return {
+      msg: `I just claimed my certificate of completion to ${this.title} by ${this.instructor}`
+    }
+  },
+  methods: {
+    getUrlToBuy () {
+      return window.location.origin + '/buyCourse/' + this.courseId
+    },
+    shareToTwitter () {
+      shareSocialMediaService.shareToTwitter(this.msg, this.getUrlToBuy())
+    },
+    shareToInstagram () {
+      shareSocialMediaService.shareToInstagram()
+    },
+    shareToWarpcast () {
+      shareSocialMediaService.shareToWarpcast(this.msg, this.getUrlToBuy())
+    }
+  }
+}
+
+</script>
 <template>
   <div>
     <h4 class="tw-text-cyan-500 tw-mb-4">
       Share your activity
     </h4>
-    <div class="tw-border tw-flex tw-flex-col tw-rounded tw-p-4 tw-gap-4">
+    <div id="coursePurchased" class="tw-border tw-flex tw-flex-col tw-rounded tw-p-4 tw-gap-4">
       <img src="https://moccasin-perfect-trout-941.mypinata.cloud/ipfs/QmQ6xcW8YcPdCVxM82mAjc9qSTmyks3buzaxsynjQZndby">
       <p class="tw-m-0">
-        I just enrolled in <span class="tw-font-bold tw-text-cyan-500">'Introduction to Pixel Shaders'</span> by ilithya! Check it out below
+        I just enrolled in <span class="tw-font-bold tw-text-cyan-500">'{{ title }}'</span> by {{ instructor }}! Check it out below
       </p>
     </div>
     <hr>
     <div>
       <span class="tw-font-bold tw-text-xs">Share this link via:</span>
       <div class="tw-flex tw-gap-2 tw-items-center">
-        <div class="tw-flex tw-items-center tw-justify-center tw-border tw-p-2 tw-rounded tw-w-[40px] tw-h-[40px] hover:tw-bg-cyan-500 tw-duration-200 tw-ease-in-out tw-cursor-pointer hover:tw-text-white hover:tw-border-0">
+        <div
+          class="tw-flex tw-items-center tw-justify-center tw-border tw-p-2 tw-rounded tw-w-[40px] tw-h-[40px] hover:tw-bg-cyan-500 tw-duration-200 tw-ease-in-out tw-cursor-pointer hover:tw-text-white hover:tw-border-0"
+          @click="shareToWarpcast"
+        >
           <Icon icon="simple-icons:farcaster" width="1rem" height="1rem" />
         </div>
-        <div class="tw-flex tw-items-center tw-justify-center tw-border tw-p-2 tw-rounded tw-w-[40px] tw-h-[40px] hover:tw-bg-cyan-500 tw-duration-200 tw-ease-in-out tw-cursor-pointer hover:tw-text-white hover:tw-border-0">
+        <div
+          class="tw-flex tw-items-center tw-justify-center tw-border tw-p-2 tw-rounded tw-w-[40px] tw-h-[40px] hover:tw-bg-cyan-500 tw-duration-200 tw-ease-in-out tw-cursor-pointer hover:tw-text-white hover:tw-border-0"
+          @click="shareToTwitter"
+        >
           <Icon icon="ri:twitter-x-line" width="1rem" height="1rem" />
         </div>
-        <div class="tw-flex tw-items-center tw-justify-center tw-border tw-p-2 tw-rounded tw-w-[40px] tw-h-[40px] hover:tw-bg-cyan-500 tw-duration-200 tw-ease-in-out tw-cursor-pointer hover:tw-text-white hover:tw-border-0">
+        <div
+          class="tw-flex tw-items-center tw-justify-center tw-border tw-p-2 tw-rounded tw-w-[40px] tw-h-[40px] hover:tw-bg-cyan-500 tw-duration-200 tw-ease-in-out tw-cursor-pointer hover:tw-text-white hover:tw-border-0"
+          @click="shareToInstagram"
+        >
           <Icon icon="mdi:instagram" width="1rem" height="1rem" />
         </div>
       </div>
