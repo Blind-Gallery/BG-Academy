b5e2eddc51165242ffcb92274a9addecd1fdcab9
crissthiandi <albertocenaa@gmail.com>
2024-09-11 13:30:53 -0600

    feat: Remove unused dependencies and code related to social media sharing functionality

--- ui/services/share/index.js
+++ ui/services/share/index.js
@@ -1,21 +1,6 @@
-import * as htmlToImage from 'html-to-image'
-import { saveAs } from 'file-saver'
-
 export default class ShareSocialMediaService {
   constructor (id) {
     this.documentId = id
-  }
-
-  createImage () {
-    htmlToImage
-      .toBlob(document.getElementById(this.documentId))
-      .then(function (blob) {
-        saveAs(blob, 'academyCoursePassed.jpg')
-      })
-  }
-
-  shareToInstagram () {
-    this.createImage()
   }
 
   shareToWarpcast (msj, urlEmbed) {
