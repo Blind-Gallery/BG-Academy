bc38cfd994c98f9b13da71bf880eab8a2af42ada
crissthiandi <albertocenaa@gmail.com>
2024-09-10 23:43:13 -0600

    feat: Update social media sharing functionality in activityClaimCertificate.vue

--- ui/components/share/activityClaimCertificate.vue
+++ ui/components/share/activityClaimCertificate.vue
@@ -1,6 +1,6 @@
 <script>
 import ShareSocialMediaService from '@/services/share'
-const shareSocialMediaService = new ShareSocialMediaService('testPassed')
+const shareSocialMediaService = new ShareSocialMediaService('certificateClaimed')
 export default {
   props: {
     title: {
@@ -44,25 +44,36 @@ export default {
       Share your activity
     </h4>
     <div class="tw-flex tw-flex-col tw-gap-2">
-      <div class="tw-rounded tw-p-4 tw-flex tw-justify-center tw-items-center tw-bg-cyan-300">
-        <img class="tw-h-[200px]" src="https://moccasin-perfect-trout-941.mypinata.cloud/ipfs/QmbvGpZbCYPxPJhRax5vosX646ZtUmmeLqTqxPqFad3kLP">
-      </div>
-      <div>
-        <p class="tw-m-0">
-          I just claimed my certificate of completion to <span class="tw-font-bold tw-text-cyan-500">{{ title }}</span> by {{ instructor }}
-        </p>
+      <div id="certificateClaimed">
+        <div class="tw-rounded tw-p-4 tw-flex tw-justify-center tw-items-center tw-bg-cyan-300">
+          <img class="tw-h-[200px]" src="https://moccasin-perfect-trout-941.mypinata.cloud/ipfs/QmbvGpZbCYPxPJhRax5vosX646ZtUmmeLqTqxPqFad3kLP">
+        </div>
+        <div>
+          <p class="tw-m-0">
+            I just claimed my certificate of completion to <span class="tw-font-bold tw-text-cyan-500">{{ title }}</span> by {{ instructor }}
+          </p>
+        </div>
       </div>
       <hr>
       <div>
         <span class="tw-font-bold tw-text-xs">Share this link via:</span>
         <div class="tw-flex tw-gap-2 tw-items-center">
-          <div class="tw-flex tw-items-center tw-justify-center tw-border tw-p-2 tw-rounded tw-w-[40px] tw-h-[40px] hover:tw-bg-cyan-500 tw-duration-200 tw-ease-in-out tw-cursor-pointer hover:tw-text-white hover:tw-border-0">
+          <div
+            class="tw-flex tw-items-center tw-justify-center tw-border tw-p-2 tw-rounded tw-w-[40px] tw-h-[40px] hover:tw-bg-cyan-500 tw-duration-200 tw-ease-in-out tw-cursor-pointer hover:tw-text-white hover:tw-border-0"
+            @click="shareToWarpcast"
+          >
             <Icon icon="simple-icons:farcaster" width="1rem" height="1rem" />
           </div>
-          <div class="tw-flex tw-items-center tw-justify-center tw-border tw-p-2 tw-rounded tw-w-[40px] tw-h-[40px] hover:tw-bg-cyan-500 tw-duration-200 tw-ease-in-out tw-cursor-pointer hover:tw-text-white hover:tw-border-0">
+          <div
+            class="tw-flex tw-items-center tw-justify-center tw-border tw-p-2 tw-rounded tw-w-[40px] tw-h-[40px] hover:tw-bg-cyan-500 tw-duration-200 tw-ease-in-out tw-cursor-pointer hover:tw-text-white hover:tw-border-0"
+            @click="shareToTwitter"
+          >
             <Icon icon="ri:twitter-x-line" width="1rem" height="1rem" />
           </div>
-          <div class="tw-flex tw-items-center tw-justify-center tw-border tw-p-2 tw-rounded tw-w-[40px] tw-h-[40px] hover:tw-bg-cyan-500 tw-duration-200 tw-ease-in-out tw-cursor-pointer hover:tw-text-white hover:tw-border-0">
+          <div
+            class="tw-flex tw-items-center tw-justify-center tw-border tw-p-2 tw-rounded tw-w-[40px] tw-h-[40px] hover:tw-bg-cyan-500 tw-duration-200 tw-ease-in-out tw-cursor-pointer hover:tw-text-white hover:tw-border-0"
+            @click="shareToInstagram"
+          >
             <Icon icon="mdi:instagram" width="1rem" height="1rem" />
           </div>
         </div>
