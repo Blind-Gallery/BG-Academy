3aee7970418badafee5a56aa24a89e6743c66a78
Datzel <davemg71@hotmail.com>
2024-09-12 09:54:16 -0600

    style fixed for checkbox wrappers

--- ui/plugins/formulate.config.js
+++ ui/plugins/formulate.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-dupe-keys */
 /* eslint-disable space-before-function-paren */
 import { en } from '@braid/vue-formulate-i18n'
 
@@ -13,14 +14,34 @@ export default {
       switch (context.classification) {
         case 'button':
           return 'tw-bg-cyan-500 hover:tw-bg-cyan-600 tw-ease-in-out tw-duration-200 tw-px-3 tw-py-2 tw-text-white  tw-rounded tw-w-full'
+        case 'box':
+          return 'tw-flex tw-border tw-p-2 tw-rounded tw-cursor-pointer hover:tw-border-gray-300 tw-ease-in-out tw-duration-200 tw-items-center'
         default:
           return 'tw-w-full tw-px-3 tw-py-2 tw-border tw-border-gray-300 tw-border-box tw-rounded tw-leading-none focus:tw-border-cyan-500 tw-text-sm tw-outline-none'
       }
     },
-    label: 'tw-text-sm',
+
+    label(context) {
+      switch (context.classification) {
+        case 'box':
+          return 'tw-text-gray-500 tw-text-sm tw-m-0'
+        default:
+          return 'tw-text-sm'
+      }
+    },
+
+    wrapper(context) {
+      switch (context.classification) {
+        case 'box':
+          return 'tw-flex tw-gap-2 tw-items-center'
+        default:
+          return 'tw-mb-2'
+      }
+    },
+
     help: 'tw-text-xs tw-text-gray-500',
     error: 'tw-text-xs tw-text-red-500',
-    formErrors: 'tw-text-xs tw-text-red-500 tw-mb-2',
-    wrapper: 'tw-mb-1'
+    formErrors: 'tw-text-xs tw-text-red-500 tw-mb-2'
+
   }
 }
