46b2ada0c9e58e5fce1cafe7a366eb4bef0323d0
crissthiandi <albertocenaa@gmail.com>
2024-09-12 23:22:39 -0600

    feat: Update Tezos RPC endpoints to use new URLs

---
+++ hasura/migrations/academy_db/1726188357442_update_heey_chapter_info/up.sql
@@ -0,0 +1,69 @@
+-- Module 0
+-- chapter 1
+UPDATE chapters
+SET info = E'In this chapter, heeey discusses generative art, which uses algorithms to create unique artworks that challenge traditional notions of creativity and authorship, including styles like algorithmic abstraction and organic art.'
+WHERE id = 'c88ce423-43c8-4540-9558-a74b499d134f';
+
+-- chapter 2
+UPDATE chapters
+SET info = E'In this chapter, heeey explores algorithms in generative art, describing them as rule sets that blend precision with variability, similar to recipes where coding details shape the creative process and outcomes.'
+WHERE id = 'df7ba392-86a7-442f-84ab-f146cddd0cbc';
+
+-- chapter 3
+UPDATE chapters
+SET info = E'In this chapter, heeey examines how NFTs revolutionize generative art by offering digital ownership and authenticity through blockchain, transforming the ways artworks are created, owned, and traded.'
+WHERE id = '7cf010fd-c614-4900-8e59-5d494b465aee';
+
+-- chapter 4
+UPDATE chapters
+SET info = E'In this chapter, heeey contrasts generative code art, which uses artist-designed algorithms, with generative AI art, which relies on machine learning. Both explore creativity but differ in their execution and the role of human versus machine input.'
+WHERE id = 'c95f74a1-b210-4b6d-86db-96caf8bf8ff8';
+
+-- Module 2
+
+-- chapter 1
+UPDATE chapters
+SET info = E'In this chapter, heeey discusses how generative art began with early computer pioneers like Ben Laposky and Frieder Nake, who explored algorithmic and visual outputs in the 20th century.'
+WHERE id = 'e56c1ca9-bb2b-4205-8313-e5165a886bed';
+
+-- chapter 2
+UPDATE chapters
+SET info = E'In this chapter, heeey explores how algorists and the digital revolution of the 1980s-90s expanded generative art, democratizing creative possibilities for artists.'
+WHERE id = '2b53d3d9-668c-49f8-803c-9504fb0ecfdf';
+
+-- chapter 3
+UPDATE chapters
+SET info = E'In this chapter, heeey highlights pioneers like Nake, Molnar, and Cohen, who used early computers and algorithms to push the boundaries of art and shape the field’s development.'
+WHERE id = '8633204d-e5b3-4cf8-aaa0-e23fe4f0d7af';
+
+-- Module 3
+-- chapter 1
+UPDATE chapters
+SET info = E'In this chapter, heeey discusses how online communities facilitated generative art\'s growth through forums and social media, driving innovation via Net.art, Processing, and the Demoscene.'
+WHERE id = 'c5f8d254-8e7c-44b8-908d-5193d7125eb5';
+
+-- chapter 2
+UPDATE chapters
+SET info = E'In this chapter, heeey explores how generative art influenced fields like architecture and fashion, with tools like p5.js and ShaderToy in the 2010s making creative coding more accessible.'
+WHERE id = '0a842cc9-30b1-443a-a5b1-ec8e623889da';
+
+-- chapter 3
+UPDATE chapters
+SET info = E'In this chapter, heeey highlights artists and educators like Casey Reas, Daniel Shiffman, Lauren Lee McCarthy, Golan Levin, Zach Lieberman, and Marius Watz, who advanced generative art through their innovative work and teaching.'
+WHERE id = '002542e0-6865-4556-b6d0-5723bbd788de';
+
+-- Module 4
+-- chapter 1
+UPDATE chapters
+SET info = E'In this chapter, heeey examines how blockchain technology enabled the creation and trading of unique generative art via NFTs, with early projects like Cryptokitties and CryptoPunks paving the way. Platforms like OpenSea democratized access.'
+WHERE id = '5c1213f8-6946-451c-92be-7ed9e14e9d85';
+
+-- chapter 2
+UPDATE chapters
+SET info = E'In this chapter, heeey discusses how long-form generative art, popularized by Art Blocks in 2020, emphasizes variability and emergent behavior. Platforms on Tezos and new gallery models have broadened artistic experimentation.'
+WHERE id = 'de689e53-86f0-4da9-961b-d588fa2e86c7';
+
+-- chapter 3
+UPDATE chapters
+SET info = E'In this chapter, heeey explores the works of notable artists such as Tyler Hobbs with Fidenza, Dmitri Cherniak with Ringers, and William Mapan with Dragons, who contribute to the diverse facets of generative art from intricate patterns to organic forms.'
+WHERE id = '3079e158-4009-4992-b5ff-656d9cee1639';
