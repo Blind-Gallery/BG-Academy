859f1d7a68e3c560c2329f757bbe2f1f7d39a4a1
Cristhian Diaz <albertocenaa@gmail.com>
2024-09-17 12:36:07 -0500

    Migrations/add priority column (#455)
    * add columns to order questions
    
    * Remove unnecessary columns from public_questions and question_options tables
    
    * Remove unnecessary columns from public_questions and question_options tables
    
    * feat: Update priority of questions in up.sql
    
    * feat: Update priority of questions in down.sql

---
+++ hasura/migrations/academy_db/1726256674359_add_order_in_questions_ilithya/up.sql
@@ -0,0 +1,266 @@
+
+-- A shader in WebGL is...
+UPDATE questions
+SET priority = 1
+WHERE id = '76c83b70-e59f-446a-bf31-8a96b6eff6f2';
+
+-- Shaders run in the:...
+UPDATE questions
+SET priority = 2
+WHERE id = 'cd036c83-7954-4c91-a6fc-c1810f598d21';
+
+-- Pixel shaders are commonly known...
+UPDATE questions
+SET priority = 3
+WHERE id = 'ec07a421-b301-4682-a991-6ccec44b72b9';
+
+-- A pixel shader receives a position from...
+UPDATE questions
+SET priority = 4
+WHERE id = 'e717f9df-c221-4349-9918-92bea1df1c26';
+
+-- A pixel shader returns a color in the...
+UPDATE questions
+SET priority = 5
+WHERE id = 'e0ed6aac-ef92-4562-8155-31feb0660cb4';
+
+-- GLSL syntax is based on the programming...
+UPDATE questions
+SET priority = 6
+WHERE id = '948536b7-0b1c-4eb0-a142-6304c5bca4ed';
+
+-- The following is an example of a... variable
+UPDATE questions
+SET priority = 7
+WHERE id = 'c32718bc-2d26-4ad7-8069-8a9f7bb5fb8a';
+
+-- The following is an example of a built-in type...
+UPDATE questions
+SET priority = 8
+WHERE id = '743653ba-54a1-4f0d-9143-1951bed02e52';
+
+-- The following is an example of a built-in function
+UPDATE questions
+SET priority = 9
+WHERE id = '9eb5b186-674a-4dbc-9ee9-7540ed5e8c5d';
+
+-- A UV space represents a...
+UPDATE questions
+SET priority = 10
+WHERE id = '5b478462-d56c-428d-b21c-be0eed5a7357';
+
+-- A UV space is set by the coordinates...
+UPDATE questions
+SET priority = 11
+WHERE id = '30c61044-d3ad-4b6f-874b-e84774828186';
+
+-- The following is an equation to normalize...
+UPDATE questions
+SET priority = 12
+WHERE id = '193256f4-0949-43fc-930d-6db745b04eea';
+
+-- The following is an equation to normalize the screen coordinates between -0.5 and 0.5:
+UPDATE questions
+SET priority = 13
+WHERE id = 'dd61e8e3-d6f1-4b41-bfe5-2c41b1c93839';
+
+-- The RGB color model one can think ...
+UPDATE questions
+SET priority = 14
+WHERE id = 'bb7c86ac-1563-46ad-965e-352fe26a735a';
+
+-- The yellow color represented in RGB percentage...
+UPDATE questions
+SET priority = 15
+WHERE id = '51281e94-2f62-4765-b8c7-2f2af47bc685';
+
+-- The bright sea green color represented in RGB ...
+UPDATE questions
+SET priority = 16
+WHERE id = '595d7687-0a24-47e0-ad2f-e9494fcb3213';
+
+-- Luminosity gradients have:...
+UPDATE questions
+SET priority = 17
+WHERE id = '138e1257-ba48-4a0c-aab9-a9743820bd72';
+
+-- The following is an equation to output an horizontal red luminosity gradient...
+UPDATE questions
+SET priority = 18
+WHERE id = 'efd5e519-2157-48c4-bb53-c3f0a0008614';
+
+-- The following is an equation to output an horizontal blue luminosity gradient...
+UPDATE questions
+SET priority = 19
+WHERE id = 'fbbe835d-54a9-479c-ad67-1c5040edce82';
+
+-- Saturation Continuum...
+UPDATE questions
+SET priority = 20
+WHERE id = '5f1117b2-88e8-485a-973f-55f9bcf465c1';
+
+-- This is a GLSL built-in function used to make...
+UPDATE questions
+SET priority = 21
+WHERE id = '0601d16e-b87d-4801-a512-3b282f61e018';
+
+-- The following is an equation to output an horizontal saturation...
+UPDATE questions
+SET priority = 22
+WHERE id = '5390ddc8-d655-439a-8493-8703b61b5c46';
+
+-- The following is an equation to output a vertical saturation...
+UPDATE questions
+SET priority = 23
+WHERE id = 'e2b2ec61-1f48-4a17-9a49-ace9b3504886';
+
+-- The space in a pixel aka fragment shader...
+UPDATE questions
+SET priority = 24
+WHERE id = '78d95c3f-f708-447e-b865-2811f76bc051';
+
+-- The following is a calculation to push the horizontal axis of a pixel ...
+UPDATE questions
+SET priority = 25
+WHERE id = 'cac0cba4-46c6-4701-9d68-747e2596277c';
+
+-- These two GLSL built-in functions can be used interchangeably to...
+UPDATE questions
+SET priority = 26
+WHERE id = '99aaadb1-28ce-4c9f-9d28-06e3ca0f62a3';
+
+-- This GLSL built-in function can be used to make a well-defined...
+UPDATE questions
+SET priority = 27
+WHERE id = 'e764abc4-f3e7-4061-8aed-dbb0a583348b';
+
+-- The following is a uv equation to draw a circle with an aspect ratio...
+UPDATE questions
+SET priority = 28
+WHERE id = 'a4f57813-f481-4a8b-9154-657511480c1e';
+
+-- These two GLSL built-in functions can be used together...
+UPDATE questions
+SET priority = 29
+WHERE id = '4e26542a-4946-4237-b7fc-8274917160fa';
+
+-- The following is an equation to draw a well-defined (sharp) square at the center ...
+UPDATE questions
+SET priority = 30
+WHERE id = 'a3d8e4eb-5f76-4244-9853-c201491d989c';
+
+-- The following is an equation to draw a well-defined (sharp) triangle ...
+UPDATE questions
+SET priority = 31
+WHERE id = '5f492f84-e647-4241-bf03-b96b630b94c8';
+
+-- The following is an equation to draw a reversed (peak facing south) ...
+UPDATE questions
+SET priority = 32
+WHERE id = 'f4ab43c9-af5c-4dbc-9dae-fc1a574e6ad5';
+
+-- The following is an equation to draw a well-defined (sharp) ring at the center ...
+UPDATE questions
+SET priority = 33
+WHERE id = 'ba91fa74-bff8-41c8-8212-3ee92ae06556';
+
+-- This GLSL built-in function can be used to make a solid ring ...
+UPDATE questions
+SET priority = 34
+WHERE id = '6f8a3e53-8095-4177-8f80-7bae79246db5';
+
+-- These two GLSL built-in functions can be used interchangeably...
+UPDATE questions
+SET priority = 35
+WHERE id = '0707bba0-d35f-4be6-9de2-e8d5de6e0927';
+
+-- The following calculation creates a zoom-in effect in the uv space:
+UPDATE questions
+SET priority = 36
+WHERE id = 'edd72c7f-6125-4927-9478-f1de3e35f4ca';
+
+-- The following calculation creates a zoom-out effect in the uv space:
+UPDATE questions
+SET priority = 37
+WHERE id = '55fc9cdd-85d9-4d78-828f-61b9eace2683';
+
+-- The following is an equation to make a fullscreen linear repetition...
+UPDATE questions
+SET priority = 38
+WHERE id = '9d43be05-d75e-4775-9f27-769f93837618';
+
+-- The following is an equation to make a fullscreen linear repetition with circles ...
+UPDATE questions
+SET priority = 39
+WHERE id = 'e76ae3c7-fdc9-4fb2-bbcb-6d56ea0d3ea3';
+
+-- The following is an equation to make a fullscreen grid repetition with centered circles ...
+UPDATE questions
+SET priority = 40
+WHERE id = 'd865cb8f-5be3-44c2-9956-08eefe17ff07';
+
+-- The following is an equation to make a fullscreen grid repetition with squares...
+UPDATE questions
+SET priority = 41
+WHERE id = '46047732-05e2-47dc-aed9-08f835b62941';
+
+-- The following is an equation to output two separated predefined patterns of the same color...
+UPDATE questions
+SET priority = 42
+WHERE id = '584d1ac0-a79f-43b0-a819-591258760a8d';
+
+-- This GLSL built-in function can be used to output two different predefined patterns...
+UPDATE questions
+SET priority = 43
+WHERE id = '4243764f-d99e-4d2b-a938-431d9899acbd';
+
+-- The following is an equation to output two separated predefined patterns with different colors and...
+UPDATE questions
+SET priority = 44
+WHERE id = '5fa55917-16b7-4d81-a01a-7d93ddd75f5a';
+
+-- In the pixel aka fragment shader template used in this course u_time is an example of a type qualifier known...
+UPDATE questions
+SET priority = 45
+WHERE id = '971d26a4-4c94-4bb0-aee6-bbda3be25c5b';
+
+-- The uniforms available in the fragment shader template for this course were defined...
+UPDATE questions
+SET priority = 46
+WHERE id = 'c882d398-8edc-438b-9c4c-9c28c11fea24';
+
+-- u_time is equivalent to a clock counting in...
+UPDATE questions
+SET priority = 47
+WHERE id = 'e39314d6-f025-4209-97a1-83028afcac73';
+
+-- The following is an example of u_time defined as a variable inside the void main() ...
+UPDATE questions
+SET priority = 48
+WHERE id = 'eca92a9f-3e18-4aad-a7d0-bc60fd4115a0';
+
+-- The following is an equation to move a fullscreen linear repetition with vertical lines from left to right continuously...
+UPDATE questions
+SET priority = 49
+WHERE id = 'ca2a00c3-c31b-41db-990e-af5d5ae88dbb';
+
+-- These three GLSL built-in functions can be used to add motion to our sketches with trigonometry...
+UPDATE questions
+SET priority = 50
+WHERE id = '3485268b-8e9c-467f-b259-6215504c9fcc';
+
+-- The following is an equation to move a fullscreen linear repetition with vertical lines back and forth (bouncing) infinitely...
+UPDATE questions
+SET priority = 51
+WHERE id = '4c985a5d-e047-40e4-b8b3-7db16c1e06f2';
+
+-- The following is an equation to move a fullscreen linear repetition with vertical lines...
+UPDATE questions
+SET priority = 52
+WHERE id = 'ac23acd2-cb16-4204-ad4e-27bd86e1c8d7';
+
+-- The GLSL built-in function cos() can be think of as a mirror from the GLSL built-in function...
+UPDATE questions
+SET priority = 53
+WHERE id = '1f83b98a-d3c9-42d0-a196-faf8dbe91bb5';
+
