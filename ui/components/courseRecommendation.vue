33193a8085ec7da9a5a310437076a967ab31ad8e
Tlalocman <desneruda@gmail.com>
2024-09-19 15:19:32 -0600

    refactor: Update courseRecommendation component to use "twitter" and "farcaster" props

--- ui/components/courseRecommendation.vue
+++ ui/components/courseRecommendation.vue
@@ -18,6 +18,11 @@ export default {
       required: false,
       default: ''
     },
+    farcaster: {
+      type: String,
+      required: false,
+      default: ''
+    },
     pfp: {
       type: String,
       required: true
@@ -28,7 +33,9 @@ export default {
 
 <template>
   <div class="tw-flex tw-rounded tw-shadow tw-flex-col tw-gap-2 tw-p-4 tw-relative tw-h-[250px] tw-justify-between">
-    <div class="tw-w-[40px] tw-h-[40px] tw-rounded-full tw-top-[-9%] tw-left-[10%] tw-shadow-sm tw-bg-cyan-500 tw-text-white tw-flex tw-items-center tw-justify-center tw-absolute">
+    <div
+      class="tw-w-[40px] tw-h-[40px] tw-rounded-full tw-top-[-9%] tw-left-[10%] tw-shadow-sm tw-bg-cyan-500 tw-text-white tw-flex tw-items-center tw-justify-center tw-absolute"
+    >
       <Icon icon="material-symbols-light:format-quote" width="1.5rem" height="1.5rem" style="color: white" />
     </div>
     <div>
@@ -40,7 +47,11 @@ export default {
     <div class="tw-flex tw-items-center tw-justify-between tw-border-t-2 tw-pt-2">
       <div class="tw-flex tw-items-center tw-items-center tw-gap-2 tw-text-ellipsis tw-truncate">
         <div>
-          <img class="tw-w-[30px] tw-max-w-[30px]  tw-rounded-full tw-overflow-hidden tw-object-cover" :src="pfp" alt="Profile picture">
+          <img
+            class="tw-w-[30px] tw-max-w-[30px]  tw-rounded-full tw-overflow-hidden tw-object-cover"
+            :src="pfp"
+            alt="Profile picture"
+          >
         </div>
         <div class="tw-flex tw-flex-col">
           <h6 class="tw-text-sm  tw-truncate tw-m-0">
@@ -49,8 +60,11 @@ export default {
           <span class="tw-text-xs tw-text-gray-500">{{ role }}</span>
         </div>
       </div>
-      <a v-if="twitter" :href="twitter" target="_blank">
+      <a v-show="twitter" :href="twitter" target="_blank">
         <Icon icon="ri:twitter-x-line" width="1rem" height="1rem" />
+      </a>
+      <a v-show="farcaster" :href="farcaster" target="_blank">
+        <Icon icon="simple-icons:farcaster" width="1rem" height="1rem" />
       </a>
     </div>
   </div>
