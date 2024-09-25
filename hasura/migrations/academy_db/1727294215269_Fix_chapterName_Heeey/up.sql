21747fb19e51338db037e525ffd874259b3e3b3b
Cristhian Diaz <albertocenaa@gmail.com>
2024-09-25 16:22:13 -0500

    Fix chapter name in "Contemporary Generative Art" section (#477)
    * Fix chapter name in "Contemporary Generative Art" section
    
    * Fix chapter name in "Contemporary Generative Art" section

---
+++ hasura/migrations/academy_db/1727294215269_Fix_chapterName_Heeey/up.sql
@@ -0,0 +1,7 @@
+
+-- -- In the "Contemporary Generative Art" section, there are two videos titled "Breaking Boundaries."
+--  El que esta mal nombrado es el segundo, debería ser "Artists & Educators"
+
+update chapters 
+set title = 'Artists & Educators' 
+where id = '002542e0-6865-4556-b6d0-5723bbd788de';
