17ac1bead21112d3eeadc9549d2e2975f0407175
Cristhian Diaz <albertocenaa@gmail.com>
2024-09-26 10:47:52 -0500

    ++Refactor chapter titles in "Contemporary Generative Art" section (#481)
    * Refactor chapter titles in "Contemporary Generative Art" section
    
    * Refactor chapter titles in "Contemporary Generative Art" section

---
+++ hasura/migrations/academy_db/1727364318294_fix_heeey_title_chapters/up.sql
@@ -0,0 +1,7 @@
+update chapters 
+set title = 'Contemporary Generative Art' 
+where id = 'c5f8d254-8e7c-44b8-908d-5193d7125eb5';
+
+update chapters 
+set title = 'Generative (code) art vs generative (ai) art' 
+where id = 'c95f74a1-b210-4b6d-86db-96caf8bf8ff8';
