afda331c76cf8fd98481e998da5a6672df7372ed
crissthiandi <albertocenaa@gmail.com>
2024-09-26 09:38:46 -0600

    Refactor chapter titles in "Contemporary Generative Art" section

--- hasura/migrations/academy_db/1727364318294_fix_heeey_title_chapters/up.sql
+++ hasura/migrations/academy_db/1727364318294_fix_heeey_title_chapters/up.sql
@@ -4,4 +4,4 @@ where id = 'c5f8d254-8e7c-44b8-908d-5193d7125eb5';
 
 update chapters 
 set title = 'Generative (code) art vs generative (ai) art' 
-where id = '';
+where id = 'c95f74a1-b210-4b6d-86db-96caf8bf8ff8';
