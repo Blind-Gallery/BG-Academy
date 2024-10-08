f36840f5f1cc971548315839dd70c9e52f795a5f
Tlalocman <desneruda@gmail.com>
2024-10-08 16:41:34 -0600

    Erick/feat/ilithya_requests (#484)
    * Add is_countable column to modules table and update necessary records
    
    Refactor the `modules` table in the database by adding a new column `is_countable` with a default value of `true`. Also, update specific modules to be not countable.
    
    * Refactor module count display in buyCourse page
    
    * Refactor module count display in buyCourse page
    
    * feat: add subtitle prop to courseCurriculum component
    
    * feat: Add subtitles to countable modules in public.modules table
    
    This commit adds a new column "subtitle" to the "public.modules" table in the database. The "subtitle" column is used to store the subtitle for countable modules. The commit also updates the subtitles for specific countable modules using their IDs.
    
    Co-authored-by: [Author Name]
    
    * update chapter names
    
    * update chapter names

---
+++ hasura/migrations/academy_db/1728427087093_ilithya_changes/up.sql
@@ -0,0 +1,110 @@
+-- Welcome Module
+UPDATE chapters
+SET title = 'Chapter 1: Course Introduction'
+WHERE id = '3dbd7594-ecd1-487d-a98d-6a915049b455';
+
+-- Basics Module
+UPDATE chapters
+SET title = 'Chapter 1: The Shaders'
+WHERE id = 'b127c9e1-698c-4cd7-a534-c41c87b1893b';
+
+UPDATE chapters
+SET title = 'Chapter 2: Pixel Shaders'
+WHERE id = '1631e746-0efc-4e49-becb-6a0c6c44a749';
+
+UPDATE chapters
+SET title = 'Chapter 3: GLSL Syntax'
+WHERE id = 'e7387a97-af99-4976-87b9-6510235c89d8';
+
+UPDATE chapters
+SET title = 'Chapter 4: UV Space'
+WHERE id = '9c294d3f-4df5-4532-bb81-ce6621b8dc13';
+
+UPDATE chapters
+SET title = 'Chapter 5: UV Centering & Aspect Ratio'
+WHERE id = '5c0eb529-985d-44de-b83d-78360313efae';
+
+-- Color Module
+UPDATE chapters
+SET title = 'Chapter 1: RGB Color Model'
+WHERE id = 'ea905e11-a99c-4148-8705-338779763f35';
+
+UPDATE chapters
+SET title = 'Chapter 2: Luminosity Gradients'
+WHERE id = 'e22d8840-239e-42c9-b6f7-dbc76a656b1a';
+
+UPDATE chapters
+SET title = 'Chapter 3: Saturation Continuum Gradients'
+WHERE id = '40cbea88-12fd-4e7f-a374-6cf8440d9603';
+
+-- Geometries Module
+UPDATE chapters
+SET title = 'Chapter 1: Bending Space'
+WHERE id = 'e982fbb2-7c2b-48a8-969b-7d7fbcc313f4';
+
+UPDATE chapters
+SET title = 'Chapter 2: Circle Pt 1'
+WHERE id = '0e6008a4-5402-41ad-bec4-7d4237e46400';
+
+UPDATE chapters
+SET title = 'Chapter 3: Circle Pt 2'
+WHERE id = '974053b1-077c-4fca-bc2c-131c9bc5fcb5';
+
+UPDATE chapters
+SET title = 'Chapter 4: Circle Pt 3'
+WHERE id = '3c0d4bf4-bd5d-4bb6-bf58-730e0d242ce6';
+
+UPDATE chapters
+SET title = 'Chapter 5: Square Pt 1'
+WHERE id = 'e7a4c97a-619e-4cda-9d9f-fb080a6bd23b';
+
+UPDATE chapters
+SET title = 'Chapter 6: Square Pt 2'
+WHERE id = 'b57e8b32-780f-47e5-9a9d-3a12ee6a7fd6';
+
+UPDATE chapters
+SET title = 'Chapter 7: Triangle'
+WHERE id = '899ecea6-9c2d-4779-9677-81057dafbb72';
+
+UPDATE chapters
+SET title = 'Chapter 8: Ring'
+WHERE id = '04847452-264b-4bed-be37-ac2ce8b7c316';
+
+-- Patterns Module
+UPDATE chapters
+SET title = 'Chapter 1: Repeating Space'
+WHERE id = '5fc650d0-8c74-4454-90fd-bf169dd740f5';
+
+UPDATE chapters
+SET title = 'Chapter 2: Linear Repetition Pt 1'
+WHERE id = '4651437f-2d0a-4b0c-9fb3-0cbcf517775d';
+
+UPDATE chapters
+SET title = 'Chapter 3: Linear Repetition Pt 2'
+WHERE id = '83f27b85-5e10-4429-a9fc-6e04c8b9a6d2';
+
+UPDATE chapters
+SET title = 'Chapter 4: Grid Repetition'
+WHERE id = '6156514a-9bb8-4b88-bbe7-117641d5ae28';
+
+UPDATE chapters
+SET title = 'Chapter 5: Mix Repetition'
+WHERE id = '1c3463e5-8774-46b2-9b41-8b6c85fb2650';
+
+-- Motion Module
+UPDATE chapters
+SET title = 'Chapter 1: Time Clock'
+WHERE id = '22560dcf-970c-4e46-8a8c-c8cdc4dcb5ff';
+
+UPDATE chapters
+SET title = 'Chapter 2: Trigonometry Pt 1'
+WHERE id = 'c33a7b2f-e979-4247-888a-da13f03a3b06';
+
+UPDATE chapters
+SET title = 'Chapter 3: Trigonometry Pt 2'
+WHERE id = 'cd1f2792-0139-4e4b-b72d-0c1f0b6bcb43';
+
+-- Goodbye Module
+UPDATE chapters
+SET title = 'Chapter 1: Course Ending'
+WHERE id = 'b2b58372-1a97-42b5-ac2e-c04a52fe841a';
